24c726f1809362a77763baadcccdacdc62ca5f77
Eczbek <eczbek.void@gmail.com>
2022-04-11 17:07:15 -0400

    Add utilities for file, string, and vector
    please do not the cat

--- src/util.h
+++ src/util.h
@@ -1,37 +1,74 @@
 #ifndef UTIL_H
 #define UTIL_H
 
-#include <string>
 #include <iostream>
-#include <thread>
+#include <string>
 #include <chrono>
+#include <vector>
 
 
 namespace util {
-	namespace io {
-		void ignore (char until = '\n');
-
-		template <typename inputType>
-		inputType prompt (std::string message = "") {
-			inputType value;
-			while (true) {
-				std::cout << message;
-				std::cin >> value;
-				if (!std::cin.fail()) return value;
-				std::cin.clear();
-				util::io::ignore();
-			}
+	namespace time {
+		template <typename T = std::chrono::milliseconds>
+		long now () {
+			return std::chrono::duration_cast<T>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
 		}
 	}
 
-	namespace rand {
-		double get (double max, double min = 0.0);
+	namespace console {
+		void ignoreLine ();
+
+		template <typename T>
+		T prompt (std::string message = "", bool repeat = false) {
+			T result = T();
+			do {
+				std::cout << message;
+				std::cin >> result;
+				if (!std::cin.fail()) {
+					return result;
+				}
+				util::console::ignoreLine();
+			} while (repeat);
+		}
 	}
 
-	namespace time {
-		template <typename duration = std::chrono::milliseconds>
-		long now () {
-			return std::chrono::duration_cast<duration>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+	namespace file {
+		std::string read (std::string path);
+
+		void write (std::string path, std::string content);
+	}
+
+	namespace random {
+		double get (double max, double min = 0.0, bool inclusive = false);
+	}
+
+	namespace string {
+		std::vector<std::string> split (std::string string, std::string delimiter);
+
+		std::string join (std::vector<std::string> array, std::string delimiter = ", ");
+	}
+
+	namespace vector {
+		template <typename T>
+		std::vector<T> erase (std::vector<T> vector, int startIndex, int eraseCount = 1) {
+			for (int i = 0; i < eraseCount; ++i) {
+				vector.erase(vector.begin() + startIndex);
+			}
+			return vector;
+		}
+
+		template <typename T>
+		std::vector<T> splice (std::vector<T> vector, int startIndex, int eraseCount, T insert) {
+			vector = util::vector::erase<T>(vector, startIndex, eraseCount);
+			vector.insert(vector.begin() + startIndex, insert);
+			return vector;
+		}
+
+		template <typename T>
+		std::vector<T> splice (std::vector<T> vector, int startIndex, int eraseCount, std::vector<T> insertVector) {
+			vector = util::vector::erase<T>(vector, startIndex, eraseCount);
+			vector.insert(vector.begin() + startIndex, insertVector.begin(), insertVector.end());
+			return vector;
 		}
 	}
 }
