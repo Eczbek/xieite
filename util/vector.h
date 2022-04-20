91ac189e4286d58b7d88817277462781386a4ff8
Eczbek <eczbek.void@gmail.com>
2022-04-19 21:36:27 -0400

    Add vector bounds assertions

--- util/vector.h
+++ util/vector.h
@@ -2,15 +2,18 @@
 #define UTIL_VECTOR_H
 
 #include <vector>
+#include <cassert>
 
 
 namespace util {
 	namespace vector {
 		template <typename T>
 		std::vector<T> erase (std::vector<T> vector, int start, int erase = 1) {
+			int size = vector.size();
 			if (start < 0) {
-				start += vector.size() + 1;
+				start += size + 1;
 			}
+			assert(start >= 0 && start < size && start + erase >= 0 && start + erase < size);
 			vector.erase(vector.begin() + start, vector.begin() + start + erase);
 			return vector;
 		}
@@ -37,12 +40,14 @@ namespace util {
 
 		template <typename T>
 		std::vector<T> slice (std::vector<T> vector, int start, int end = -1) {
+			int size = vector.size();
 			if (start < 0) {
-				start += vector.size() + 1;
+				start += size + 1;
 			}
 			if (end < 0) {
-				end += vector.size() + 1;
+				end += size + 1;
 			}
+			assert(start >= 0 && start < size && end >= start && end < size);
 			std::vector<T> result;
 			result.insert(result.begin(), vector.begin() + start, vector.begin() + end);
 			return result;
