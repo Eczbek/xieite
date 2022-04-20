a4cd1050522c6eed6b69323dfb6a13af171f1b22
Eczbek <eczbek.void@gmail.com>
2022-04-19 21:28:44 -0400

    Fix formatting

--- util/string.h
+++ util/string.h
@@ -7,9 +7,9 @@
 
 namespace util {
 	namespace string {
-		std::vector<std::string> split (std::string string, char delimiter);
-
 		std::vector<std::string> split (std::string string, std::string delimiter = "");
+
+		std::vector<std::string> split (std::string string, char delimiter);
 
 		std::string join (std::vector<std::string> strings, std::string delimiter = ", ");
 	}
