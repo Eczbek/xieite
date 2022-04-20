df3848940d7b424938220e42c1be748415844194
Eczbek <eczbek.void@gmail.com>
2022-04-20 10:30:15 -0400

    Improve file reading

--- util/file.cpp
+++ util/file.cpp
@@ -5,20 +5,9 @@
 
 std::string util::file::read (std::string path) {
 	std::ifstream reader(path);
-	std::string result;
-	if (reader.is_open()) {
-		std::string line;
-		while (reader.good()) {
-			std::getline(reader, line);
-			result += '\n' + line;
-		}
-	}
-	return result.substr(1);
+	return std::string(std::istreambuf_iterator<char>(reader), {});
 }
 
 void util::file::write (std::string path, std::string content) {
-	std::ofstream writer(path);
-	if (writer.is_open()) {
-		writer << content;
-	}
+	std::ofstream(path) << content;
 }
