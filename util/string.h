c3a9baac39d8e0d06567fe3c7a1d47c7a34905e2
Eczbek <eczbek.void@gmail.com>
2022-04-21 09:09:54 -0400

    Remove string splitting by char

--- util/string.h
+++ util/string.h
@@ -9,8 +9,6 @@ namespace util {
 	namespace string {
 		std::vector<std::string> split (std::string string, std::string delimiter = "");
 
-		std::vector<std::string> split (std::string string, char delimiter);
-
 		std::string join (std::vector<std::string> strings, std::string delimiter = ", ");
 	}
 }
