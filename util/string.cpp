c7c0f91eef05698d7a7c0275988fd9d942d9d6bf
Eczbek <eczbek.void@gmail.com>
2022-04-28 15:38:12 -0400

    Add split and join with char

--- util/string.cpp
+++ util/string.cpp
@@ -15,6 +15,19 @@ std::vector<std::string> util::string::split (std::string string, const std::str
 	return strings;
 }
 
+std::vector<std::string> util::string::split (std::string string, char delimiter) {
+	std::vector<std::string> strings;
+	int stringLength = string.length();
+	for (int i = stringLength > 1; i < stringLength; ++i)
+		if (string[i] == delimiter) {
+			strings.push_back(string.substr(0, i));
+			string = string.substr(i + 1);
+			i = -1;
+		}
+	strings.push_back(string);
+	return strings;
+}
+
 std::string util::string::join (const std::vector<std::string>& strings, const std::string& delimiter) {
 	if (!strings.size())
 		return "";
@@ -22,4 +35,21 @@ std::string util::string::join (const std::vector<std::string>& strings, const s
 	for (int i = 1; i < strings.size(); ++i)
 		string += delimiter + strings[i];
 	return string;
+}
+
+std::string util::string::join (const std::vector<std::string>& strings, char delimiter) {
+	return util::string::join(strings, std::string(1, delimiter));
+}
+
+std::string util::string::join (const std::vector<char>& characters, const std::string& delimiter) {
+	if (!characters.size())
+		return "";
+	std::string string = std::string(1, characters[0]);
+	for (int i = 1; i < characters.size(); ++i)
+		string += delimiter + characters[i];
+	return string;
+}
+
+std::string util::string::join (const std::vector<char>& characters, char delimiter) {
+	return util::string::join(characters, std::string(1, delimiter));
 }
--- util/string.h
+++ util/string.h
@@ -6,9 +6,17 @@
 
 namespace util {
 	namespace string {
-		std::vector<std::string> split (std::string string, const std::string& delimiter = "");
+		std::vector<std::string> split (std::string string, const std::string& delimiter);
 
-		std::string join (const std::vector<std::string>& strings, const std::string& delimiter = ", ");
+		std::vector<std::string> split (std::string string, char delimiter);
+
+		std::string join (const std::vector<std::string>& strings, const std::string& delimiter);
+
+		std::string join (const std::vector<std::string>& strings, char delimiter);
+
+		std::string join (const std::vector<char>& characters, const std::string& delimiter);
+
+		std::string join (const std::vector<char>& characters, char delimiter);
 	}
 }
 
