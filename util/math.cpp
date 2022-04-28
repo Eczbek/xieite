8539ebcf6b01b4dc6324bde6ebff12a2c0d3bfc5
Eczbek <eczbek.void@gmail.com>
2022-04-28 13:42:27 -0400

    Add math.h and math.cpp
    much maths

---
+++ util/math.cpp
@@ -0,0 +1,25 @@
+
+#include "./math.h"
+
+double util::math::clamp (double value, double max, double min) {
+	return std::fmin(std::fmax(value, min), max);
+}
+
+double util::math::lerp (double value, double max, double min) {
+	return value * (max - min) + min;
+}
+
+double util::math::norm (double value, double max, double min) {
+	return (value - min) / (max - min);
+}
+
+double util::math::wrap (double value, double max, double min) {
+	if (!min)
+		return std::fmod(value, max);
+	int diff = max - min;
+	return std::fmod(std::fmod(value - min, diff) + diff, diff) + min;
+}
+
+double util::math::round (double value, double interval) {
+	return std::round(value / interval) * interval;
+}
---
+++ util/math.h
@@ -0,0 +1,20 @@
+#ifndef UTIL_MATH_H
+#define UTIL_MATH_H
+
+#include <cmath>
+
+namespace util {
+	namespace math {
+		double clamp (double value, double max, double min = 0);
+
+		double lerp (double value, double max, double min = 0);
+
+		double norm (double value, double max, double min = 0);
+
+		double wrap (double value, double max, double min = 0);
+
+		double round (double value, double interval);
+	}
+}
+
+#endif
