e91b5a16ac73572a81c4da02205f9d2d89c5dd35
Eczbek <eczbek.void@gmail.com>
2022-04-28 11:13:12 -0400

    Add misc.h

---
+++ util/misc.h
@@ -0,0 +1,16 @@
+#ifndef UTIL_MISC_H
+#define UTIL_MISC_H
+
+#include <string>
+#include <cstdarg>
+
+namespace util {
+	namespace misc {
+		template <typename T>
+		std::string getType (T value) {
+			return std::string(typeid(value).name()).substr(1);
+		}
+	}
+}
+
+#endif
