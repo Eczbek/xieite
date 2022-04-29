f2a85fd20055dbd35ca70bd475b923600951f555
Eczbek <eczbek.void@gmail.com>
2022-04-28 22:01:03 -0400

    Add mergeSort

--- util/algorithm.h
+++ util/algorithm.h
@@ -22,6 +22,29 @@ namespace util {
 					right = middle;
 			}
 		}
+
+		template <typename T, class C>
+		std::vector<T> mergeSort (const std::vector<T>& vector, const C& sortCallback) {
+			std::vector<T> result = vector;
+			int size = result.size();
+			if (size < 2)
+				return result;
+			int middle = size / 2;
+			std::vector<T> left = mergeSort(std::vector<T>(result.begin(), result.begin() + middle), sortCallback);
+			std::vector<T> right = mergeSort(std::vector<T>(result.begin() + middle, result.end()), sortCallback);
+			result.clear();
+			while (left.size() && right.size())
+				if (sortCallback(left[0], right[0]) < 0) {
+					result.push_back(left[0]);
+					left.erase(left.begin());
+				} else {
+					result.push_back(right[0]);
+					right.erase(right.begin());
+				}
+			result.insert(result.end(), left.begin(), left.end());
+			result.insert(result.end(), right.begin(), right.end());
+			return result;
+		}
 	}
 }
 
