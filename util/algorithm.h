1c5e0b41aef4e84b7453cb8c296a7bcc5a004bc7
Eczbek <eczbek.void@gmail.com>
2022-04-28 22:19:01 -0400

    Somewhat optmize megeSort

--- util/algorithm.h
+++ util/algorithm.h
@@ -25,21 +25,19 @@ namespace util {
 
 		template <typename T, class C>
 		std::vector<T> mergeSort (const std::vector<T>& vector, const C& sortCallback) {
-			std::vector<T> result = vector;
-			int size = result.size();
-			if (size < 2)
-				return result;
-			int middle = size / 2;
-			std::vector<T> left = mergeSort(std::vector<T>(result.begin(), result.begin() + middle), sortCallback);
-			std::vector<T> right = mergeSort(std::vector<T>(result.begin() + middle, result.end()), sortCallback);
-			result.clear();
+			int middle = vector.size() / 2;
+			if (!middle)
+				return vector;
+			std::vector<T> left = mergeSort(std::vector<T>(vector.begin(), vector.begin() + middle), sortCallback);
+			std::vector<T> right = mergeSort(std::vector<T>(vector.begin() + middle, vector.end()), sortCallback);
+			std::vector<T> result;
 			while (left.size() && right.size())
-				if (sortCallback(left[0], right[0]) < 0) {
-					result.push_back(left[0]);
-					left.erase(left.begin());
-				} else {
+				if (sortCallback(right[0], left[0])) {
 					result.push_back(right[0]);
 					right.erase(right.begin());
+				} else {
+					result.push_back(left[0]);
+					left.erase(left.begin());
 				}
 			result.insert(result.end(), left.begin(), left.end());
 			result.insert(result.end(), right.begin(), right.end());
