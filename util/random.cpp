6f915511e3980c4dcf871bc6ab937c53cb41b2ab
Eczbek <eczbek.void@gmail.com>
2022-04-29 07:44:25 -0400

    Make consistent format

--- util/random.cpp
+++ util/random.cpp
@@ -3,11 +3,11 @@
 #include <random>
 
 double util::random::mt32 (double max, double min, bool inclusive) {
-	static std::mt19937 rng32 (std::random_device {}());
+	static std::mt19937 rng32 (std::random_device{}());
 	return rng32() / 2.0 / INT32_MAX * (max - min + inclusive) + min;
 }
 
 double util::random::mt64 (double max, double min, bool inclusive) {
-	static std::mt19937_64 rng64 (std::random_device {}());
+	static std::mt19937_64 rng64 (std::random_device{}());
 	return rng64() / 2.0 / INT64_MAX * (max - min + inclusive) + min;
 }
