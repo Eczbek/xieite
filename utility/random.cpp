11b3037f4f4e3dac69f07296e3ed05e2f80839c1
Eczbek <eczbek.void@gmail.com>
2022-05-02 14:48:51 -0400

    Fix space formatting

--- utility/random.cpp
+++ utility/random.cpp
@@ -3,11 +3,11 @@
 #include <random>
 
 double utility::random::mt32 (double max, double min, bool inclusive) {
-	static std::mt19937 rng32 (std::random_device{}());
+	static std::mt19937 rng32(std::random_device {}());
 	return rng32() / 2.0 / INT32_MAX * (max - min + inclusive) + min;
 }
 
 double utility::random::mt64 (double max, double min, bool inclusive) {
-	static std::mt19937_64 rng64 (std::random_device{}());
+	static std::mt19937_64 rng64(std::random_device {}());
 	return rng64() / 2.0 / INT64_MAX * (max - min + inclusive) + min;
 }
