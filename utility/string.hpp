5b134f82029b26a1f977c4b06b44628ab314d8f7
Eczbek <eczbek.void@gmail.com>
2022-05-03 14:29:36 -0400

    Template-ify string split

--- utility/string.hpp
+++ utility/string.hpp
@@ -1,15 +1,37 @@
 #ifndef UTILITY_STRING_HPP
 #define UTILITY_STRING_HPP
 
-#include <string>
 #include <string_view>
 #include <vector>
 
 namespace utility {
 	namespace string {
-		std::vector<std::string_view> split (std::string_view string, const std::string_view& delimiter);
+		template <typename StringType = std::string>
+		std::vector<StringType> split (StringType string, const StringType& delimiter) {
+			std::vector<StringType> segments;
+			const int delimiterLength = delimiter.length();
+			for (int i = string.length() > 1; i < string.length(); ++i)
+				if (string.substr(i, delimiterLength) == delimiter) {
+					segments.push_back(string.substr(0, i));
+					string = string.substr(i + delimiterLength);
+					i = -!!delimiterLength;
+				}
+			segments.push_back(string);
+			return segments;
+		}
 
-		std::vector<std::string_view> split (std::string_view string, char delimiter);
+		template <typename StringType = std::string>
+		std::vector<StringType> split (StringType string, char delimiter) {
+			std::vector<StringType> segments;
+			for (int i = string.length() > 1; i < string.length(); ++i)
+				if (string[i] == delimiter) {
+					segments.push_back(string.substr(0, i));
+					string = string.substr(i + 1);
+					i = -1;
+				}
+			segments.push_back(string);
+			return segments;
+		}
 	}
 }
 
