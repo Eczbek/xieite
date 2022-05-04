ec2eb9b0926e383b1b1e1eebc677112ffcb2ad44
Eczbek <eczbek.void@gmail.com>
2022-05-04 19:46:43 -0400

    Improve random functions with uniform distribution

--- utility/random.cpp
+++ utility/random.cpp
@@ -4,10 +4,10 @@
 
 double utility::random::mt32 (double max, double min, bool inclusive) {
 	static std::mt19937 rng32(std::random_device {}());
-	return rng32() / 2.0 / INT32_MAX * (max - min + inclusive) + min;
+	return std::uniform_real_distribution<>(min, max + inclusive)(rng32);
 }
 
 double utility::random::mt64 (double max, double min, bool inclusive) {
 	static std::mt19937_64 rng64(std::random_device {}());
-	return rng64() / 2.0 / INT64_MAX * (max - min + inclusive) + min;
+	return std::uniform_real_distribution<>(min, max + inclusive)(rng64);
 }
