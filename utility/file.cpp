7e175b29f5f859e1fec0c81907b4aa074815590b
Eczbek <eczbek.void@gmail.com>
2022-05-04 07:31:21 -0400

    Template-ify file reader

--- utility/file.cpp
+++ utility/file.cpp
@@ -1,14 +1,7 @@
 
 #include "./file.hpp"
-#include <fstream>
 
 std::string utility::file::read (const std::string& path) {
 	std::ifstream reader(path);
 	return std::string(std::istreambuf_iterator<char>(reader), {});
-}
-
-void utility::file::write (const std::string& path, const std::string_view& data) {
-	std::ofstream writer(path);
-	if (writer)
-		writer << data;
 }
--- utility/file.hpp
+++ utility/file.hpp
@@ -3,12 +3,18 @@
 
 #include <string>
 #include <string_view>
+#include <fstream>
 
 namespace utility {
 	namespace file {
 		std::string read (const std::string& path);
-
-		void write (const std::string& path, const std::string_view& data);
+		
+		template <typename DataType = std::string_view>
+		void write (const std::string& path, const DataType& data) {
+			std::ofstream writer(path);
+			if (writer)
+				writer << data;
+		}
 	}
 }
 
