1240e4f7b36496e05e109a74334d7b4911becea2
Eczbek <eczbek.void@gmail.com>
2022-05-05 19:42:28 -0400

    Make shuffle accept iterators and remove mergeSort

--- utility/algorithm.hpp
+++ utility/algorithm.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <vector>
 #include <random>
 
 namespace utility {
@@ -22,37 +21,14 @@ namespace utility {
 			}
 		}
 
-		template <typename VectorType, class LambdaType>
-		std::vector<VectorType> mergeSort (const std::vector<VectorType>& vector, const LambdaType& sortCallback) {
-			const int middle = vector.size() / 2;
-			if (!middle)
-				return vector;
-			std::vector<VectorType> left = mergeSort(std::vector<VectorType>(vector.begin(), vector.begin() + middle), sortCallback);
-			std::vector<VectorType> right = mergeSort(std::vector<VectorType>(vector.begin() + middle, vector.end()), sortCallback);
-			std::vector<VectorType> sorted;
-			while (left.size() && right.size())
-				if (sortCallback(right[0], left[0])) {
-					sorted.push_back(right[0]);
-					right.erase(right.begin());
-				} else {
-					sorted.push_back(left[0]);
-					left.erase(left.begin());
-				}
-			sorted.insert(sorted.end(), left.begin(), left.end());
-			sorted.insert(sorted.end(), right.begin(), right.end());
-			return sorted;
-		}
-
-		template <typename VectorType>
-		std::vector<VectorType> shuffle (std::vector<VectorType> vector) {
+		template <typename IteratorType>
+		void shuffle (IteratorType begin, IteratorType end) {
 			std::mt19937 rng32 (std::random_device{}());
-			for (int i = vector.size() - 1; i > 0; --i) {
-				const int j = std::uniform_int_distribution<>(0, i)(rng32);
-				const VectorType temp = vector[i];
-				vector[i] = vector[j];
-				vector[j] = temp;
+			for (auto size = std::distance(begin, end); begin != end; ++begin) {
+				IteratorType temp = begin;
+				std::advance(temp, std::uniform_int_distribution<>(0, --size)(rng32));
+				std::swap(*temp, *begin);
 			}
-			return vector;
 		}
 	}
 }
