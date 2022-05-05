911c22c29d7884d07e4bec7915ab5578b606d651
Eczbek <eczbek.void@gmail.com>
2022-05-04 20:51:03 -0400

    Remove random.h and random.cpp

--- utility/random.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace utility {
-	namespace random {
-		double mt32 (double max, double min = 0, bool inclusive = false);
-
-		double mt64 (double max, double min = 0, bool inclusive = false);
-	}
-}
