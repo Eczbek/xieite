4b7bc7f362c79e33ef03eeb6fa892bda89b1452e
Eczbek <eczbek.void@gmail.com>
2022-05-05 21:23:42 -0400

    Remove duplicates of std functions

--- utility/math.cpp
+++ utility/math.cpp
@@ -1,22 +1,7 @@
 #include "./math.hpp"
 
 #include <cmath>
-
-double utility::math::clamp (double value, double max, double min) {
-	return value > max
-		? max
-		: value < min
-			? min
-			: value;
-}
-
-double utility::math::lerp (double value, double max, double min) {
-	return value * (max - min) + min;
-}
-
-double utility::math::norm (double value, double max, double min) {
-	return (value - min) / (max - min);
-}
+#include <algorithm>
 
 double utility::math::wrap (double value, double max, double min) {
 	if (!min)
@@ -25,16 +10,16 @@ double utility::math::wrap (double value, double max, double min) {
 	return std::fmod(std::fmod(value - min, diff) + diff, diff) + min;
 }
 
-double utility::math::round (double value, double interval) {
-	return std::round(value / interval) * interval;
-}
-
 bool utility::math::approxEqual (double value1, double value2, double absoluteEpsilon, double relativeEpsilon) {
 	return std::abs(value1 - value2) <= std::fmin(absoluteEpsilon, std::fmax(std::abs(value1), std::abs(value2)) * relativeEpsilon);
 }
 
 int utility::math::sign (double value) {
-	return utility::math::clamp(value, 1, -1);
+	return value > 0
+		? 1
+		: value < 0
+			? -1
+			: 0;
 }
 
 double utility::math::radiansToDegrees (double radians) {
--- utility/math.hpp
+++ utility/math.hpp
@@ -2,15 +2,7 @@
 	
 namespace utility {
 	namespace math {
-		double clamp (double value, double max, double min = 0);
-
-		double lerp (double value, double max, double min = 0);
-
-		double norm (double value, double max, double min = 0);
-
 		double wrap (double value, double max, double min = 0);
-
-		double round (double value, double interval = 1);
 
 		bool approxEqual (double value1, double value2, double absoluteEpsilon = 1e-12, double relativeEpsilon = 1e-8);
 
