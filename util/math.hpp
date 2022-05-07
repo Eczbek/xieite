7bb801a430269f0d2878499ea0fe4f161a0c2051
Eczbek <eczbek.void@gmail.com>
2022-05-06 20:52:05 -0400

    Template-ify math functions

--- util/math.hpp
+++ util/math.hpp
@@ -2,14 +2,34 @@
 	
 namespace util {
 	namespace math {
-		double wrap (const double value, const double min, const double max);
+		template <typename NumberType>
+		NumberType wrap (const NumberType value, const NumberType min, const NumberType max) {
+			const NumberType diff = max - min;
+			return std::fmod(std::fmod(value - min, diff) + diff, diff) + min;
+		}
 
-		bool approx_eq (const double value1, const double value2, const double absoluteEpsilon = 1e-12, const double relativeEpsilon = 1e-8);
+		template <typename NumberType>
+		bool approx_eq (const NumberType value1, const NumberType value2, const double absoluteEpsilon = 1e-12, const double relativeEpsilon = 1e-8) {
+			return std::abs(value1 - value2) <= std::fmin(absoluteEpsilon, std::fmax(std::abs(value1), std::abs(value2)) * relativeEpsilon);
+		}
 
-		int sign (const double value);
+		template <typename NumberType>
+		int sign (const NumberType value) {
+			return value > 0
+				? 1
+				: value < 0
+					? -1
+					: 0;
+		}
 
-		double rad_to_deg (const double radians);
+		template <typename NumberType>
+		NumberType rad_to_deg (const NumberType radians) {
+			return radians * 180.0 / M_PI;
+		}
 
-		double deg_to_rad (const double degrees);
+		template <typename NumberType>
+		NumberType deg_to_rad (const NumberType degrees) {
+			return degrees * M_PI / 180.0;
+		}
 	}
 }
