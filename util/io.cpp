c691bd89f8d41a3850d041c14516a09b11f4256e
Eczbek <eczbek.void@gmail.com>
2022-05-07 11:23:43 -0400

    Optimize raw and nonblock code

--- util/io.cpp
+++ util/io.cpp
@@ -1,9 +1,25 @@
 #include "./io.hpp"
 
-#include <limits>
+#include <fcntl.h>
+#include <iostream>
+
+util::io::raw::raw () {
+	tcgetattr(STDIN_FILENO, &cooked);
+	termios raw = cooked;
+	cfmakeraw(&raw);
+	tcsetattr(STDIN_FILENO, TCSANOW, &raw);
+}
+
+util::io::raw::~raw () {
+	tcsetattr(STDIN_FILENO, TCSANOW, &cooked);
+}
+
+void util::io::nonblock () {
+	fcntl(STDIN_FILENO, F_SETFL, fcntl(STDIN_FILENO, F_GETFL) | O_NONBLOCK);
+}
 
 void util::io::ignr () {
-	fcntl(STDIN_FILENO, F_SETFL, fcntl(STDIN_FILENO, F_GETFL) | O_NONBLOCK);
+	util::io::nonblock();
 	char temp;
 	while (read(STDIN_FILENO, &temp, 1) == 1);
 }
@@ -12,28 +28,15 @@ void util::io::clr_scrn () {
 	std::cout << "\033[2J\033[1;1H";
 }
 
-char util::io::char_wait (bool echo) {
-	termios cooked, raw;
-	tcgetattr(STDIN_FILENO, &cooked);
-	raw = cooked;
-	raw.c_lflag &= ~ICANON;
-	raw.c_lflag &= echo ? ECHO : ~ECHO;
-	tcsetattr(STDIN_FILENO, TCSANOW, &raw);
-	const char input = getchar();
-	tcsetattr(STDIN_FILENO, TCSANOW, &cooked);
-	return input;
+char util::io::char_wait () {
+	util::io::raw();
+	return getchar();
 }
 
-char util::io::char_read (bool echo) {
-	termios cooked, raw;
-	tcgetattr(STDIN_FILENO, &cooked);
-	raw = cooked;
-	raw.c_lflag &= ~ICANON;
-	raw.c_lflag &= echo ? ECHO : ~ECHO;
-	tcsetattr(STDIN_FILENO, TCSANOW, &raw);
-	fcntl(STDIN_FILENO, F_SETFL, fcntl(STDIN_FILENO, F_GETFL) | O_NONBLOCK);
+char util::io::char_read () {
+	util::io::raw();
+	util::io::nonblock();
 	char input = 0;
 	read(STDIN_FILENO, &input, 1);
-	tcsetattr(STDIN_FILENO, TCSANOW, &cooked);
 	return input;
 }
--- util/io.hpp
+++ util/io.hpp
@@ -1,37 +1,38 @@
 #pragma once
 
-#include <chrono>
 #include <termios.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <thread>
-#include <string>
-#include <vector>
-#include <iostream>
 
 namespace util {
 	namespace io {
+		class raw {
+			private:
+				termios cooked;
+
+			public:
+				raw ();
+
+				~raw ();
+		};
+
+		void nonblock ();
+
 		void ignr ();
 
 		void clr_scrn ();
 
-		char char_wait (bool echo = true);
+		char char_wait ();
 
-		char char_read (bool echo = true);
+		char char_read ();
 
 		template <typename DurationType>
-		char char_read_timeout (const DurationType& timeout, char defaultChar = 0, bool echo = true) {
-			termios cooked, raw;
-			tcgetattr(STDIN_FILENO, &cooked);
-			raw = cooked;
-			raw.c_lflag &= ~ICANON;
-			raw.c_lflag &= echo ? ECHO : ~ECHO;
-			tcsetattr(STDIN_FILENO, TCSANOW, &raw);
-			fcntl(STDIN_FILENO, F_SETFL, fcntl(STDIN_FILENO, F_GETFL) | O_NONBLOCK);
+		char char_read_timeout (const DurationType& timeout, const char defaultChar = 0) {
+			util::io::raw();
+			util::io::nonblock();
 			std::this_thread::sleep_for(timeout);
 			char input = defaultChar;
 			while (read(STDIN_FILENO, &input, 1) == 1);
-			tcsetattr(STDIN_FILENO, TCSANOW, &cooked);
 			return input;
 		}
 	}
