4c3bbcf6c50f08ee4c509de5c35028d49e542632
Eczbek <eczbek.void@gmail.com>
2022-05-08 09:53:25 -0400

    Add universal base converter

--- util/math.hpp
+++ util/math.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <cmath>
-	
+#include <string_view>
+#include <algorithm>
+
 namespace util {
 	namespace math {
 		template <typename NumberType>
@@ -32,6 +34,34 @@ namespace util {
 		template <typename NumberType>
 		NumberType deg_to_rad (const NumberType degrees) {
 			return degrees * M_PI / 180.0;
+		}
+
+		std::string base (std::string value, const int from, const int to, const std::string_view& digits = "0123456789abcdefghijklmnopqrstuvwxyz") {
+			if (from == 10) {
+				std::string result;
+				int number = std::stoi(value);
+				while (number) {
+					result = digits[number % to] + result;
+					number /= to;
+				}
+				return result;
+			} else {
+				int number = 0;
+				for (int i = value.length() - 1; i >= 0; --i)
+					for (int index = 0; digits[index]; ++index)
+						if (digits[index] == value[i]) {
+							number += index * std::pow(from, value.length() - i - 1);
+							break;
+						}
+				if (to == 10)
+					return std::to_string(number);
+				std::string result;
+				while (number) {
+					result = digits[number % to] + result;
+					number /= to;
+				}
+				return result;
+			}
 		}
 	}
 }
