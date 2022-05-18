3e0b02ad4c17fba3f306bc1f1c247d15015acb86
Eczbek <eczbek.void@gmail.com>
2022-05-18 09:47:05 -0400

    Remove HOURS_BEFORE_EPOCH
    I calculated it wrong anyway

--- time.hpp
+++ time.hpp
@@ -5,8 +5,6 @@
 
 namespace util {
 	namespace time {
-		constexpr int HOURS_BEFORE_EPOCH = 17268648;
-
 		constexpr std::array<std::string_view, 12> MONTH_NAMES { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
 		constexpr std::array<std::string_view, 7> WEEKDAY_NAMES { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
 
