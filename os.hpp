e5c282cceba37ddd0d2fcb8f43f296cc3665ba28
Eczbek <eczbek.void@gmail.com>
2022-05-20 15:00:31 -0400

    Create os.hpp

---
+++ os.hpp
@@ -0,0 +1,8 @@
+
+#if defined(_WIN32) || defined(_WIN64) || defined(__WIN32__) || defined(__WINDOWS__)
+	#define OS_WINDOWS
+#elif defined(__APPLE__) || defined(__MACH__)
+	#define OS_MAC
+#elif defined(__unix__) || defined(__unix) || defined(unix)
+	#define OS_UNIX
+#endif
