9f60f29bd3025bc0f7191071c2b94446ed7784ba
Eczbek <eczbek.void@gmail.com>
2022-05-22 21:09:46 -0400

    Add string trimmers

--- str.cpp
+++ str.cpp
@@ -1,13 +1,14 @@
 #include "./str.hpp"
+#include <limits>
+#include <array>
 
 std::vector<std::string> util::str::split (const std::string& string, const std::string_view& delimiter) {
 	std::vector<std::string> segments;
 	std::size_t i = 0;
 	for (std::size_t j = 0; j < string.length() - i; ++j)
 		if (string.substr(j, delimiter.length()) == delimiter) {
-			segments.push_back(string.substr(i, j));
-			i = j + delimiter.length();
-			j = -1;
+			segments.push_back(string.substr(i, j - i));
+			i = j += delimiter.length();
 		}
 	segments.push_back(string.substr(i));
 	return segments;
@@ -20,7 +21,7 @@ std::vector<std::string> util::str::split (const std::string& string, const char
 		if (string[j] == delimiter) {
 			segments.push_back(string.substr(i, j - i));
 			i = ++j;
-		}
+		} 
 	segments.push_back(string.substr(i));
 	return segments;
 }
@@ -29,4 +30,44 @@ std::string util::str::truncate (const std::string& string, const std::size_t le
 	return string.length() <= length
 		? string
 		: string.substr(0, length - suffix.length()) + suffix;
+}
+
+std::string util::str::trim_left (const std::string& string, const char character) {
+	for (std::size_t i = 0; i < string.length(); ++i)
+		if (string[i] != character)
+			return string.substr(i);
+	return "";
+}
+
+std::string util::str::trim_right (const std::string& string, const char character) {
+	for (std::size_t i = string.length(); i > 0; --i)
+		if (string[i - 1] != character)
+			return string.substr(0, i);
+	return "";
+}
+
+std::string util::str::trim_left (const std::string& string, const std::string_view& characters) {
+	std::array<bool, std::numeric_limits<char>::max() - std::numeric_limits<char>::min() + 1> charMap;
+	const auto getChar = [&](const char character) -> bool& {
+		return charMap[character - std::numeric_limits<char>::min()];
+	};
+	for (const char character : characters)
+		getChar(character) = true;
+	for (std::size_t i = 0; i < string.length(); ++i)
+		if (!getChar(string[i]))
+			return string.substr(i);
+	return "";
+}
+
+std::string util::str::trim_right (const std::string& string, const std::string_view& characters) {
+	std::array<bool, std::numeric_limits<char>::max() - std::numeric_limits<char>::min() + 1> charMap;
+	const auto hasChar = [&](const char character) -> bool& {
+		return charMap[character - std::numeric_limits<char>::min()];
+	};
+	for (const char character : characters)
+		hasChar(character) = true;
+	for (std::size_t i = string.length(); i > 0; --i)
+		if (!hasChar(string[i - 1]))
+			return string.substr(0, i);
+	return "";
 }
--- str.hpp
+++ str.hpp
@@ -10,5 +10,13 @@ namespace util {
 		std::vector<std::string> split (const std::string& string, const char delimiter);
 
 		std::string truncate (const std::string& string, const std::size_t length, const std::string& suffix = "...");
+
+		std::string trim_left (const std::string& string, const char character);
+
+		std::string trim_right (const std::string& string, const char character);
+
+		std::string trim_left (const std::string& string, const std::string_view& characters);
+
+		std::string trim_right (const std::string& string, const std::string_view& characters);
 	}
 }
