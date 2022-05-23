1bf715dd2f455cb8524bb362fafb9546fc55daf9
Eczbek <eczbek.void@gmail.com>
2022-05-23 08:32:13 -0400

    Add short versions of months and week days

--- time.hpp
+++ time.hpp
@@ -5,8 +5,10 @@
 
 namespace util {
 	namespace time {
-		constexpr std::array<std::string_view, 12> MONTH_NAMES { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
-		constexpr std::array<std::string_view, 7> WEEKDAY_NAMES { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+		constexpr std::array<std::string_view, 12> MONTHS { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+		constexpr std::array<std::string_view, 12> MONTHS_SHORT { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+		constexpr std::array<std::string_view, 7> WEEK_DAYS { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+		constexpr std::array<std::string_view, 7> WEEK_DAYS_SHORT { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
 
 		template <typename Duration = std::chrono::nanoseconds>
 		long now () {
