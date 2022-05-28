623e61754f2bc91a30654d21cc91cec64062ebdb
Eczbek <eczbek.void@gmail.com>
2022-05-27 22:18:09 -0400

    Restore class 'matrix'

---
+++ mat.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <cassert>
+#include <vector>
+
+
+namespace util {
+	template <typename Value>
+	class matrix {
+	private:
+		std::vector<Value> values;
+		std::vector<std::size_t> dimensions;
+
+	public:
+		matrix(const std::vector<std::size_t>& dimensions)
+			: dimensions(dimensions)
+		{
+			std::size_t size = 1;
+			for (const std::size_t dimension : dimensions)
+				size *= dimension;
+			values.resize(size);
+		}
+
+		matrix(const std::vector<std::size_t>& dimensions, const std::vector<Value>& values)
+			: values(values), dimensions(dimensions)
+		{
+			std::size_t size = 1;
+			for (const std::size_t dimension : dimensions)
+				size *= dimension;
+			assert(size == values.size());
+		}
+
+		Value& operator[](const std::vector<std::size_t>& indices) {
+			assert(indices.size() == dimensions.size());
+			std::size_t index = 0;
+			for (std::size_t i = 0; i < indices.size(); ++i) {
+				assert(indices[i] < dimensions[i]);
+				index = index * dimensions[i] + indices[i];
+			}
+			return values[index];
+		}
+
+		const Value& operator[](const std::vector<std::size_t>& indices) const {
+			assert(indices.size() == dimensions.size());
+			std::size_t index = 0;
+			for (std::size_t i = 0; i < indices.size(); ++i) {
+				assert(indices[i] < dimensions[i]);
+				index = index * dimensions[i] + indices[i];
+			}
+			return values[index];
+		}
+
+		const std::vector<Value>& values() const {
+			return values;
+		}
+
+		const std::vector<std::size_t>& dimensions() const {
+			return dimensions;
+		}
+	};
+}
