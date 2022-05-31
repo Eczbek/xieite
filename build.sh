733c1ded07085f1c4d894e49601d9ee80a58c85c
Eczbek <eczbek.void@gmail.com>
2022-05-31 10:04:50 -0400

    Improve build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
 
-mkdir bin
-cd bin
-for FILE in `ls ../src`
+mkdir temp
+for FILE in `ls src`
 do
-	g++ ../src/$FILE -o $FILE'.o' -c -std=c++20 -I ../include -lutil
+	g++ src/$FILE -o temp/$FILE'.o' -c -std=c++20 -I include -lutil
 done
-ar rcs ../lib/util.a `ls -d $PWD/*`
-cd ..
-rm -rf bin
+ar crs lib/util.a `ls -d temp/*`
+rm -r temp
