ef0c21b1f5bd2c3891b565ab67bf18a58175096b
Eczbek <eczbek.void@gmail.com>
2022-05-30 22:46:02 -0400

    Fix typos from removed namespace

--- src/io.cpp
+++ src/io.cpp
@@ -4,55 +4,55 @@
 #include <sys/ioctl.h>
 
 
-util::io::lock::raw::raw() {
+util::io::raw::raw() {
 	tcgetattr(STDIN_FILENO, &cooked);
 	termios raw = cooked;
 	cfmakeraw(&raw);
 	tcsetattr(STDIN_FILENO, TCSANOW, &raw);
 }
 
-util::io::lock::raw::~raw() {
+util::io::raw::~raw() {
 	tcsetattr(STDIN_FILENO, TCSANOW, &cooked);
 }
 
-util::io::lock::nonblock::nonblock() {
+util::io::nonblock::nonblock() {
 	fcntl(STDIN_FILENO, F_SETFL, fcntl(STDIN_FILENO, F_GETFL) | O_NONBLOCK);
 }
 
-util::io::lock::nonblock::~nonblock() {
+util::io::nonblock::~nonblock() {
 	fcntl(STDIN_FILENO, F_SETFL, fcntl(STDIN_FILENO, F_GETFL) & ~O_NONBLOCK);
 }
 
 void util::io::ignore(const char until) {
-	util::io::lock::nonblock nonblockLock;
+	util::io::nonblock nonblockLock;
 	char input;
 	while (input != until && read(STDIN_FILENO, &input, 1) == 1);
 }
 
 void util::io::ignore(std::streamsize characters) {
-	util::io::lock::nonblock nonblockLock;
+	util::io::nonblock nonblockLock;
 	char input;
 	while (characters-- > 0 && read(STDIN_FILENO, &input, 1) == 1);
 }
 
 char util::io::char_wait() {
-	util::io::lock::raw rawLock;
+	util::io::raw rawLock;
 	char input;
 	read(STDIN_FILENO, &input, 1);
 	return input;
 }
 
 char util::io::char_read(const char defaultChar) {
-	util::io::lock::raw rawLock;
-	util::io::lock::nonblock nonblockLock;
+	util::io::raw rawLock;
+	util::io::nonblock nonblockLock;
 	char input = defaultChar;
 	read(STDIN_FILENO, &input, 1);
 	return input;
 }
 
 std::string util::io::string_read() {
-	util::io::lock::raw rawLock;
-	util::io::lock::nonblock nonblockLock;
+	util::io::raw rawLock;
+	util::io::nonblock nonblockLock;
 	std::string result;
 	char input;
 	while (read(STDIN_FILENO, &input, 1) == 1)
@@ -68,7 +68,7 @@ void util::io::erase_line() {
 	std::cout << "\033[2K";
 }
 
-void util::io::style_set(const int style) {
+void util::io::style(const int style) {
 	std::cout << "\033[" << style << 'm';
 }
 
@@ -80,7 +80,7 @@ void util::io::get_win_size(int& rows, int& cols) {
 }
 
 void util::io::cursor::get(int& row, int& col) {
-	util::io::lock::raw rawLock;
+	util::io::raw rawLock;
 	write(STDOUT_FILENO, "\033[6n", 4);
 	std::string buffer;
 	char input;
