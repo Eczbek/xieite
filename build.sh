5ff9ae18c500612514391c3b910aa38523661d7b
Eczbek <eczbek.void@gmail.com>
2022-06-11 21:50:19 -0400

    Fix formatting in 'build.sh'

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 mkdir temp
 for FILE in `ls src`
 do
-	g++ src/$FILE -o temp/$FILE'.o' -c -std=c++20 -I include -lutil
+	g++ src/$FILE -o temp/$FILE'.o' -c -std=c++20 -I include/ -l util
 done
 ar crs util.a `ls -d temp/*`
 rm -r temp
