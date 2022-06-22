d6778b8457956702b64b4db5a89962b8af97cec5
Eczbek <eczbek.void@gmail.com>
2022-06-22 12:29:32 -0400

    Use 'throw' instead of 'assert'

--- include/util/mat.hpp
+++ include/util/mat.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
-#include <cassert>
+#include <span>
+#include <stdexcept>
 #include <vector>
 
 
@@ -8,12 +9,12 @@ namespace util {
 	template <typename Value>
 	class Matrix {
 	private:
-		std::vector<Value> values;
 		std::vector<std::size_t> dimensions;
+		std::vector<Value> values;
 
 	public:
-		Matrix(const std::vector<std::size_t>& dimensions)
-			: dimensions(dimensions)
+		Matrix(std::span<const std::size_t> dimensions)
+			: dimensions(dimensions.begin(), dimensions.end())
 		{
 			std::size_t size = 1;
 			for (const std::size_t dimension: dimensions)
@@ -21,40 +22,45 @@ namespace util {
 			values.resize(size);
 		}
 
-		Matrix(const std::vector<std::size_t>& dimensions, const std::vector<Value>& values)
-			: values(values), dimensions(dimensions)
+		Matrix(std::span<const std::size_t> dimensions, std::span<const Value> values)
+			: dimensions(dimensions.begin(), dimensions.end()), values(values.begin(), values.end())
 		{
 			std::size_t size = 1;
 			for (const std::size_t dimension: dimensions)
 				size *= dimension;
-			assert(size == values.size());
+			if (size != values.size())
+				throw std::runtime_error("Values do not match dimensions");
 		}
 
-		Value& operator[](const std::vector<std::size_t>& indices) {
-			assert(indices.size() == dimensions.size());
+		Value& operator[](std::span<const std::size_t> indices) {
+			if (indices.size() != dimensions.size())
+				throw std::runtime_error("Indices do not match dimensions");
 			std::size_t index = 0;
 			for (std::size_t i = 0; i < indices.size(); ++i) {
-				assert(indices[i] < dimensions[i]);
+				if (indices[i] >= dimensions[i])
+					throw std::runtime_error("Index out of bounds");
 				index = index * dimensions[i] + indices[i];
 			}
 			return values[index];
 		}
 
-		const Value& operator[](const std::vector<std::size_t>& indices) const {
-			assert(indices.size() == dimensions.size());
+		const Value& operator[](std::span<const std::size_t> indices) const {
+			if (indices.size() != dimensions.size())
+				throw std::runtime_error("Indices do not match dimensions");
 			std::size_t index = 0;
 			for (std::size_t i = 0; i < indices.size(); ++i) {
-				assert(indices[i] < dimensions[i]);
+				if (indices[i] >= dimensions[i])
+					throw std::runtime_error("Index out of bounds");
 				index = index * dimensions[i] + indices[i];
 			}
 			return values[index];
 		}
 
-		const std::vector<Value>& values() const {
+		std::span<const Value> getValues() const {
 			return values;
 		}
 
-		const std::vector<std::size_t>& dimensions() const {
+		std::span<const std::size_t> getDimensions() const {
 			return dimensions;
 		}
 	};
