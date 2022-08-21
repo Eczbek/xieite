5dbd4e0fd26fc5a0485b490c173b00789b9d19e0
Eczbek <eczbek.void@gmail.com>
2022-08-21 12:34:32 -0400

    Remove 'l' flag when building

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 DIR=`mktemp -d`
 for FILE in `ls src`
 do
-	g++ src/$FILE -o $DIR/$FILE -c -std=c++20 -I include/ -l util
+	g++ src/$FILE -o $DIR/$FILE -c -std=c++20 -I include/
 done
 ar crs ${1:-util.a} `ls -d $DIR/*`
 rm -rf $DIR
