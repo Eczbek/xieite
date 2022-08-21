ee5fbf20ff502549c2b8f6b489c96c2d4127abb8
Eczbek <eczbek.void@gmail.com>
2022-08-21 14:58:46 -0400

    Remove default archive name in build system

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ do
 	g++ src/$FILE -o $TEMP_DIR/$FILE -c -std=c++20 -I include/
 done
 echo 'Archiving'
-ar crs ${1:-util.a} `ls -d $TEMP_DIR/*`
+ar crs ${1:-.a} `ls -d $TEMP_DIR/*`
 rm -rf $TEMP_DIR
 echo 'Done'
