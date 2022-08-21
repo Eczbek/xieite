75de7e11a2cb1f502e9569cc93a79c82acb16c62
Eczbek <eczbek.void@gmail.com>
2022-08-21 14:44:15 -0400

    Rename variable 'DIR' to 'TEMP_DIR' in build system

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-DIR=`mktemp -d`
+TEMP_DIR=`mktemp -d`
 for FILE in `ls src`
 do
 	echo 'Compiling '$FILE
-	g++ src/$FILE -o $DIR/$FILE -c -std=c++20 -I include/
+	g++ src/$FILE -o $TEMP_DIR/$FILE -c -std=c++20 -I include/
 done
 echo 'Archiving'
-ar crs ${1:-util.a} `ls -d $DIR/*`
-rm -rf $DIR
+ar crs ${1:-util.a} `ls -d $TEMP_DIR/*`
+rm -rf $TEMP_DIR
 echo 'Done'
