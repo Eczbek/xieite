e6c0cc640d646a581c9a081c8e70ea0c81dd6409
Eczbek <eczbek.void@gmail.com>
2022-08-21 14:41:33 -0400

    Add log to build system

--- build.sh
+++ build.sh
@@ -3,7 +3,10 @@
 DIR=`mktemp -d`
 for FILE in `ls src`
 do
+	echo 'Compiling '$FILE
 	g++ src/$FILE -o $DIR/$FILE -c -std=c++20 -I include/
 done
+echo 'Archiving'
 ar crs ${1:-util.a} `ls -d $DIR/*`
 rm -rf $DIR
+echo 'Done'
