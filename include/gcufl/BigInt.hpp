f1fa2b0e144cdcce5a48ba6e662134644ae35f16
Eczbek <eczbek.void@gmail.com>
2022-08-30 20:03:27 -0400

    Add more methods to class 'gcufl::BigInt'

--- include/gcufl/BigInt.hpp
+++ include/gcufl/BigInt.hpp
@@ -1,24 +1,51 @@
 #pragma once
 
+#include <cmath>
 #include <concepts>
+#include <ostream>
 #include <vector>
 
 
 namespace gcufl {
 	struct BigInt {
-	public:
+	private:
 		std::vector<short> digits;
+		bool sign;
 
+	public:
 		template <std::integral N = int>
-		BigInt(N value = 0) noexcept {
-			while (value) {
+		BigInt(N value = 0) noexcept : sign(value >= 0) {
+			value = std::abs(value);
+			do {
 				digits.push_back(value % 10);
 				value /= 10;
-			}
+			} while (value);
 		}
 
 		BigInt(const gcufl::BigInt& other) noexcept;
 
+		bool operator==(const gcufl::BigInt& other) const noexcept;
+
+		bool operator!=(const gcufl::BigInt& other) const noexcept;
+
+		bool operator>(const gcufl::BigInt& other) const noexcept;
+		
+		bool operator>=(const gcufl::BigInt& other) const noexcept;
+
+		bool operator<(const gcufl::BigInt& other) const noexcept;
+
+		bool operator<=(const gcufl::BigInt& other) const noexcept;
+
 		gcufl::BigInt operator+(const gcufl::BigInt& other) const noexcept;
+
+		gcufl::BigInt operator-(const gcufl::BigInt& other) const noexcept;
+
+		friend std::ostream& operator<<(std::ostream& stream, const gcufl::BigInt& self) {
+			if (!self.sign)
+				stream << '-';
+			for (std::size_t i = self.digits.size(); i > 0; --i)
+				stream << self.digits[i - 1];
+			return stream;
+		}
 	};
 }
--- src/BigInt.cpp
+++ src/BigInt.cpp
@@ -1,21 +1,96 @@
-#include <cmath>
+#include <algorithm>
 #include <gcufl/BigInt.hpp>
 
 
-gcufl::BigInt::BigInt(const gcufl::BigInt& other) noexcept : digits(other.digits.begin(), other.digits.end()) {}
+gcufl::BigInt::BigInt(const gcufl::BigInt& other) noexcept : digits(other.digits.begin(), other.digits.end()), sign(other.sign) {}
+
+bool gcufl::BigInt::operator==(const gcufl::BigInt& other) const noexcept {
+	if (sign != other.sign || digits.size() != other.digits.size())
+		return false;
+	for (std::size_t i = 0; i < digits.size(); ++i)
+		if (digits[i] != other.digits[i])
+			return false;
+	return true;
+}
+
+bool gcufl::BigInt::operator!=(const gcufl::BigInt& other) const noexcept {
+	return !(*this == other);
+}
+
+bool gcufl::BigInt::operator>(const gcufl::BigInt& other) const noexcept {
+	if (sign != other.sign)
+		return sign;
+	if (sign && digits.size() > other.digits.size() || !sign && digits.size() < other.digits.size())
+		return true;
+	for (std::size_t i = 0; i < digits.size(); ++i)
+		if (digits[i] != other.digits[i])
+			return sign && digits[i] > other.digits[i] || !sign && digits[i] < other.digits[i];
+	return false;
+}
+
+bool gcufl::BigInt::operator>=(const gcufl::BigInt& other) const noexcept {
+	return *this > other || *this == other;
+}
+
+bool gcufl::BigInt::operator<(const gcufl::BigInt& other) const noexcept {
+	return !(*this >= other);
+}
+
+bool gcufl::BigInt::operator<=(const gcufl::BigInt& other) const noexcept {
+	return *this < other || *this == other;
+}
 
 gcufl::BigInt gcufl::BigInt::operator+(const gcufl::BigInt& other) const noexcept {
+	if (sign != other.sign) {
+		gcufl::BigInt copy(other);
+		copy.sign = sign;
+		return *this - copy;
+	}
 	gcufl::BigInt result;
+	result.digits = {};
+	result.sign = sign;
 	bool carry = false;
-	for (std::size_t i = 0; i < std::min(digits.size(), other.digits.size()) || carry; ++i) {
-		int sum = carry;
+	const std::size_t minSize = std::min(digits.size(), other.digits.size());
+	for (std::size_t i = 0; i < minSize || carry; ++i) {
+		short sum = carry;
 		if (i < digits.size())
 			sum += digits[i];
 		if (i < other.digits.size())
 			sum += other.digits[i];
 		carry = sum > 9;
-		sum -= 10 * carry;
-		result.digits.push_back(sum);
+		result.digits.push_back(sum % 10);
+	}
+	return result;
+}
+
+gcufl::BigInt gcufl::BigInt::operator-(const gcufl::BigInt& other) const noexcept {
+	if (sign != other.sign) {
+		gcufl::BigInt copy(other);
+		copy.sign = sign;
+		return *this + copy;
+	}
+	if (sign && *this < other || !sign && *this > other) {
+		gcufl::BigInt result(other - *this);
+		result.sign = !sign;
+		return result;
+	}
+	gcufl::BigInt result;
+	result.digits = {};
+	result.sign = sign;
+	bool borrow = false;
+	for (std::size_t i = 0; i < digits.size(); ++i) {
+		short difference = -borrow;
+		if (i < digits.size())
+			difference += digits[i];
+		if (i < other.digits.size())
+			difference -= other.digits[i];
+		borrow = difference < 0;
+		result.digits.push_back(std::abs(difference));
+	}
+	result.sign ^= borrow;
+	if (!result.digits.size()) {
+		result.digits = { 0 };
+		result.sign = true;
 	}
 	return result;
 }
