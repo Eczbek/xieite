1e0c052ef829f8b30cc6ae171aa3848885803b82
Eczbek <eczbek.void@gmail.com>
2022-09-05 02:30:49 -0400

    Create concepts 'gcufl::traits::Arithmetic' and 'gcufl::traits::Duration'

--- include/gcufl/traits.hpp
+++ include/gcufl/traits.hpp
@@ -1,14 +1,33 @@
 #pragma once
 
+#include <chrono>
 #include <concepts>
 #include <ostream>
+#include <type_traits>
 
 
 namespace gcufl {
 	namespace traits {
-		template <typename V>
-		concept Streamable = requires(std::ostream& stream, const V& value) {
+		template <typename T>
+		concept Streamable = requires(std::ostream& stream, const T& value) {
 			{ stream << value } -> std::convertible_to<std::ostream&>;
   		};
+
+		template <typename T>
+		concept Arithmetic = std::is_arithmetic_v<T>;
+
+		template <typename>
+		struct is_duration
+		: std::false_type {};
+
+		template <gcufl::traits::Arithmetic R, std::intmax_t N, std::intmax_t D>
+		struct is_duration<std::chrono::duration<R, std::ratio<N, D>>>
+		: std::true_type {};
+
+		template <typename T>
+		constexpr bool is_duration_v = gcufl::traits::is_duration<T>::value;
+
+		template <typename T>
+		concept Duration = gcufl::traits::is_duration_v<std::remove_cv_t<T>>;
 	}
 }
