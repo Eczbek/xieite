d8658988846bb70b80b91f29ac903aab4ccf7d47
Eczbek <eczbek.void@gmail.com>
2022-09-05 02:31:32 -0400

    Constrain template typenames of function 'gcufl::time::now'

--- include/gcufl/time.hpp
+++ include/gcufl/time.hpp
@@ -3,6 +3,7 @@
 #include <array>
 #include <chrono>
 #include <string_view>
+#include <gcufl/traits.hpp>
 
 
 namespace gcufl {
@@ -10,8 +11,8 @@ namespace gcufl {
 		constexpr std::array<std::string_view, 12> months { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
 		constexpr std::array<std::string_view, 7> weekDays { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
 
-		template <typename D = std::chrono::nanoseconds>
-		long now() noexcept {
+		template <gcufl::traits::Duration D = std::chrono::nanoseconds>
+		D::rep now() noexcept {
 			return std::chrono::duration_cast<D>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
 		}
 	}
