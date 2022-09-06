5dc210d06552cfea10d1e6468920d9155b4415b9
Eczbek <eczbek.void@gmail.com>
2022-09-06 15:12:14 -0400

    Scan source directory recursively in build system

--- build.sh
+++ build.sh
@@ -1,27 +1,27 @@
 #! /bin/bash
 
+HEADER_DIRECTORY="$PWD/include/"
 
 SOURCE_DIRECTORY="$PWD/src/"
 
-HEADER_DIRECTORY="$PWD/include/"
-
 DESTINATION=${1:-"$PWD/.a"}
 
+COMPILER_FLAGS="-c -std=c++20 -pthread"
 
-shopt -s nullglob
-SOURCE_FILES=($SOURCE_DIRECTORY/*)
-shopt -u nullglob
+
+SOURCE_FILES=()
+mapfile -d '' SOURCE_FILES < <(find $(realpath "$SOURCE_DIRECTORY") -type f -print0)
 SOURCE_FILES_COUNT=${#SOURCE_FILES[@]}
-TEMPORARY_DIRECTORY=$(mktemp -d)
+TEMPORARY_DIRECTORY="$(mktemp -d)"
 stty -echo
 for (( i=0 ; i<$SOURCE_FILES_COUNT ; i++ )); do
-	SOURCE_FILE=$(realpath ${SOURCE_FILES[i]})
-	printf "\033[2K  Compiling file $(( $i+1 ))/$SOURCE_FILES_COUNT: $SOURCE_FILE\r"
-	g++ $SOURCE_FILE -I $HEADER_DIRECTORY -o "$TEMPORARY_DIRECTORY/$(basename $SOURCE_FILE)" -c -std=c++20 -pthread
+	SOURCE_FILE_PATH="$(realpath ${SOURCE_FILES[i]})"
+	printf "\r\033[2KCompiling file $(( $i+1 ))/$SOURCE_FILES_COUNT: $SOURCE_FILE_PATH"
+	g++ "$SOURCE_FILE_PATH" -I "$HEADER_DIRECTORY" -o "$TEMPORARY_DIRECTORY/$(basename \"$(mktemp)\")" $COMPILER_FLAGS
 done
 read -N 9999999 -t 0.001
 stty echo
-printf "\033[2K  Archiving\r"
-ar crs $DESTINATION $(ls -d $TEMPORARY_DIRECTORY/*)
-rm -rf $TEMPORARY_DIRECTORY
-printf "\033[2K"
+printf "\r\033[2KArchiving"
+ar crs "$DESTINATION" $(ls -d $TEMPORARY_DIRECTORY/*)
+rm -rf "$TEMPORARY_DIRECTORY"
+printf "\r\033[2K"
