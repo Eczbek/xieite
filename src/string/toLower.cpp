0c3e8bc826307b14570a5f87d7bacecb44e1863e
Eczbek <eczbek.void@gmail.com>
2022-09-23 11:45:53 -0400

    Fix functions 'gcufl::string::toLower' and 'gcufl::string::toUpper'

--- src/string/toLower.cpp
+++ src/string/toLower.cpp
@@ -4,6 +4,7 @@
 
 
 std::string gcufl::string::toLower(std::string string) noexcept {
-	for (std::size_t i = 0; i < string; ++i)
-		string[i] = static_cast<char>(std::tolower(static_cast<unsigned char>(string[i])));
+	for (char& character : string)
+		character = static_cast<char>(std::tolower(static_cast<unsigned char>(character)));
+	return string;
 }
--- src/string/toUpper.cpp
+++ src/string/toUpper.cpp
@@ -4,6 +4,7 @@
 
 
 std::string gcufl::string::toUpper(std::string string) noexcept {
-	for (std::size_t i = 0; i < string; ++i)
-		string[i] = static_cast<char>(std::toupper(static_cast<unsigned char>(string[i])));
+	for (char& character : string)
+		character = static_cast<char>(std::toupper(static_cast<unsigned char>(character)));
+	return string;
 }
