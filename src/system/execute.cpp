d9e0fb7a5fe5179c1caa0fad189811a28cbbde47
Eczbek <eczbek.void@gmail.com>
2022-09-25 19:37:33 -0400

    Use 'gcufl::stream::read' in function 'gcufl::system::execute'

--- src/system/execute.cpp
+++ src/system/execute.cpp
@@ -1,19 +1,11 @@
 
-#include <array>
 #include <cstdio>
+#include <gcufl/stream/read.hpp>
 #include <gcufl/system/execute.hpp>
-#include <memory>
 #include <string>
 #include <string_view>
 
 
 std::string gcufl::system::execute(const std::string_view command) noexcept {
-	std::shared_ptr<FILE> pipe(popen(command.data(), "r"), pclose);
-	std::string result;
-	if (pipe) {
-		std::array<char, 1024> buffer;
-		while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
-			result += buffer.data();
-	}
-	return result;
+	return gcufl::stream::read(popen(command.data(), "r"));
 }
