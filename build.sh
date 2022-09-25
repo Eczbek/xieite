5f2d307bea85dc2268455d8a4d7df70379f6a45d
Eczbek <eczbek.void@gmail.com>
2022-09-25 19:24:33 -0400

    Replace 'i++' with '++i' in build system loop

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ SOURCE_FILES=()
 mapfile -d '' SOURCE_FILES < <(find $SOURCE_DIRECTORY -type f -print0)
 SOURCE_FILES_COUNT=${#SOURCE_FILES[@]}
 TEMPORARY_DIRECTORY=$(mktemp -d)
-for (( i=0 ; i<$SOURCE_FILES_COUNT ; i++ ))
+for (( i=0 ; i<$SOURCE_FILES_COUNT ; ++i ))
 do
 	g++ ${SOURCE_FILES[i]} -I $HEADER_DIRECTORY -o "$TEMPORARY_DIRECTORY/$(basename $(mktemp))" $COMPILER_FLAGS &
 done
