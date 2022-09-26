f5177d0cb531fa4b2ff276682dad968abed71391
Eczbek <eczbek.void@gmail.com>
2022-09-25 23:08:46 -0400

    Rename parameter 'callback' to 'selector' in function 'gcufl::algorithms::partialReverse'

--- include/gcufl/algorithms/partialReverse.hpp
+++ include/gcufl/algorithms/partialReverse.hpp
@@ -8,10 +8,10 @@
 
 namespace gcufl::algorithms {
 	template<std::forward_iterator I>
-	void partialReverse(I begin, const I end, const std::function<bool(const typename std::iterator_traits<I>::value_type)>& callback) noexcept {
+	void partialReverse(I begin, const I end, const std::function<bool(const typename std::iterator_traits<I>::value_type)>& selector) noexcept {
 		std::vector<I> iterators;
 		for (; begin != end; ++begin)
-			if (callback(*begin))
+			if (selector(*begin))
 				iterators.push_back(begin);
 		const std::size_t iteratorsSize = iterators.size();
 		for (std::size_t i = 0; i < iteratorsSize / 2; ++i)
