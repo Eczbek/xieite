b67671076e4f31930aea390af311537d5f69bd73
Eczbek <eczbek.void@gmail.com>
2022-09-26 03:19:39 -0400

    Remove functions 'gcufl::string::toLower' and 'gcufl::string::toUpper'

--- include/gcufl/string/toLower.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <string>
-
-
-namespace gcufl::string {
-	std::string toLower(std::string string) noexcept;
-}
--- include/gcufl/string/toUpper.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <string>
-
-
-namespace gcufl::string {
-	std::string toUpper(std::string string) noexcept;
-}
