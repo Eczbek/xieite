1e0637b4856527f5853f5f05570168d4cceb615f
Eczbek <eczbek.void@gmail.com>
2022-10-08 02:09:54 -0400

    Make class 'gcufl::BigInt' constexpr and store bits instead of digits

--- include/gcufl/BigInt.hpp
+++ include/gcufl/BigInt.hpp
@@ -1,201 +1,425 @@
 #pragma once
 
 #include <cmath>
+#include <compare>
 #include <concepts>
-#include <cstddef>
-#include <cstdint>
 #include <ostream>
-#include <span>
+#include <stdexcept>
 #include <vector>
 
 
 namespace gcufl {
 	class BigInt {
 	private:
-		std::vector<std::uint8_t> digits;
 		bool sign;
+		std::vector<bool> bits;
+
+		constexpr gcufl::BigInt& clean() noexcept {
+			while (bits.size() > 1 && !bits.back())
+				bits.pop_back();
+			if (bits.size() == 1 && !bits[0])
+				sign = false;
+			return *this;
+		}
 
 	public:
-		BigInt(const gcufl::BigInt& other) noexcept;
-
-		BigInt(std::span<const std::uint8_t> span, const bool sign = true) noexcept;
+		[[nodiscard]]
+		constexpr BigInt(const gcufl::BigInt& other) noexcept
+		: sign(other.sign), bits(other.bits) {}
 
 		template<std::signed_integral N = int>
-		BigInt(N value = 0) noexcept
-		: sign(value >= 0) {
+		[[nodiscard]]
+		constexpr BigInt(N value = 0) noexcept
+		: sign(value < 0) {
 			value = std::abs(value);
 			do {
-				digits.push_back(value % 10);
-				value /= 10;
+				bits.push_back(value % 2);
+				value /= 2;
 			} while (value);
 		}
 
 		template<std::unsigned_integral N>
-		BigInt(N value = 0) noexcept
-		: sign(true) {
+		[[nodiscard]]
+		constexpr BigInt(N value) noexcept
+		: sign(false) {
 			do {
-				digits.push_back(value % 10);
-				value /= 10;
+				bits.push_back(value % 2);
+				value /= 2;
 			} while (value);
 		}
 
-		gcufl::BigInt& operator=(const gcufl::BigInt& other) noexcept;
+		template<std::forward_iterator I>
+		requires(std::convertible_to<typename I::value_type, bool>)
+		[[nodiscard]]
+		constexpr BigInt(const I begin, const I end, const bool sign = false) noexcept
+		: sign(sign), bits(begin, end) {}
+
+		constexpr gcufl::BigInt& operator=(const gcufl::BigInt& other) noexcept {
+			sign = other.sign;
+			bits = other.bits;
+			return *this;
+		}
 
 		template<std::integral N>
-		gcufl::BigInt& operator=(const N value) noexcept {
+		constexpr gcufl::BigInt& operator=(const N value) noexcept {
 			return *this = gcufl::BigInt(value);
 		}
 
-		operator bool() const noexcept;
+		[[nodiscard]]
+		constexpr operator bool() const noexcept {
+			return *this != 0;
+		}
 
-		bool operator!() const noexcept;
+		[[nodiscard]]
+		constexpr bool operator!() const noexcept {
+			return !static_cast<bool>(*this);
+		}
 
-		bool operator==(const gcufl::BigInt& other) const noexcept;
+		[[nodiscard]]
+		constexpr bool operator==(const gcufl::BigInt& other) const noexcept {
+			return sign == other.sign && bits == other.bits;
+		}
 
 		template<std::integral N>
-		bool operator==(const N value) const noexcept {
+		[[nodiscard]]
+		constexpr bool operator==(const N value) const noexcept {
 			return *this == gcufl::BigInt(value);
 		}
 
-		bool operator!=(const gcufl::BigInt& other) const noexcept;
-
-		template<std::integral N>
-		bool operator!=(const N value) const noexcept {
-			return *this != gcufl::BigInt(value);
+		[[nodiscard]]
+		constexpr std::strong_ordering operator<=>(const gcufl::BigInt& other) const noexcept {
+			return (sign != other.sign)
+				? (other.sign <=> sign)
+				: sign
+					? (bits.size() != other.bits.size())
+						? (other.bits.size() <=> bits.size())
+						: (std::vector<bool>(other.bits.rbegin(), other.bits.rend()) <=> std::vector<bool>(bits.rbegin(), bits.rend()))
+					: (bits.size() != other.bits.size())
+						? (bits.size() <=> other.bits.size())
+						: (std::vector<bool>(bits.rbegin(), bits.rend()) <=> std::vector<bool>(other.bits.rbegin(), other.bits.rend()));
 		}
 
-		bool operator>(const gcufl::BigInt& other) const noexcept;
-
 		template<std::integral N>
-		bool operator>(const N value) const noexcept {
-			return *this > gcufl::BigInt(value);
+		[[nodiscard]]
+		constexpr std::strong_ordering operator<=>(const N value) const noexcept {
+			return *this <=> gcufl::BigInt(value);
 		}
 
-		bool operator>=(const gcufl::BigInt& other) const noexcept;
-
-		template<std::integral N>
-		bool operator>=(const N value) const noexcept {
-			return *this >= gcufl::BigInt(value);
+		[[nodiscard]]
+		constexpr gcufl::BigInt operator+() const noexcept {
+			return *this;
 		}
 
-		bool operator<(const gcufl::BigInt& other) const noexcept;
-
-		template<std::integral N>
-		bool operator<(const N value) const noexcept {
-			return *this < gcufl::BigInt(value);
+		[[nodiscard]]
+		constexpr gcufl::BigInt operator+(const gcufl::BigInt& other) const noexcept {
+			if (sign != other.sign)
+				return *this - (-other);
+			if (!*this)
+				return other;
+			if (!other)
+				return *this;
+			std::vector<bool> result;
+			bool carry = false;
+			const std::size_t bitsSize = bits.size();
+			const std::size_t otherBitsSize = other.bits.size();
+			for (std::size_t i = 0; i < bitsSize || i < otherBitsSize || carry; ++i) {
+				int sum = carry;
+				if (i < bitsSize)
+					sum += bits[i];
+				if (i < otherBitsSize)
+					sum += other.bits[i];
+				carry = sum > 1;
+				result.push_back(sum % 2);
+			}
+			return gcufl::BigInt(result.begin(), result.end(), sign);
 		}
 
-		bool operator<=(const gcufl::BigInt& other) const noexcept;
-
 		template<std::integral N>
-		bool operator<=(const N value) const noexcept {
-			return *this <= gcufl::BigInt(value);
-		}
-
-		gcufl::BigInt operator+() const noexcept;
-
-		gcufl::BigInt operator+(const gcufl::BigInt& other) const noexcept;
-
-		template<std::integral N>
-		gcufl::BigInt operator+(const N value) const noexcept {
+		[[nodiscard]]
+		constexpr gcufl::BigInt operator+(const N value) const noexcept {
 			return *this + gcufl::BigInt(value);
 		}
 
-		gcufl::BigInt& operator+=(const gcufl::BigInt& other) noexcept;
+		constexpr gcufl::BigInt& operator+=(const gcufl::BigInt& other) noexcept {
+			return *this = *this + other;
+		}
 
 		template<std::integral N>
-		gcufl::BigInt& operator+=(const N value) noexcept {
+		constexpr gcufl::BigInt& operator+=(const N value) noexcept {
 			return *this += gcufl::BigInt(value);
 		}
 
-		gcufl::BigInt& operator++() noexcept;
+		constexpr gcufl::BigInt& operator++() noexcept {
+			return *this += 1;
+		}
 
-		gcufl::BigInt operator++(int) noexcept;
+		constexpr gcufl::BigInt operator++(int) noexcept {
+			const gcufl::BigInt copy = *this;
+			++*this;
+			return copy;
+		}
 
-		gcufl::BigInt operator-() const noexcept;
+		[[nodiscard]]
+		constexpr gcufl::BigInt operator-() const noexcept {
+			gcufl::BigInt copy = *this;
+			if (copy)
+				copy.sign = !copy.sign;
+			return copy;
+		}
 
-		gcufl::BigInt operator-(const gcufl::BigInt& other) const noexcept;
+		[[nodiscard]]
+		constexpr gcufl::BigInt operator-(const gcufl::BigInt& other) const noexcept {
+			if (!other)
+				return *this;
+			if (sign != other.sign)
+				return *this + (-other);
+			if (sign && *this > other || !sign && *this < other)
+				return -(other - *this);
+			gcufl::BigInt result = *this;
+			bool borrow = false;
+			const std::size_t bitsSize = bits.size();
+			const std::size_t otherBitsSize = other.bits.size();
+			for (std::size_t i = 0; i < otherBitsSize || borrow; ++i) {
+				int difference = 2 - borrow + bits[i];
+				if (i < otherBitsSize)
+					difference -= other.bits[i];
+				borrow = i < bitsSize - 1 && difference < 2;
+				result.bits[i] = difference % 2;
+			}
+			result.sign = result.clean() && sign != borrow;
+			return result;
+		}
 
 		template<std::integral N>
-		gcufl::BigInt operator-(const N value) const noexcept {
+		[[nodiscard]]
+		constexpr gcufl::BigInt operator-(const N value) const noexcept {
 			return *this - gcufl::BigInt(value);
 		}
 
-		gcufl::BigInt& operator-=(const gcufl::BigInt& other) noexcept;
+		constexpr gcufl::BigInt& operator-=(const gcufl::BigInt& other) noexcept {
+			return *this = *this - other;
+		}
 
 		template<std::integral N>
-		gcufl::BigInt& operator-=(const N value) noexcept {
+		constexpr gcufl::BigInt& operator-=(const N value) noexcept {
 			return *this -= gcufl::BigInt(value);
 		}
 
-		gcufl::BigInt& operator--() noexcept;
+		constexpr gcufl::BigInt& operator--() noexcept {
+			return *this -= 1;
+		}
 
-		gcufl::BigInt operator--(int) noexcept;
+		constexpr gcufl::BigInt operator--(int) noexcept {
+			const gcufl::BigInt copy = *this;
+			--*this;
+			return copy;
+		}
 
-		gcufl::BigInt operator*(gcufl::BigInt other) const noexcept;
+		[[nodiscard]]
+		constexpr gcufl::BigInt operator*(const gcufl::BigInt& other) const noexcept {
+			if (*this == 1)
+				return other;
+			if (other == 1)
+				return *this;
+			if (!*this || !other)
+				return gcufl::BigInt(0);
+			if (*this == -1)
+				return -other;
+			if (other == -1)
+				return -*this;
+			gcufl::BigInt result;
+			std::vector<bool> prefixBits;
+			const std::size_t bitsSize = bits.size();
+			const std::size_t otherBitsSize = other.bits.size();
+			for (std::size_t i = 0; i < bitsSize; ++i) {
+				if (!bits[i])
+					continue;
+				for (std::size_t j = 0; j < otherBitsSize; ++j) {
+					if (!other.bits[j])
+						continue;
+					gcufl::BigInt sum = bits[i] * other.bits[j];
+					prefixBits.resize(i + j);
+					sum.bits.insert(sum.bits.begin(), prefixBits.begin(), prefixBits.end());
+					result += sum;
+				}
+			}
+			result.sign = sign != other.sign;
+			return result;
+		}
 
 		template<std::integral N>
-		gcufl::BigInt operator*(const N value) const noexcept {
+		[[nodiscard]]
+		constexpr gcufl::BigInt operator*(const N value) const noexcept {
 			return *this * gcufl::BigInt(value);
 		}
 
-		gcufl::BigInt& operator*=(const gcufl::BigInt& other) noexcept;
+		constexpr gcufl::BigInt& operator*=(const gcufl::BigInt& other) noexcept {
+			return *this = *this * other;
+		}
 
 		template<std::integral N>
-		gcufl::BigInt& operator*=(const N value) noexcept {
+		constexpr gcufl::BigInt& operator*=(const N value) noexcept {
 			return *this *= gcufl::BigInt(value);
 		}
 
-		gcufl::BigInt operator/(gcufl::BigInt other) const;
+		[[nodiscard]]
+		constexpr gcufl::BigInt operator/(gcufl::BigInt other) const {
+			if (other == 1)
+				return *this;
+			if (!other)
+				throw std::domain_error("Cannot divide by 0");
+			if (other == -1)
+				return -*this;
+			const bool otherSign = other.sign;
+			other.sign = false;
+			if (abs() < other)
+				return gcufl::BigInt(0);
+			std::vector<bool> result;
+			gcufl::BigInt difference;
+			for (std::size_t i = bits.size(); i--;) {
+				if (!difference)
+					difference.bits.clear();
+				difference.bits.insert(difference.bits.begin(), bits[i]);
+				bool quotient = difference >= other;
+				if (quotient)
+					difference -= other;
+				result.insert(result.begin(), quotient);
+			}
+			return gcufl::BigInt(result.begin(), result.end(), sign != otherSign).clean();
+		}
 
 		template<std::integral N>
-		gcufl::BigInt operator/(const N value) const {
+		[[nodiscard]]
+		constexpr gcufl::BigInt operator/(const N value) const {
 			return *this / gcufl::BigInt(value);
 		}
 
-		gcufl::BigInt& operator/=(const gcufl::BigInt& other);
+		constexpr gcufl::BigInt& operator/=(const gcufl::BigInt& other) {
+			return *this = *this / other;
+		}
 
 		template<std::integral N>
-		gcufl::BigInt& operator/=(const N value) {
+		constexpr gcufl::BigInt& operator/=(const N value) {
 			return *this /= gcufl::BigInt(value);
 		}
 
-		gcufl::BigInt operator%(gcufl::BigInt other) const;
+		[[nodiscard]]
+		constexpr gcufl::BigInt operator%(gcufl::BigInt other) const {
+			const bool otherSign = other.sign;
+			other.sign = false;
+			if (!other)
+				throw std::domain_error("Cannot divide by 0");
+			gcufl::BigInt result = abs();
+			if (result == 1 || result == other)
+				return gcufl::BigInt(0);
+			if (result < other)
+				return *this;
+			result.bits.clear();
+			for (std::size_t i = bits.size(); i--;) {
+				result.bits.insert(result.bits.begin(), bits[i]);
+				if (result >= other)
+					result -= other;
+			}
+			if (sign != otherSign)
+				++result;
+			if (result.clean())
+				result.sign = otherSign;
+			return result;
+		}
 
 		template<std::integral N>
-		gcufl::BigInt operator%(const N value) const {
+		[[nodiscard]]
+		constexpr gcufl::BigInt operator%(const N value) const {
 			return *this % gcufl::BigInt(value);
 		}
 
-		gcufl::BigInt& operator%=(const gcufl::BigInt& other);
+		constexpr gcufl::BigInt& operator%=(const gcufl::BigInt& other) {
+			return *this = *this % other;
+		}
 
 		template<std::integral N>
-		gcufl::BigInt& operator%=(const N value) {
+		constexpr gcufl::BigInt& operator%=(const N value) {
 			return *this %= gcufl::BigInt(value);
 		}
 
-		gcufl::BigInt operator~() const noexcept;
-
-		friend std::ostream& operator<<(std::ostream& stream, const gcufl::BigInt& self) {
-			if (!self.sign)
-				stream << '-';
-			for (std::size_t i = self.digits.size(); i--;)
-				stream << static_cast<int>(self.digits[i]);
-			return stream;
+		constexpr gcufl::BigInt operator~() const noexcept {
+			gcufl::BigInt result = *this + 1;
+			if (result)
+				result.sign = !sign;
+			return result;
 		}
 
-		gcufl::BigInt abs() const noexcept;
+		friend constexpr std::ostream& operator<<(std::ostream& outStream, const gcufl::BigInt& self) noexcept {
+			if (self.sign)
+				outStream << '-';
+			for (std::size_t i = self.bits.size(); i--;)
+				outStream << "01"[self.bits[i]];
+			return outStream;
+		}
 
-		gcufl::BigInt pow(gcufl::BigInt other) const;
+		[[nodiscard]]
+		constexpr gcufl::BigInt abs() const noexcept {
+			gcufl::BigInt copy = *this;
+			copy.sign = false;
+			return copy;
+		}
+
+		[[nodiscard]]
+		constexpr gcufl::BigInt pow(gcufl::BigInt other) const {
+			if (*this == 1 || other == 1)
+				return *this;
+			if (*this == -1)
+				return other % 2
+					? *this
+					: -*this;
+			if (!other)
+				return gcufl::BigInt(1);
+			if (other.sign) {
+				if (!*this)
+					throw std::domain_error("Cannot divide by 0");
+				return !other;
+			}
+			if (!*this)
+				return !other;
+			gcufl::BigInt x = *this;
+			gcufl::BigInt y = 1;
+			while (other > 1) {
+				if (other % 2) {
+					y *= x;
+					other = (other - 1) / 2;
+				} else
+					other /= 2;
+				x *= x;
+			}
+			return x * y;
+		}
 
 		template<std::integral N>
-		gcufl::BigInt pow(const N value) const {
+		[[nodiscard]]
+		constexpr gcufl::BigInt pow(const N value) const {
 			return pow(gcufl::BigInt(value));
 		}
 
-		gcufl::BigInt root(const gcufl::BigInt& other) const;
+		[[nodiscard]]
+		gcufl::BigInt root(const gcufl::BigInt& other) const {
+			if (sign)
+				throw std::domain_error("Cannot find root of negative");
+			if (*this == 1)
+				return gcufl::BigInt(1);
+			if (other.sign)
+				return gcufl::BigInt(0);
+			gcufl::BigInt x = other - 1;
+			gcufl::BigInt y = *this;
+			gcufl::BigInt z = y + 1;
+			while (y < z) {
+				z = y;
+				y = (x * y + *this / y.pow(x)) / other;
+			}
+			return z;
+		}
 
 		template<std::integral N>
+		[[nodiscard]]
 		gcufl::BigInt root(const N value) const {
 			return root(gcufl::BigInt(value));
 		}
