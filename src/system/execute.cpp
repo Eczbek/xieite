d49cd1af6473080efe1c7d7147de8fff829eabdc
Eczbek <eczbek.void@gmail.com>
2022-11-28 15:46:00 -0500

    Improve function 'xieite::system::execute'

--- src/system/execute.cpp
+++ src/system/execute.cpp
@@ -1,17 +1,20 @@
 #include <cstdio>
+#include <memory>
 #include <string>
 #include <string_view>
 #include <xieite/system/execute.hpp>
 
 std::string xieite::system::execute(const std::string_view command, const std::size_t bufferSize) noexcept {
-	std::FILE* file = popen(command.data(), "r");
+	decltype([](std::FILE* const file) {
+		pclose(file);
+	}) closer;
+	std::unique_ptr<std::FILE, decltype(closer)> pipe(popen(command.data(), "r"), closer);
 	std::string result;
 	std::size_t status;
 	do {
 		std::string buffer(bufferSize, '\0');
-		status = std::fread(buffer.data(), sizeof(char), bufferSize, file);
+		status = std::fread(buffer.data(), sizeof(char), bufferSize, pipe.get());
 		result += buffer;
 	} while (status);
-	pclose(file);
 	return result;
 }
