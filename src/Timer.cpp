206a77dea69f5a48381e4fb6675b21520a4e0188
Eczbek <eczbek.void@gmail.com>
2022-12-03 13:56:07 -0500

    Remove class xieite::Timer

--- src/Timer.cpp
+++
@@ -1,9 +0,0 @@
-#include <chrono>
-#include <xieite/Timer.hpp>
-
-xieite::Timer::Timer() noexcept
-: start(std::chrono::high_resolution_clock::now()) {}
-
-void xieite::Timer::reset() noexcept {
-	start = std::chrono::high_resolution_clock::now();
-}
