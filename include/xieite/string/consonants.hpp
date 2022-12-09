2992d7fac923c964c98841bb5386b82921d4ee62
Eczbek <eczbek.void@gmail.com>
2022-12-08 20:37:54 -0500

    Remove xieite::string::consonants and xieite::string::vowels

--- include/xieite/string/consonants.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <unordered_set>
-
-namespace xieite::string {
-	constexpr std::unordered_set<char> consonants { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' };
-}
--- include/xieite/string/vowels.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <unordered_set>
-
-namespace xieite::string {
-	constexpr std::unordered_set<char> vowels { 'a', 'e', 'i', 'o', 'u' };
-}
