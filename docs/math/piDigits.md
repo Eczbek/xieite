037cbd60c8724152933fef1016b3ca1b94a2b37f
Eczbek <eczbek.void@gmail.com>
2023-02-21 20:47:18 -0500

    Replace xieite::math::PiGenerator with xieite::math::piDigits

---
+++ docs/math/piDigits.md
@@ -0,0 +1,32 @@
+# xieite::math::piDigits
+Declared in `<xieite/math/piDigits.hpp>`
+
+<br/>
+
+Calculates digits of PI.
+
+<br/><br/>
+
+## Declaration
+```cpp
+template<std::integral N>
+std::vector<N> piDigits(const std::size_t digits) noexcept;
+```
+
+<br/><br/>
+
+## Example
+```cpp
+#include <iostream> // std::cout
+#include <xieite/math/piDigits.hpp>
+
+int main() {
+	for (const int digit : xieite::math::piDigits(50))
+		std::cout << digit;
+	std::cout << '\n';
+}
+```
+Output:
+```
+31415926535897932384626433832795028841971693993751
+```
--- include/xieite/math/piDigits.hpp
+++ include/xieite/math/piDigits.hpp
@@ -1,28 +1,25 @@
 #pragma once
 #include <concepts> // std::integral
+#include <vector> // std::vector
 #include <xieite/math/BigInteger.hpp>
 
 namespace xieite::math {
-	class PiGenerator {
-	private:
+	template<std::integral N>
+	std::vector<N> piDigits(const std::size_t digits) noexcept {
 		xieite::math::BigInteger a;
 		xieite::math::BigInteger b;
 		xieite::math::BigInteger c;
 		xieite::math::BigInteger d;
-
-	public:
-		constexpr PiGenerator() noexcept
-		: a(180), b(1), c(60), d(2) {}
-
-		template<std::integral N = int>
-		constexpr N next() noexcept {
+		std::vector<N> result;
+		while (digits--) {
 			const xieite::math::BigInteger e = (b * (d * 27 - 12) + a * 5) / c / 5;
 			const xieite::math::BigInteger f = (d * 3 + 1) * (d * 3 + 2) * 3;
 			a = (a + b * (d * 5 - 2) - c * e) * f * 10;
 			b *= d * (d * 2 - 1) * 10;
 			c *= f;
 			++d;
-			return static_cast<N>(e);
+			result.push_back(static_cast<N>(e));
 		}
-	};
+		return result;
+	}
 }
