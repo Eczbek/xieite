d19d46b85ec69d645ab8fb0756d8faff53197933
Eczbek <eczbek.void@gmail.com>
2023-03-23 16:13:21 -0400

    Remove aggregator headers

--- include/xieite/consoleCursor.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <xieite/console/CursorPosition.hpp>
-#include <xieite/console/cursorControls.hpp>
-#include <xieite/console/getCursorPosition.hpp>
-#include <xieite/console/getWindowSize.hpp>
-#include <xieite/console/moveCursor.hpp>
-#include <xieite/console/setCursorPosition.hpp>
--- include/xieite/identificationMacros.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <xieite/macros/ARCHITECTURE_TYPE.hpp>
-#include <xieite/macros/COMPILER_TYPE.hpp>
-#include <xieite/macros/COMPILER_VERSION.hpp>
-#include <xieite/macros/PLATFORM_TYPE.hpp>
