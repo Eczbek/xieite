1c6855d25245e389a5962dd77b376edf450ee98b
Eczbek <eczbek.void@gmail.com>
2023-03-31 20:18:43 -0400

    Rename namespace xieite::console::cursorControls to xieite::console::cursor

--- include/xieite/console/RawLock.hpp
+++ include/xieite/console/RawLock.hpp
@@ -6,19 +6,17 @@
 namespace xieite::console {
 	class RawLock {
 	private:
-		static bool locked = false;
-		static termios cookedMode;
+		bool locked;
+		termios cookedMode;
 
 	public:
-		RawLock(const bool echo = true) noexcept {
-			if (!this->locked) {
-				this->locked = true;
-				tcgetattr(STDIN_FILENO, &this->cookedMode);
-				termios rawMode = this->cookedMode;
-				cfmakeraw(&rawMode);
-				rawMode.c_lflag &= ~(ICANON | (ECHO * echo));
-				tcsetattr(STDIN_FILENO, TCSANOW, &rawMode);
-			}
+		RawLock(const bool echo = true) noexcept
+		: locked(true) {
+			tcgetattr(STDIN_FILENO, &this->cookedMode);
+			termios rawMode = this->cookedMode;
+			cfmakeraw(&rawMode);
+			rawMode.c_lflag &= ~(ICANON | (ECHO * echo));
+			tcsetattr(STDIN_FILENO, TCSANOW, &rawMode);
 		}
 
 		~RawLock() {
