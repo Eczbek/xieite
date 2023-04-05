dbac0651cde9fe5eac57646cf8ad02cc6635c425
Eczbek <eczbek.void@gmail.com>
2023-04-05 10:21:47 -0400

    Create xieite::algorithms::numberSearch

---
+++ include/xieite/algorithms/numberSearch.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <utility>
+#include <xieite/concepts/Arithmetic.hpp>
+#include <xieite/concepts/SelectorCallback.hpp>
+#include <xieite/math/approximatelyEqual.hpp>
+
+namespace xieite::algorithms {
+	template<xieite::concepts::Arithmetic Number>
+	[[nodiscard]]
+	constexpr Number numberSearch(xieite::concepts::SelectorCallback<Number> auto&& selector, Number minimum, Number maximum) noexcept {
+		while (true) {
+			const Number middle = (minimum + maximum) / 2;
+			if (xieite::math::approximatelyEqual(middle, minimum) || xieite::math::approximatelyEqual(middle, maximum))
+				return middle;
+			(selector(middle) ? maximum : minimum) = middle;
+		}
+	}
+
+	template<xieite::concepts::Arithmetic Number, xieite::concepts::SelectorCallback<Number> Callback>
+	[[nodiscard]]
+	constexpr Number numberSearch(Callback&& selector) noexcept {
+		Number minimum = -1;
+		Number maximum = 1;
+		if (selector(0)) {
+			maximum = 0;
+			while (selector(minimum))
+				minimum -= absolute(minimum);
+		} else {
+			minimum = 0;
+			while (!selector(maximum))
+				maximum += absolute(maximum);
+		}
+		return numberSearch(std::forward<Callback>(selector), minimum, maximum);
+	}
+}
