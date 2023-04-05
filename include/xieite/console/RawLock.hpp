25fcf1b30d65ad042d9dea2b7c23f98513588879
Eczbek <eczbek.void@gmail.com>
2023-04-05 10:22:17 -0400

    Create more methods in xieite::console::RawLock

--- include/xieite/console/RawLock.hpp
+++ include/xieite/console/RawLock.hpp
@@ -8,19 +8,40 @@ namespace xieite::console {
 	private:
 		bool locked;
 		termios cookedMode;
+		termios rawMode;
 
 	public:
-		RawLock(const bool echo = true) noexcept
+		RawLock() noexcept
 		: locked(true) {
 			tcgetattr(STDIN_FILENO, &this->cookedMode);
-			termios rawMode = this->cookedMode;
-			cfmakeraw(&rawMode);
-			rawMode.c_lflag &= ~(ICANON | (ECHO * echo));
+			this->rawMode = this->cookedMode;
+			cfmakeraw(&this->rawMode);
 			tcsetattr(STDIN_FILENO, TCSANOW, &rawMode);
 		}
 
 		~RawLock() {
 			this->unlock();
+		}
+
+		void echo(const bool value) noexcept {
+			this->rawMode.c_lflag |= (ECHO * value);
+			tcsetattr(STDIN_FILENO, TCSANOW, &this->rawMode);
+		}
+
+		void canonical(const bool value) noexcept {
+			this->rawMode.c_lflag |= (ICANON * value);
+			tcsetattr(STDIN_FILENO, TCSANOW, &this->rawMode);
+		}
+
+		void signals(const bool value) noexcept {
+			this->rawMode.c_iflag &= ~((ICRNL * value) | (IXON * value));
+			this->rawMode.c_lflag &= ~((IEXTEN * value) | (ISIG * value));
+			tcsetattr(STDIN_FILENO, TCSANOW, &this->rawMode);
+		}
+
+		void processing(const bool value) noexcept {
+			this->rawMode.c_oflag |= (OPOST * value);
+			tcsetattr(STDIN_FILENO, TCSANOW, &this->rawMode);
 		}
 
 		void unlock() noexcept {
