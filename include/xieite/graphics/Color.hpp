7751499518528ee766255ea63c91a40c74a8d7a6
Eczbek <eczbek.void@gmail.com>
2023-04-21 18:26:01 -0400

    Use numeric limits of uint8_t in Color

--- include/xieite/graphics/Color.hpp
+++ include/xieite/graphics/Color.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <limits>
 #include <cstdint>
 
 namespace xieite::graphics {
@@ -9,18 +10,18 @@ namespace xieite::graphics {
 		std::uint8_t blue;
 		std::uint8_t alpha;
 
-		constexpr Color(const std::uint8_t red, const std::uint8_t green, const std::uint8_t blue, const std::uint8_t alpha = 255) noexcept
+		constexpr Color(const std::uint8_t red, const std::uint8_t green, const std::uint8_t blue, const std::uint8_t alpha = std::numeric_limits<std::uint8_t>::max()) noexcept
 		: red(red), green(green), blue(blue), alpha(alpha) {}
 
 		constexpr Color(const std::uint32_t value) noexcept
-		: red(value >> 24), green(value >> 16 & 255), blue(value >> 8 & 255), alpha(value & 255) {}
+		: red(value >> (3 * std::numeric_limits<std::uint8_t>::digits)), green(value >> (2 * std::numeric_limits<std::uint8_t>::digits) & std::numeric_limits<std::uint8_t>::max()), blue(value >> std::numeric_limits<std::uint8_t>::digits & std::numeric_limits<std::uint8_t>::max()), alpha(value & std::numeric_limits<std::uint8_t>::max()) {}
 
 		constexpr bool operator==(const xieite::graphics::Color& color) const noexcept {
 			return (this->red == color.red) && (this->green == color.green) && (this->blue == color.blue) && (this->alpha == color.alpha);
 		}
 
 		constexpr std::uint32_t value() noexcept {
-			return (this->red << 24) + (this->green << 16) + (this->blue << 8) + this->alpha;
+			return (this->red << (3 * std::numeric_limits<std::uint8_t>::digits)) + (this->green << (2 * std::numeric_limits<std::uint8_t>::digits)) + (this->blue << std::numeric_limits<std::uint8_t>::digits) + this->alpha;
 		}
 	};
 }
