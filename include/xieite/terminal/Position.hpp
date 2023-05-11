a8abea642c67c479831d507d32b8fc9099d0d1dc
Eczbek <eczbek.void@gmail.com>
2023-05-11 17:44:57 -0400

    Create xieite::terminal::ModeLock::isLocked

--- include/xieite/terminal/Position.hpp
+++ include/xieite/terminal/Position.hpp
@@ -5,9 +5,6 @@ namespace xieite::terminal {
 		int row;
 		int column;
 
-		constexpr Position() noexcept
-		: row(0), column(0) {}
-
 		constexpr Position(const int row, const int column) noexcept
 		: row(row), column(column) {}
 	};
