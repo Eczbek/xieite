14af65ad35e06aa6a91e9ae681866bd3b510a5ab
Eczbek <eczbek.void@gmail.com>
2023-05-23 20:08:38 -0400

    Create xieite::types::List::Data

---
+++ docs/types/List/Data.md
@@ -0,0 +1,18 @@
+# [`xieite`](../../../README.md)`::`[`types`](../../../docs/types.md)`::`[`List<Types...>`](../../../docs/types/List.md)`::Data`
+Defined in header [`<xieite/types/List.hpp>`](../../../include/xieite/types/List.hpp)
+
+<br/>
+
+Creates a `std::tuple` of `Types...`.
+
+<br/><br/>
+
+## Synopsis
+
+<br/>
+
+```cpp
+using Data = std::tuple<Types...>;
+```
+### Resulting type
+- A `std::tuple` of `Types...`
--- include/xieite/types/List.hpp
+++ include/xieite/types/List.hpp
@@ -2,11 +2,14 @@
 #	define XIEITE_HEADER_TYPES_LIST
 
 #	include <concepts>
+#	include <tuple>
 #	include <type_traits>
 
 namespace xieite::types {
 	template<typename... Types>
 	struct List final {
+		using Data = std::tuple<Types...>;
+
 		template<typename Type>
 		constexpr std::conditional_t<(std::same_as<Types, Type> || ...), xieite::types::List<Types...>, xieite::types::List<Types..., Type>> operator->*(xieite::types::List<Type>);
 	};
