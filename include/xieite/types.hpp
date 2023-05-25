98ae4dbd0c274c14158207af451cdbf9df1dbe17
Eczbek <eczbek.void@gmail.com>
2023-05-25 18:49:55 -0400

    Create xieite::types::count

--- include/xieite/types.hpp
+++ include/xieite/types.hpp
@@ -4,6 +4,7 @@
 #	include <xieite/types/Anything.hpp>
 #	include <xieite/types/List.hpp>
 #	include <xieite/types/Unique.hpp>
+#	include <xieite/types/count.hpp>
 #	include <xieite/types/demangle.hpp>
 
 #endif
