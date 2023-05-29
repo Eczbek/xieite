5a7f22912c2cc1ac97d65d4433bec60117d4f128
Eczbek <eczbek.void@gmail.com>
2023-05-29 13:07:44 -0400

    Fix calls to xieite::math::baseTo

--- include/xieite/terminal/setCursorPosition.hpp
+++ include/xieite/terminal/setCursorPosition.hpp
@@ -7,7 +7,7 @@
 
 namespace xieite::terminal {
 	constexpr std::string setCursorPosition(const xieite::terminal::Position position) noexcept {
-		return "\u001b[" + xieite::math::baseTo(position.row + 1, 10) + ';' + xieite::math::baseTo(position.column + 1, 10) + 'H';
+		return "\u001b[" + xieite::math::baseTo(10, position.row + 1) + ';' + xieite::math::baseTo(10, position.column + 1) + 'H';
 	}
 }
 
