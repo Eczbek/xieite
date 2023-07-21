875c465e789fcede77531d808477cfeae8f487e9
Eczbek <eczbek.void@gmail.com>
2023-07-21 12:05:00 -0400

    Improve xieite::math::logarithm

--- include/xieite/math/logarithm.hpp
+++ include/xieite/math/logarithm.hpp
@@ -9,7 +9,7 @@ namespace xieite::math {
 	template<xieite::concepts::Arithmetic Number1, xieite::concepts::Arithmetic Number2>
 	[[nodiscard]]
 	constexpr xieite::math::Result<Number1, Number2> logarithm(const Number1 base, const Number2 value) noexcept {
-		return std::log2(value) / std::log2(base);
+		return std::log(value) / std::log(base);
 	}
 }
 
