e7a8c95ad3f5b01d97976223d7bdfdb79272de5d
Eczbek <eczbek.void@gmail.com>
2023-07-24 12:24:01 -0400

    Improve xieite::math::Result

--- include/xieite/math/Result.hpp
+++ include/xieite/math/Result.hpp
@@ -1,13 +1,13 @@
 #ifndef XIEITE_HEADER_MATH_RESULT
 #	define XIEITE_HEADER_MATH_RESULT
 
-#	include <concepts>
 #	include <type_traits>
 #	include <xieite/concepts/Arithmetic.hpp>
+#	include <xieite/concepts/SameAs.hpp>
 
 namespace xieite::math {
 	template<xieite::concepts::Arithmetic... Arithmetics>
-	using Result = std::conditional_t<(std::same_as<Arithmetics, long double> || ...), long double, double>;
+	using Result = std::conditional_t<xieite::concepts::SameAs<long double, Arithmetics...>, long double, double>;
 }
 
 #endif
