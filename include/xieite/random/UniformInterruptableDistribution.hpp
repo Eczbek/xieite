177c263afa027be264400c9d3897a9f6623bd7c5
Eczbek <eczbek.void@gmail.com>
2023-07-24 12:18:14 -0400

    Fix logic in xieite::random::UniformInterruptableDistribution

--- include/xieite/random/UniformInterruptableDistribution.hpp
+++ include/xieite/random/UniformInterruptableDistribution.hpp
@@ -21,23 +21,23 @@ namespace xieite::random {
 	public:
 		template<xieite::concepts::RangeOf<xieite::math::Interval<Arithmetic>> IntervalRange>
 		UniformInterruptableDistribution(const xieite::math::Interval<Arithmetic> interval, const IntervalRange& interruptions) {
-			const Arithmetic lower = std::min(interval.start, interval.end);
-			const Arithmetic upper = std::max(interval.start, interval.end);
-			Arithmetic maximum = upper;
+			const Arithmetic minimum = std::min(interval.start, interval.end);
+			const Arithmetic maximum = std::max(interval.start, interval.end);
+			Arithmetic upper = maximum;
 			for (const xieite::math::Interval<Arithmetic> interruption : xieite::math::mergeIntervals<Arithmetic>(interruptions)) {
-				const Arithmetic start = std::clamp(interruption.start, lower, upper);
-				const Arithmetic end = std::clamp(interruption.end, lower, upper);
-				if (((start >= lower) && (start <= upper)) || ((end >= lower) && (end <= upper))) {
+				if (((interruption.start >= minimum) || (interruption.end >= minimum)) && ((interruption.start <= maximum) || (interruption.end <= maximum))) {
+					const Arithmetic start = std::clamp(interruption.start, minimum, maximum);
+					const Arithmetic end = std::clamp(interruption.end, minimum, maximum);
 					const Arithmetic difference = xieite::math::difference(start, end);
-					if (maximum > (lower + difference)) {
-						maximum -= (difference + std::integral<Arithmetic>);
+					if (upper > (minimum + difference)) {
+						upper -= (difference + std::integral<Arithmetic>);
 						this->interruptions.push_back(xieite::math::Interval<Arithmetic>(std::min(start, end), difference));
 					} else {
 						throw std::range_error("Cannot exclude entire interval");
 					}
 				}
 			}
-			this->distribution = xieite::random::UniformInterruptableDistribution<Arithmetic>::UniformDistribution(lower, maximum);
+			this->distribution = xieite::random::UniformInterruptableDistribution<Arithmetic>::UniformDistribution(minimum, upper);
 			std::ranges::sort(this->interruptions.begin(), this->interruptions.end(), [](const xieite::math::Interval<Arithmetic> interruption1, const xieite::math::Interval<Arithmetic> interruption2) noexcept -> Arithmetic {
 				return interruption1.start < interruption2.start;
 			});
