61bb933b12cfc76cf2e8eb7e86001fda82d3927a
Eczbek <eczbek.void@gmail.com>
2023-07-26 20:10:33 -0400

    Improve xieite::math::robert

--- include/xieite/math/robert.hpp
+++ include/xieite/math/robert.hpp
@@ -6,8 +6,8 @@
 #	include <numbers>
 
 namespace xieite::math {
-	template<std::integral Integral>
-	inline constexpr Integral robert = std::numeric_limits<Integral>::max() / std::numbers::phi_v<double>;
+	template<std::integral Integral, std::floating_point FloatingPoint = double>
+	inline constexpr Integral robert = std::numeric_limits<Integral>::max() / std::numbers::phi_v<FloatingPoint>;
 }
 
 // Thanks to melak47 for naming this constant
