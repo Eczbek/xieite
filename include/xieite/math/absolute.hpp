6760428df2cf18ad163131d2ac8725b7b107df0c
Eczbek <eczbek.void@gmail.com>
2023-08-01 23:13:59 -0400

    Improve xieite::math::absolute

--- include/xieite/math/absolute.hpp
+++ include/xieite/math/absolute.hpp
@@ -1,13 +1,22 @@
 #ifndef XIEITE_HEADER_MATH_ABSOLUTE
 #	define XIEITE_HEADER_MATH_ABSOLUTE
 
-#	include "../concepts/Arithmetic.hpp"
+#	include <cmath>
+#	include <concepts>
+#	include <limits>
+#	include <type_traits>
 
 namespace xieite::math {
-	template<xieite::concepts::Arithmetic Arithmetic>
+	template<std::integral Integral>
 	[[nodiscard]]
-	constexpr Arithmetic absolute(const Arithmetic value) noexcept {
-		return (value * ((value >= 0) * 2 - 1));
+	constexpr std::make_unsigned_t<Integral> absolute(const Integral value) noexcept {
+		return ((value < 0) ? (std::numeric_limits<std::make_unsigned_t<Integral>>::max() - value + 1) : value);
+	}
+
+	template<std::floating_point FloatingPoint>
+	[[nodiscard]]
+	constexpr FloatingPoint absolute(const FloatingPoint value) noexcept {
+		return std::abs(value);
 	}
 }
 
