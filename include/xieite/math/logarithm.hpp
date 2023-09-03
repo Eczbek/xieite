aceb713cecc0a422276e05d861d22da44a68f482
Eczbek <eczbek.void@gmail.com>
2023-09-02 22:15:31 -0400

    Fix `xieite::math::logarithm`

--- include/xieite/math/logarithm.hpp
+++ include/xieite/math/logarithm.hpp
@@ -2,12 +2,12 @@
 #	define XIEITE_HEADER__MATH__LOGARITHM
 
 #	include <cmath>
-#	include "../concepts/Numeric.hpp"
+#	include "../concepts/Arithmetic.hpp"
 #	include "../math/Result.hpp"
 
 namespace xieite::math {
-	template<xieite::concepts::Numeric Number>
-	constexpr xieite::math::Result<Number> logarithm(const Number base, const Number value) noexcept {
+	template<xieite::concepts::Arithmetic Arithmetic1, xieite::concepts::Arithmetic Arithmetic2>
+	constexpr xieite::math::Result<Arithmetic1, Arithmetic2> logarithm(const Arithmetic1 base, const Arithmetic2 value) noexcept {
 		return std::log(value) / std::log(base);
 	}
 }
