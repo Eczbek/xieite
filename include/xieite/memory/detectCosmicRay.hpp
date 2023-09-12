e259e6f7e055afc39cd2ceff377d52afb2d40a13
Eczbek <eczbek.void@gmail.com>
2023-09-11 23:32:19 -0400

    Unmark `xieite::memory::detectCosmicRay` as `noexcept`

--- include/xieite/memory/detectCosmicRay.hpp
+++ include/xieite/memory/detectCosmicRay.hpp
@@ -7,7 +7,7 @@
 
 namespace xieite::memory {
 	template<std::unsigned_integral Chunk = std::uintmax_t>
-	constexpr void detectCosmicRay(const std::size_t bytes) noexcept {
+	constexpr void detectCosmicRay(const std::size_t bytes) {
 		const std::size_t size = bytes / sizeof(Chunk);
 		const auto detector = std::make_unique<volatile const Chunk[]>(size);
 		for (std::size_t i = 0; !detector[i]; i = (i + 1) % size);
