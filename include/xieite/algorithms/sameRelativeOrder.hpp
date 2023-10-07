13ff0bb408536cd1f7cf80dbdb26c21865784472
Eczbek <eczbek.void@gmail.com>
2023-10-07 12:29:54 -0400

    Improve `xieite::algorithms::sameRelativeOrder`

--- include/xieite/algorithms/sameRelativeOrder.hpp
+++ include/xieite/algorithms/sameRelativeOrder.hpp
@@ -1,7 +1,8 @@
 #ifndef XIEITE_HEADER__ALGORITHMS__SAME_RELATIVE_ORDER
 #	define XIEITE_HEADER__ALGORITHMS__SAME_RELATIVE_ORDER
 
-#	include <concepts>
+#	include <algorithm>
+#	include <cstddef>
 #	include <functional>
 #	include <iterator>
 #	include <ranges>
@@ -9,28 +10,14 @@
 
 namespace xieite::algorithms {
 	template<std::ranges::range Range1, std::ranges::range Range2, xieite::concepts::Functable<bool(std::ranges::range_value_t<Range1>, std::ranges::range_value_t<Range2>)> Callback = std::ranges::equal_to>
-	[[nodiscard]] constexpr bool sameRelativeOrder(const Range1& range1, const Range2& range2, const Callback& comparator = Callback()) {
-		std::ranges::const_iterator_t<const Range1&> begin1 = std::ranges::begin(range1);
-		std::ranges::const_iterator_t<const Range1&> end1 = std::ranges::end(range1);
-		std::ranges::const_iterator_t<const Range2&> begin2 = std::ranges::begin(range2);
-		std::ranges::const_iterator_t<const Range2&> end2 = std::ranges::end(range2);
-		if (std::ranges::distance(begin1, end1) == std::ranges::distance(begin2, end2)) {
-			for (std::ranges::const_iterator_t<const Range1&> i = begin1; i != end1; ++i) {
-				std::ranges::const_iterator_t<const Range1&> copy1 = i;
-				std::ranges::const_iterator_t<const Range1&> copy2 = begin2;
-				while (copy2 != end2) {
-					if (copy1 == end1) {
-						copy1 = begin1;
-					}
-					if (!comparator(*copy1, *copy2)) {
-						break;
-					}
-					++copy1;
-					++copy2;
-				}
-				if (copy2 == end2) {
+	[[nodiscard]] constexpr bool sameRelativeOrder(const Range1& range1, Range2 range2, const Callback& comparator = Callback()) {
+		const std::size_t rangeSize = std::ranges::size(range1);
+		if (rangeSize == std::ranges::size(range2)) {
+			for (std::size_t i = 0; i < rangeSize; ++i) {
+				if (std::ranges::equal(range1, range2)) {
 					return true;
 				}
+				std::ranges::rotate(range2, std::ranges::next(std::ranges::begin(range2)));
 			}
 		}
 		return false;
