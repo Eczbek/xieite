457144cf9a90fa83aa4e3909b2a73947e96344ba
Eczbek <eczbek.void@gmail.com>
2023-11-27 12:17:42 -0500

    Improve `xieite::strings::isDigit`

--- include/xieite/strings/is_digit.hpp
+++ include/xieite/strings/is_digit.hpp
@@ -5,7 +5,7 @@
 
 namespace xieite::strings {
 	[[nodiscard]] constexpr bool isDigit(const char character) noexcept {
-		return std::string_view("0123456789").contains(character);
+		return (character >= '0') && (character <= '9');
 	}
 }
 
