03faeae29ac2fb54969e7d0b96a1e751ca73d378
Eczbek <eczbek.void@gmail.com>
2023-12-13 12:57:19 -0500

    Add a comment

--- include/xieite/math/digits.hpp
+++ include/xieite/math/digits.hpp
@@ -11,6 +11,8 @@ namespace xieite::math {
 	template<std::integral Integer>
 	[[nodiscard]] constexpr std::size_t digits(const Integer value, const std::size_t base = 10) noexcept {
 		return !base ? 0 : ((base == 1) ? value : static_cast<std::size_t>(std::ceil(xieite::math::logarithm(base, xieite::math::absolute(value) + !value + 1))));
+
+		// std::floor(xieite::math::logarithm(base, xieite::math::absolute(value) + !value)) + 1
 	}
 }
 
