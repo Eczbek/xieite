c7daf469594d424746065eba3024ead5e75de486
Eczbek <eczbek.void@gmail.com>
2024-01-04 13:54:34 -0500

    Fix `xieite::strings::quoted`

--- include/xieite/strings/quoted.hpp
+++ include/xieite/strings/quoted.hpp
@@ -7,7 +7,7 @@
 
 namespace xieite::strings {
 	[[nodiscard]] constexpr std::string quoted(const std::string_view value, const char delimiter = '"', const char escape = '\\') noexcept {
-		return delimiter + xieite::strings::replace(xieite::strings::replace(value, escape, std::string(2, escape)), delimiter, std::string(1, escape) + delimiter) + delimiter;
+		return delimiter + xieite::strings::replace((delimiter == escape) ? value : xieite::strings::replace(value, escape, std::string(2, escape)), delimiter, std::string(1, escape) + delimiter) + delimiter;
 	}
 }
 
