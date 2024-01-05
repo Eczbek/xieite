30445ddb1ccfea438686e27e2e05ced918f33109
Eczbek <eczbek.void@gmail.com>
2024-01-04 19:52:28 -0500

    Fix `xieite::math::almostEqual`

--- include/xieite/math/almost_equal.hpp
+++ include/xieite/math/almost_equal.hpp
@@ -1,15 +1,19 @@
 #ifndef XIEITE_HEADER_MATH_ALMOST_EQUAL
 #	define XIEITE_HEADER_MATH_ALMOST_EQUAL
 
+#	include <cmath>
+#	include <concepts>
 #	include <limits>
 #	include <type_traits>
 #	include "../concepts/arithmetic.hpp"
-#	include "../math/absolute.hpp"
 
 namespace xieite::math {
 	template<xieite::concepts::Arithmetic Number1, xieite::concepts::Arithmetic Number2>
 	[[nodiscard]] constexpr bool almostEqual(const Number1 value1, const Number2 value2) noexcept {
-		return xieite::math::absolute(value1 - value2) <= std::numeric_limits<std::common_type_t<Number1, Number2>>::epsilon();
+		if constexpr (std::integral<std::common_type_t<Number1, Number2>>) {
+			return value1 == value2;
+		}
+		return std::abs(value1 - value2) <= std::numeric_limits<std::common_type_t<Number1, Number2>>::epsilon();
 	}
 }
 
