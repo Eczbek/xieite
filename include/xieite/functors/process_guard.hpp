1e2c97ce13596aabc373ee056929f3b1535bf5ab
Eczbek <eczbek.void@gmail.com>
2024-01-12 21:47:23 -0500

    Handle function pointers in constructor of `xieite::functors::ProcessGuard`

--- include/xieite/functors/process_guard.hpp
+++ include/xieite/functors/process_guard.hpp
@@ -2,23 +2,36 @@
 #	define XIEITE_HEADER_FUNCTORS_PROCESS_GUARD
 
 #	include <concepts>
+#	include <cstdint>
 #	include <functor>
 #	include <memory>
+#	include <type_traits>
+#	include <unordered_map>
 
 namespace xieite::functors {
 	class ProcessGuard {
 	public:
 		template<std::invocable<> Functor>
-		ProcessGuard(const Functor callback) noexcept
+		ProcessGuard(Functor&& callback) noexcept
 		: released(std::make_shared<bool>(false)) {
-			static const std::shared_ptr<bool> released = this->released;
-			static const struct Lock final {
+			struct Lock {
+				const std::shared_ptr<bool> released;
+
+				Lock(const std::shared_ptr<bool> released) noexcept
+				: released(released) {}
+
 				~Lock() {
-					if (!*released) {
+					if (!*this->released) {
 						std::invoke(callback);
 					}
 				}
-			} lock;
+			};
+			if constexpr (std::is_pointer_v<Functor>) {
+				static std::unordered_map<std::uintptr_t, Lock> locks;
+				locks[reinterpret_cast<std::uintptr_t>(callback)] = Lock(this->released);
+			} else {
+				static Lock lock = Lock(this->released);
+			}
 		}
 
 		void release() noexcept {
