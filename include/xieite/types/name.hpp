39e322116da06f18791fa14576d0387ece02e6b2
Eczbek <eczbek.void@gmail.com>
2024-01-16 13:47:16 -0500

    Replace usage of `std::source_location`

--- include/xieite/types/name.hpp
+++ include/xieite/types/name.hpp
@@ -1,16 +1,43 @@
 #ifndef XIEITE_HEADER_TYPES_NAME
 #	define XIEITE_HEADER_TYPES_NAME
 
-#	include <source_location>
 #	include <string_view>
-#	include "../strings/between.hpp"
+#	include "../macros/compiler.hpp"
+
+#	if XIEITE_COMPILER_GCC
+#		include "../strings/between.hpp"
 
 namespace XIEITE_INTERNAL_TYPES_NAME {
 	template<typename>
 	[[nodiscard]] constexpr std::string_view name() noexcept {
-		return xieite::strings::between(std::source_location::current().function_name(), "= ", ";");
+		return xieite::strings::between(__PRETTY_FUNCTION__, "= ", ';');
 	}
 }
+
+#	elif XIEITE_COMPILER_LLVM
+#		include "../strings/between.hpp"
+
+namespace XIEITE_INTERNAL_TYPES_NAME {
+	template<typename>
+	[[nodiscard]] constexpr std::string_view name() noexcept {
+		return xieite::strings::between(__PRETTY_FUNCTION__, "= ", ']');
+	}
+}
+
+#	elif XIEITE_COMPILER_MSVC
+#		include "../strings/after.hpp"
+#		include "../strings/between.hpp"
+
+namespace XIEITE_INTERNAL_TYPES_NAME {
+	template<typename>
+	[[nodiscard]] constexpr std::string_view name() noexcept {
+		return xieite::strings::between(xieite::strings::after(__FUNCSIG__, "__cdecl"), '<', ">(");
+	}
+}
+
+#	else
+#		error "Compiler not supported"
+#	endif
 
 namespace xieite::types {
 	template<typename Type>
