a0394d7677c92ff2eff729000d1b42eb5f709a9a
Eczbek <eczbek.void@gmail.com>
2024-01-16 19:55:23 -0500

    Remove excessive newlines in initializer lists

--- include/xieite/math/factorial.hpp
+++ include/xieite/math/factorial.hpp
@@ -12,9 +12,7 @@ namespace xieite::math {
 	template<xieite::concepts::Arithmetic Number>
 	inline constexpr auto factorial = ([] {
 		static constexpr auto generate = [] {
-			std::vector<Number> result = {
-				1
-			};
+			std::vector<Number> result = { 1 };
 			for (Number i = 1; !xieite::math::multiplicationOverflows(i, result[i - 1]); ++i) {
 				result.push_back(i * result[i - 1]);
 			}
--- include/xieite/metadata/compilation_timestamp.hpp
+++ include/xieite/metadata/compilation_timestamp.hpp
@@ -7,20 +7,7 @@
 #	include "../math/parse.hpp"
 
 namespace xieite::metadata {
-	static constexpr std::chrono::seconds compilationTimestamp = std::chrono::years(xieite::math::parse<int>(std::string_view(__DATE__).substr(7))) + std::chrono::months((xieite::containers::FixedMap<std::string_view, int, 12> {
-		{ "Jan", 1 },
-		{ "Feb", 2 },
-		{ "Mar", 3 },
-		{ "Apr", 4 },
-		{ "May", 5 },
-		{ "Jun", 6 },
-		{ "Jul", 7 },
-		{ "Aug", 8 },
-		{ "Sep", 9 },
-		{ "Oct", 10 },
-		{ "Nov", 11 },
-		{ "Dec", 12 }
-	})[std::string_view(__DATE__).substr(0, 3)]) + std::chrono::days(xieite::math::parse<int>(std::string_view(__DATE__).substr(4, 2))) + std::chrono::hours(xieite::math::parse<int>(std::string_view(__TIME__).substr(0, 2))) + std::chrono::minutes(xieite::math::parse<int>(std::string_view(__TIME__).substr(3, 2))) + std::chrono::seconds(xieite::math::parse<int>(std::string_view(__TIME__).substr(6)));
+	static constexpr std::chrono::seconds compilationTimestamp = std::chrono::years(xieite::math::parse<int>(std::string_view(__DATE__).substr(7))) + std::chrono::months((xieite::containers::FixedMap<std::string_view, int, 12> { { "Jan", 1 }, { "Feb", 2 }, { "Mar", 3 }, { "Apr", 4 }, { "May", 5 }, { "Jun", 6 }, { "Jul", 7 }, { "Aug", 8 }, { "Sep", 9 }, { "Oct", 10 }, { "Nov", 11 }, { "Dec", 12 } })[std::string_view(__DATE__).substr(0, 3)]) + std::chrono::days(xieite::math::parse<int>(std::string_view(__DATE__).substr(4, 2))) + std::chrono::hours(xieite::math::parse<int>(std::string_view(__TIME__).substr(0, 2))) + std::chrono::minutes(xieite::math::parse<int>(std::string_view(__TIME__).substr(3, 2))) + std::chrono::seconds(xieite::math::parse<int>(std::string_view(__TIME__).substr(6)));
 }
 
 #endif
