e83b349f0d8ab060f5c168a05aac47682b285b6c
Eczbek <eczbek.void@gmail.com>
2024-01-23 03:27:35 -0500

    Fix `XIEITE_HAS_INCLUDE`

--- include/xieite/macros/has_include.hpp
+++ include/xieite/macros/has_include.hpp
@@ -3,7 +3,7 @@
 
 #	include "../macros/language_standard.hpp"
 
-#	ifdef XIEITE_LANGUAGE_STANDARD_CPP_2017
+#	ifdef XIEITE_LANGUAGE_STANDARD_CPP_2017 || XIEITE_LANGUAGE_STANDARD_C_2023
 #		define XIEITE_HAS_INCLUDE(...) __has_include(__VA_ARGS__)
 #	else
 #		define XEIITE_HAS_INCLUDE(...) 0
