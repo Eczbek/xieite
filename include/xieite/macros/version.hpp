e4e373f1215a128bd65d3757396e69ca5b56e419
Eczbek <eczbek.void@gmail.com>
2024-02-17 15:36:58 -0500

    Accept booleans in `xieite::math::isPrime`

--- include/xieite/macros/version.hpp
+++ include/xieite/macros/version.hpp
@@ -2,8 +2,8 @@
 #	define XIEITE_HEADER_MACROS_VERSION
 
 #	define XIEITE_VERSION_MAJOR 6
-#	define XIEITE_VERSION_MINOR 42
-#	define XIEITE_VERSION_PATCH 2
+#	define XIEITE_VERSION_MINOR 43
+#	define XIEITE_VERSION_PATCH 0
 
 #	define XIEITE_VERSION_LEAST(major, minor, patch) ((XIEITE_VERSION_MAJOR > (major)) || (XIEITE_VERSION_MAJOR == (major)) && ((XIEITE_VERSION_MINOR > (minor) || (XIEITE_VERSION_MINOR == (minor)) && (XIEITE_VERSION_PATCH >= (patch)))))
 #	define XIEITE_VERSION_MOST(major, minor, patch) ((XIEITE_VERSION_MAJOR < (major)) || (XIEITE_VERSION_MAJOR == (major)) && ((XIEITE_VERSION_MINOR < (minor) || (XIEITE_VERSION_MINOR == (minor)) && (XIEITE_VERSION_PATCH <= (patch)))))
--- include/xieite/math/is_prime.hpp
+++ include/xieite/math/is_prime.hpp
@@ -2,21 +2,23 @@
 #	define XIEITE_HEADER_MATH_IS_PRIME
 
 #	include <cmath>
-#	include "../concepts/integer.hpp"
+#	include <concepts>
 
 namespace xieite::math {
-	template<xieite::concepts::Integer Integer>
+	template<std::integral Integer>
 	[[nodiscard]] constexpr bool isPrime(const Integer value) noexcept {
-		if (value < 4) {
-			return value > 1;
-		}
-		if (!(value % 2) || !(value % 3)) {
-			return false;
-		}
-		const Integer limit = std::sqrt(value);
-		for (Integer i = 5; i <= limit; i += 6) {
-			if (!(value % i) || !(value % (i + 2))) {
+		if constexpr (!std::same_as<Integer, bool>) {
+			if (value < 4) {
+				return value > 1;
+			}
+			if (!(value % 2) || !(value % 3)) {
 				return false;
+			}
+			const Integer limit = std::sqrt(value);
+			for (Integer i = 5; i <= limit; i += 6) {
+				if (!(value % i) || !(value % (i + 2))) {
+					return false;
+				}
 			}
 		}
 		return true;
