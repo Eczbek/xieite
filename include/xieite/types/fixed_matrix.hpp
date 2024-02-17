7676465042c5194801458f91aaeb985ff5b8a3f8
Eczbek <eczbek.void@gmail.com>
2024-02-17 15:37:47 -0500

    Improve `xieite::types::FixedMatrix`

--- include/xieite/types/fixed_matrix.hpp
+++ include/xieite/types/fixed_matrix.hpp
@@ -1,13 +1,15 @@
 #ifndef XIEITE_HEADER_TYPES_FIXED_MATRIX
 #	define XIEITE_HEADER_TYPES_FIXED_MATRIX
 
-namespace xieite::types {
+#	include <cstddef>
+
+namespace xieite::detail {
 	template<template<typename, std::size_t> typename, typename, std::size_t...>
 	struct FixedMatrix;
 
 	template<template<typename, std::size_t> typename FixedContainer, typename Value, std::size_t sizesFirst, std::size_t... sizesRest>
 	struct FixedMatrix<FixedContainer, Value, sizesFirst, sizesRest...> {
-		using Type = FixedMatrix<FixedContainer, FixedContainer<Value, sizesFirst>, sizesRest...>::Type;
+		using Type = FixedContainer<typename xieite::detail::FixedMatrix<FixedContainer, Value, sizesRest...>::Type, sizesFirst>;
 	};
 
 	template<template<typename, std::size_t> typename FixedContainer, typename Value, std::size_t size>
@@ -19,6 +21,11 @@ namespace xieite::types {
 	struct FixedMatrix<FixedContainer, Value> {
 		using Type = Value;
 	};
+};
+
+namespace xieite::types {
+	template<template<typename, std::size_t> typename Container, typename Value, std::size_t... sizes>
+	using FixedMatrix = xieite::detail::FixedMatrix<Container, Value, sizes...>::Type;
 }
 
 #endif
