fe6f782721b8fd180f96b6cfd0c82158ac5ec0dd
Eczbek <eczbek.void@gmail.com>
2024-03-13 08:20:48 -0400

    Improve `xieite::types::MaybeReference`

--- include/xieite/types/maybe_reference.hpp
+++ include/xieite/types/maybe_reference.hpp
@@ -6,7 +6,7 @@
 
 namespace xieite::types {
 	template<typename Type>
-	using MaybeReference = std::conditional_t<(sizeof(Type) > sizeof(std::size_t)), const Type&, const Type>;
+	using MaybeReference = std::conditional_t<(sizeof(Type) <= sizeof(std::size_t)) && std::is_trivially_copyable_v<Type>, Type, Type&>;
 }
 
 #endif
