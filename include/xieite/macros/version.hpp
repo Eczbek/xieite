82b91f570654f812fafbe2b02b7780b370936e56
Eczbek <eczbek.void@gmail.com>
2024-05-08 00:33:27 -0400

    Make `xieite::system::segmentationFault()` reinterpret `nullptr` as a non-constant pointer

--- include/xieite/macros/version.hpp
+++ include/xieite/macros/version.hpp
@@ -3,7 +3,7 @@
 
 #	define XIEITE_VERSION_MAJOR 8
 #	define XIEITE_VERSION_MINOR 0
-#	define XIEITE_VERSION_PATCH 0
+#	define XIEITE_VERSION_PATCH 1
 
 #	define XIEITE_VERSION_LEAST(major_, minor_, patch_) ((XIEITE_VERSION_MAJOR > (major_)) || (XIEITE_VERSION_MAJOR == (major_)) && ((XIEITE_VERSION_MINOR > (minor_) || (XIEITE_VERSION_MINOR == (minor_)) && (XIEITE_VERSION_PATCH >= (patch_)))))
 #	define XIEITE_VERSION_MOST(major_, minor_, patch_) ((XIEITE_VERSION_MAJOR < (major_)) || (XIEITE_VERSION_MAJOR == (major_)) && ((XIEITE_VERSION_MINOR < (minor_) || (XIEITE_VERSION_MINOR == (minor_)) && (XIEITE_VERSION_PATCH <= (patch_)))))
--- include/xieite/system/segmentation_fault.hpp
+++ include/xieite/system/segmentation_fault.hpp
@@ -3,7 +3,7 @@
 
 namespace xieite::system {
 	inline void segmentationFault() noexcept {
-		*static_cast<volatile const int*>(nullptr);
+		*static_cast<volatile int*>(nullptr);
 	}
 }
 
