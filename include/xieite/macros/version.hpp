8fb3f600ff98d076fdd9b249708af4af4b4bae9a
Eczbek <eczbek.void@gmail.com>
2024-08-12 15:54:56 -0400

    Remove `xieite::containers::initializeMultidimensionalArray<>()`

--- include/xieite/macros/version.hpp
+++ include/xieite/macros/version.hpp
@@ -1,9 +1,9 @@
 #ifndef XIEITE_HEADER_MACROS_VERSION
 #	define XIEITE_HEADER_MACROS_VERSION
 
-#	define XIEITE_VERSION_MAJOR 17
-#	define XIEITE_VERSION_MINOR 6
-#	define XIEITE_VERSION_PATCH 4
+#	define XIEITE_VERSION_MAJOR 18
+#	define XIEITE_VERSION_MINOR 0
+#	define XIEITE_VERSION_PATCH 0
 
 #	define XIEITE_VERSION_EQUAL(major_, minor_, patch_) ((XIEITE_VERSION_MAJOR == (major_)) && (XIEITE_VERSION_MINOR == (minor_)) && (XIEITE_VERSION_PATCH == (patch_)))
 #	define XIEITE_VERSION_LEAST(major_, minor_, patch_) ((XIEITE_VERSION_MAJOR > (major_)) || (XIEITE_VERSION_MAJOR == (major_)) && ((XIEITE_VERSION_MINOR > (minor_) || (XIEITE_VERSION_MINOR == (minor_)) && (XIEITE_VERSION_PATCH >= (patch_)))))
