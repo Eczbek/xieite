59a6f0fe20a34cdee160b8531b9c35265720925b
Eczbek <eczbek.void@gmail.com>
2024-09-14 11:59:56 -0400

    Possibly improve `xieite::math::fibonacci<>` and `xieite::math::factorial<>`

--- src/math/factorial.cpp
+++ src/math/factorial.cpp
@@ -5,18 +5,16 @@ import :concepts.Arithmetic;
 import :containers.makeArray;
 import :math.multiplicationOverflows;
 
-template<xieite::concepts::Arithmetic Arithmetic>
-[[nodiscard]] constexpr std::vector<Arithmetic> get() noexcept {
-	std::vector<Arithmetic> result = { 1 };
-	for (Arithmetic i = 1; !xieite::math::multiplicationOverflows(i, result[i - 1]); ++i) {
-		result.push_back(i * result[i - 1]);
-	}
-	return result;
-}
-
 export namespace xieite::math {
 	template<xieite::concepts::Arithmetic Arithmetic>
-	inline constexpr auto factorial = xieite::containers::makeArray<Arithmetic, get<Arithmetic>().size()>(get<Arithmetic>());
+	constexpr auto factorial = ([] {
+		static constexpr auto get = [] {
+			std::vector<Arithmetic> result = { 1 };
+			for (Arithmetic i = 1; !xieite::math::multiplicationOverflows(i, result[i - 1]); ++i) {
+				result.push_back(i * result[i - 1]);
+			}
+			return result;
+		};
+		return xieite::containers::makeArray<Arithmetic, get().size()>(get());
+	})();
 }
-
-// TODO: remove `inline` once wreien fixes related bug next week
--- src/math/fibonacci.cpp
+++ src/math/fibonacci.cpp
@@ -5,18 +5,16 @@ import :concepts.Arithmetic;
 import :containers.makeArray;
 import :math.additionOverflows;
 
-template<xieite::concepts::Arithmetic Arithmetic>
-[[nodiscard]] constexpr std::vector<Arithmetic> get() noexcept {
-	std::vector<Arithmetic> result = { 0, 1 };
-	for (std::size_t i = 2; !xieite::math::additionOverflows(result[i - 1], result[i - 2]); ++i) {
-		result.push_back(result[i - 1] + result[i - 2]);
-	}
-	return result;
-}
-
 export namespace xieite::math {
 	template<xieite::concepts::Arithmetic Arithmetic>
-	inline constexpr auto fibonacci = xieite::containers::makeArray<Arithmetic, get<Arithmetic>().size()>(get<Arithmetic>());
+	constexpr auto fibonacci = ([] {
+		static constexpr auto get = [] {
+			std::vector<Arithmetic> result = { 0, 1 };
+			for (std::size_t i = 2; !xieite::math::additionOverflows(result[i - 1], result[i - 2]); ++i) {
+				result.push_back(result[i - 1] + result[i - 2]);
+			}
+			return result;
+		};
+		return xieite::containers::makeArray<Arithmetic, get().size()>(get());
+	})();
 }
-
-// TODO: remove `inline` once wreien fixes related bug next week
