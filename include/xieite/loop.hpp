1ba786f517308208336c6ee09070058dedf4ae9b
Eczbek <eczbek.void@gmail.com>
2024-09-17 13:13:14 -0400

    Allow empty third statement in `XIEITE_LOOP_FOR()`

--- include/xieite/loop.hpp
+++ include/xieite/loop.hpp
@@ -1,15 +1,27 @@
 #pragma once
 
+#include <xieite/forward.hpp>
+
 #define XIEITE_LOOP_FOR(...) \
 	for (struct { \
 		int flag = 2; \
 		bool broke = false; \
+		constexpr auto chainable(auto&&...) const noexcept { \
+			struct Chainable { \
+				constexpr void operator+() const noexcept {} \
+				\
+				/* discardable */ constexpr auto&& operator+(auto&& value, Chainable) noexcept { \
+					return XIEITE_FORWARD(value); \
+				} \
+			}; \
+			return Chainable(); \
+		}
 	} XIEITE_DETAIL_LOOP_STATE; XIEITE_DETAIL_LOOP_STATE.flag && !XIEITE_DETAIL_LOOP_STATE.broke; --XIEITE_DETAIL_LOOP_STATE.flag) \
 		if ((XIEITE_DETAIL_LOOP_STATE.flag == 2) && !XIEITE_DETAIL_LOOP_STATE.broke) \
-			for (__VA_ARGS__, XIEITE_DETAIL_LOOP_STATE.broke = true) \
+			for (__VA_ARGS__ + XIEITE_DETAIL_LOOP_STATE.chainable(XIEITE_DETAIL_LOOP_STATE.broke = true)) \
 				if ((XIEITE_DETAIL_LOOP_STATE.broke = false)); \
 				else
 
-#define XIEITE_LOOP_WHILE(...) XIEITE_LOOP_FOR(; __VA_ARGS__; void())
+#define XIEITE_LOOP_WHILE(...) XIEITE_LOOP_FOR(; static_cast<bool>(__VA_ARGS__);)
 
 // Blatantly stolen from m1lkweed (https://gist.github.com/m1lkweed/3be672402c43ac5697f1e29bcb79e004)
