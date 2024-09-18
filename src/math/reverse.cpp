9a54d1ecb5903c7c5a37e79832b9788fa74ab646
Eczbek <eczbek.void@gmail.com>
2024-09-18 14:56:40 -0400

    Fix `xieite::math::reverse<>()`

--- src/math/reverse.cpp
+++ src/math/reverse.cpp
@@ -3,23 +3,33 @@ export module xieite:math.reverse;
 import std;
 import :math.absolute;
 import :math.isNegative;
-import :math.sign;
+import :math.TryUnsigned;
 
 export namespace xieite::math {
 	template<std::integral Integral>
 	[[nodiscard]] constexpr Integral reverse(Integral value, const std::common_type_t<Integral> radix = 10) noexcept {
-		const Integral sign = static_cast<Integral>(xieite::math::sign(value));
-		value *= sign;
+		if (!radix) {
+			return 0;
+		} else if ((radix == 1) || (!std::unsigned_integral<Integral> && (radix == static_cast<Integral>(-1)))) {
+			return value;
+		}
+		const bool negative = xieite::math::isNegative(value);
 		Integral result = 0;
-		while (value) {
-			Integral digit = value % radix;
+		const auto nextDigit = [&value, radix, &result](Integral digit) {
 			value /= radix;
 			if (xieite::math::isNegative(digit)) {
-				digit += xieite::math::absolute(radix);
+				digit = xieite::math::isNegative(radix) ? (digit - radix) : (digit + radix);
 				++value;
 			}
 			result = result * radix + digit;
+		};
+		if (value == std::numeric_limits<Integral>::min()) {
+			nextDigit(-(value % radix));
 		}
-		return result * sign;
+		value = static_cast<Integral>(xieite::math::absolute(value));
+		while (value) {
+			nextDigit(value % radix);
+		}
+		return negative ? -result : result;
 	}
 }
