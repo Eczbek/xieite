131db93b35c1cd437f68bb3d87b9337e4e1f108e
Eczbek <eczbek.void@gmail.com>
2024-09-27 01:42:44 -0400

    Fix `xieite::types::CollapseReference<>` and `xieite::types::CopyReference<>` for `void` types

--- src/types/collapse_reference.cpp
+++ src/types/collapse_reference.cpp
@@ -4,5 +4,5 @@ import std;
 
 export namespace xieite::types {
 	template<typename Source, typename Target>
-	using CollapseReference = std::conditional_t<std::is_lvalue_reference_v<Source>, Target&, std::conditional_t<std::is_rvalue_reference_v<Source>, Target&&, Target>>;
+	using CollapseReference = std::conditional_t<std::is_lvalue_reference_v<Source>, std::add_lvalue_reference_t<Target>, std::conditional_t<std::is_rvalue_reference_v<Source>, std::add_rvalue_reference_t<Target>, std::type_identity_t<Target>>>;
 }
