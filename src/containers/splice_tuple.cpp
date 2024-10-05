db1409285b815b8bcd1d74dda27374a2be8c930b
Eczbek <eczbek.void@gmail.com>
2024-10-04 20:59:19 -0400

    Improve formatting slightly

--- src/containers/splice_tuple.cpp
+++ src/containers/splice_tuple.cpp
@@ -14,10 +14,14 @@ export namespace xieite::containers {
 		static constexpr std::size_t tupleSize = std::tuple_size_v<std::remove_cvref_t<Tuple1>>;
 		static_assert(start <= tupleSize, "start index must be within tuple size");
 		static_assert(end <= tupleSize, "end index must be within tuple size");
-		return std::tuple_cat(([&tuple1]<std::size_t... i>(std::index_sequence<i...>) {
-			return std::forward_as_tuple(std::get<i>(std::move(tuple1))...);
-		})(std::make_index_sequence<start>()), xieite::containers::forwardTuple(XIEITE_FORWARD(tuple2)), ([&tuple1]<std::size_t... i>(std::index_sequence<i...>) {
-			return std::forward_as_tuple(std::get<end + i>(std::move(tuple1))...);
-		})(std::make_index_sequence<tupleSize - end>()));
+		return std::tuple_cat(
+			([&tuple1]<std::size_t... i>(std::index_sequence<i...>) {
+				return std::forward_as_tuple(std::get<i>(std::move(tuple1))...);
+			})(std::make_index_sequence<start>()),
+			xieite::containers::forwardTuple(XIEITE_FORWARD(tuple2)),
+			([&tuple1]<std::size_t... i>(std::index_sequence<i...>) {
+				return std::forward_as_tuple(std::get<end + i>(std::move(tuple1))...);
+			})(std::make_index_sequence<tupleSize - end>())
+		);
 	}
 }
