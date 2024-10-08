17bdb1fc9aec38eff5b86441c70473d38c7c1b74
Eczbek <eczbek.void@gmail.com>
2024-10-08 13:18:01 -0400

    Fix formatting

--- src/containers/forward_tuple.cpp
+++ src/containers/forward_tuple.cpp
@@ -10,8 +10,11 @@ import :concepts.SpecializationOf;
 export namespace xieite::containers {
 	template<xieite::concepts::SpecializationOf<std::tuple> Tuple>
 	[[nodiscard]] constexpr auto forwardTuple(Tuple&& tuple) noexcept {
-		return std::apply([](auto&&... values) {
-			return std::forward_as_tuple(XIEITE_FORWARD(values)...);
-		}, XIEITE_FORWARD(tuple));
+		return std::apply(
+			[](auto&&... values) {
+				return std::forward_as_tuple(XIEITE_FORWARD(values)...);
+			},
+			XIEITE_FORWARD(tuple)
+		);
 	}
 }
