8dd6ee81c4565e2f5dcf9136fd366c001bbc8263
Eczbek <eczbek.void@gmail.com>
2024-10-09 04:46:25 -0400

    Add more unnecessary features to `XIEITE_FORWARD`

--- include/xieite/forward.hpp
+++ include/xieite/forward.hpp
@@ -3,4 +3,13 @@
 #include <concepts>
 #include <type_traits>
 
-#define XIEITE_FORWARD(...) static_cast<std::conditional_t<std::same_as<decltype(__VA_ARGS__), decltype((__VA_ARGS__))>, decltype(__VA_ARGS__), std::add_rvalue_reference_t<decltype(__VA_ARGS__)>>(__VA_ARGS__)
+#define XIEITE_FORWARD(...) \
+	([&]<auto = [] {}> -> decltype(auto) { \
+		if constexpr (::std::same_as<decltype(__VA_ARGS__), decltype(__VA_ARGS__)> && !::std::same_as<decltype(__VA_ARGS__), decltype((__VA_ARGS__))>) { \
+			return static_cast<::std::add_rvalue_reference_t<decltype(__VA_ARGS__)>>(__VA_ARGS__); \
+		} else { \
+			return (__VA_ARGS__); \
+		} \
+	})()
+
+// The `static_cast` is translated unless the condition of the `if constexpr` is both dependent and false
