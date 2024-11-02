19895d2743ce6e7b2ed2aa198f78f3acd39e50f2
Eczbek <eczbek.void@gmail.com>
2024-11-02 16:06:08 -0400

    Improve `xieite::detect_cosmic_ray()`

--- src/sys/detect_cosmic_ray.cpp
+++ src/sys/detect_cosmic_ray.cpp
@@ -4,10 +4,8 @@ import std;
 
 export namespace xieite {
 	void detect_cosmic_ray(std::size_t bytes) noexcept {
-		using Array = volatile const std::byte[];
 		const std::size_t size = bytes / sizeof(std::intmax_t);
-		const auto detector = std::make_unique<Array>(size);
-		std::size_t i = 0;
-		while (!detector[++i %= size]);
+		const auto detector = std::make_unique<volatile const std::byte[]>(size);
+		for (std::size_t i = 0; !detector[++i %= size];);
 	}
 }
