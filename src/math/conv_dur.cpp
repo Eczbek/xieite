ada3ed3e3e48272a7b2ee78459d0f87c1e26220b
Eczbek <eczbek.void@gmail.com>
2024-12-02 13:40:59 -0500

    Improve `xieite::conv_dur()`

--- src/math/conv_dur.cpp
+++ src/math/conv_dur.cpp
@@ -7,11 +7,7 @@ export namespace xieite {
 	template<xieite::is_dur T, xieite::is_dur... Us>
 	[[nodiscard]] constexpr std::tuple<Us...> conv_dur(T dur) noexcept {
 		std::tuple<Us...> results;
-		(..., ([&dur, &results] -> void {
-			const Us result = std::chrono::duration_cast<Us>(dur);
-			std::get<Us>(results) = result;
-			dur -= result;
-		})());
+		(..., (dur -= std::get<Us>(results) = std::chrono::duration_cast<Us>(dur)));
 		return results;
 	}
 }
