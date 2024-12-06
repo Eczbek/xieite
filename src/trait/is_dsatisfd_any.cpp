7cbcc8186350f9d83ace3eb10242588220209d69
Eczbek <eczbek.void@gmail.com>
2024-12-06 15:15:19 -0500

    Fix `xieite::is_nothrow_conv<>`

--- src/trait/is_dsatisfd_any.cpp
+++ src/trait/is_dsatisfd_any.cpp
@@ -1,8 +1,8 @@
-export module xieite:is_dissatisfied_by_any;
+export module xieite:is_dissatisfied_any;
 
 import :is_satisfd_any;
 
 export namespace xieite {
 	template<auto fn, typename... Ts>
-	concept is_dissatisfied_by_any = !xieite::is_satisfd_any<fn, Ts...>;
+	concept is_dissatisfied_any = !xieite::is_satisfd_any<fn, Ts...>;
 }
--- src/trait/is_satisf.cpp
+++ src/trait/is_satisf.cpp
@@ -1,8 +1,8 @@
 export module xieite:is_satisf;
 
-import :is_satisfd_by;
+import :is_satisfd;
 
 export namespace xieite {
 	template<typename T, auto fn>
-	concept is_satisf = xieite::is_satisfd_by<fn, T>;
+	concept is_satisf = xieite::is_satisfd<fn, T>;
 }
