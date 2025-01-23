20d5060f9b232ea45a13d9247fa85d43304323b6
Eczbek <eczbek.void@gmail.com>
2025-01-22 20:55:50 -0500

    Accomodate for Clang bug

--- src/trait/is_invoc.cpp
+++ src/trait/is_invoc.cpp
@@ -5,7 +5,7 @@ import :t;
 
 export namespace xieite {
 	template<typename T, typename Sig = void()>
-	concept is_invoc = ([]<typename Ret, typename... Args>(xieite::t<Ret(Args...)>) -> bool {
+	concept is_invoc = ([]<typename Ret, typename... Args>(xieite::t<Ret(Args...)>) /* -> bool */ {
 		return std::is_invocable_r_v<Ret, T, Args...>;
 	})(xieite::t<Sig>());
 }
