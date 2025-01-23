f0054d6d4917808b16ecf7c9a7d5b2f03f067c85
Eczbek <eczbek.void@gmail.com>
2025-01-23 15:33:38 -0500

    Improve concepts related to character types

--- src/trait/is_same_any.cpp
+++ src/trait/is_same_any.cpp
@@ -3,6 +3,6 @@ export module xieite:is_same_any;
 import std;
 
 export namespace xieite {
-	template<typename T, typename... Us>
-	concept is_same_any = (... || std::same_as<T, Us>);
+	template<typename... Ts>
+	concept is_same_any = (... || std::same_as<Ts...[0], Ts>);
 }
--- src/trait/is_wide_ch.cpp
+++ src/trait/is_wide_ch.cpp
@@ -1,10 +1,10 @@
 export module xieite:is_wide_ch;
 
-import std;
 import :is_same_any;
+import :rm_cv
 
 export namespace xieite {
 	template<typename T>
-	concept is_wide_ch = xieite::is_same_any<std::remove_cv_t<T>, wchar_t, char16_t, char32_t>;
-	// A section from the C++ working draft was previously linked here, but now it's gone and apparently never existed. Oh well.
+	concept is_wide_ch = xieite::is_same_any<xieite::rm_cv<T>, wchar_t, char16_t, char32_t>;
+	// I'm certain that a section of the C++ working draft was previously referenced here, but now it's gone and apparently never existed. Oh well.
 }
