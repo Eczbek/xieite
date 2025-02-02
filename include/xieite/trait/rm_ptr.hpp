145a847db53f4d03a1856e0d0ceacfdf5a8b6354
Eczbek <eczbek.void@gmail.com>
2025-02-02 14:16:45 -0500

    Fix `xieite::rm_ptr<>`

--- include/xieite/trait/rm_ptr.hpp
+++ include/xieite/trait/rm_ptr.hpp
@@ -9,7 +9,7 @@ namespace xieite {
 	template<typename T, std::size_t n = 1>
 	using rm_ptr = xieite::cp_ref<T, typename decltype(([]<typename U = std::remove_reference_t<T>, std::size_t m = n>(this auto self) {
 		if constexpr (std::is_pointer_v<U> && m) {
-			return self.template operator()<std::remove_pointer_t<T>, (m - 1)>();
+			return self.template operator()<std::remove_pointer_t<U>, (m - 1)>();
 		} else {
 			return xieite::t<U>();
 		}
