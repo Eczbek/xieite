c9d9ef6f2cd40aa95b70e1d0c255b5f6f853687c
Eczbek <eczbek.void@gmail.com>
2025-02-06 10:28:10 -0500

    Fix `xieite::arity<>`

--- include/xieite/meta/arity.hpp
+++ include/xieite/meta/arity.hpp
@@ -5,7 +5,11 @@
 #include <type_traits>
 #include "../meta/any.hpp"
 #include "../meta/tuple_size.hpp"
+#include "../pp/diag.hpp"
 #include "../trait/is_tuple_like.hpp"
+
+XIEITE_DIAG_PUSH_CLANG()
+XIEITE_DIAG_OFF_CLANG("-Wunused-lambda-capture")
 
 namespace xieite {
 	template<typename T>
@@ -13,17 +17,17 @@ namespace xieite {
 		if constexpr (xieite::is_tuple_like<T>) {
 			return xieite::tuple_size<T>;
 		} else if constexpr (std::is_aggregate_v<T>) {
-			return ([](this auto self, std::size_t offset = 0, auto... curr) -> std::size_t {
+			return ([](this auto self, std::size_t offset = 0, auto... curr) /* -> std::size_t */ {
 				if constexpr (requires { T { curr..., xieite::any() }; } || requires { T { curr..., { xieite::any(), xieite::any() } }; }) {
-					([&offset, curr...](this auto self, auto... pre) -> std::size_t {
+					([&offset, curr...](this auto self, auto... pre) -> void {
 						if constexpr (requires { T { curr..., pre..., xieite::any() }; }) {
 							self(pre..., xieite::any());
 						} else {
-							([&offset, curr..., pre...](this auto self, auto... post) -> std::size_t {
+							([&offset, curr..., pre...](this auto self, auto... post) -> void {
 								if constexpr (requires { T { curr..., { xieite::any(), xieite::any() }, post..., xieite::any() }; }) {
 									self(post..., xieite::any());
 								} else if constexpr (sizeof...(pre) != (sizeof...(post) + 1)) {
-									([&offset, curr...](this auto self, auto... inner) -> std::size_t {
+									([&offset, curr...](this auto self, auto... inner) -> void {
 										if constexpr (requires { T { curr..., { inner..., xieite::any() } }; }) {
 											self(inner..., xieite::any());
 										} else {
@@ -45,4 +49,6 @@ namespace xieite {
 	})();
 }
 
-// Originally stolen from lapinozz (https://github.com/lapinozz)
+XIEITE_DIAG_POP_CLANG()
+
+// Algorithm stolen from lapinozz (https://github.com/lapinozz)
--- include/xieite/pp/diag.hpp
+++ include/xieite/pp/diag.hpp
@@ -4,8 +4,17 @@
 #include "../pp/pragma.hpp"
 
 #define XIEITE_DIAG_MSG(_)
+#define XIEITE_DIAG_MSG_GCC(_)
+#define XIEITE_DIAG_MSG_CLANG(_)
+#define XIEITE_DIAG_MSG_MSVC(_)
 #define XIEITE_DIAG_WARN(_)
+#define XIEITE_DIAG_WARN_GCC(_)
+#define XIEITE_DIAG_WARN_CLANG(_)
+#define XIEITE_DIAG_WARN_MSVC(_)
 #define XIEITE_DIAG_ERR(_)
+#define XIEITE_DIAG_ERR_GCC(_)
+#define XIEITE_DIAG_ERR_CLANG(_)
+#define XIEITE_DIAG_ERR_MSVC(_)
 #define XIEITE_DIAG_PUSH()
 #define XIEITE_DIAG_PUSH_GCC()
 #define XIEITE_DIAG_PUSH_CLANG()
@@ -28,11 +37,20 @@
 #	undef XIEITE_DIAG_MSG
 #	define XIEITE_DIAG_MSG(s_) XIEITE_PRAGMA(message s_)
 
+#	undef XIEITE_DIAG_MSG_GCC
+#	define XIEITE_DIAG_MSG_GCC(s_) XIEITE_DIAG_MSG(s_)
+
 #	undef XIEITE_DIAG_WARN
 #	define XIEITE_DIAG_WARN(s_) XIEITE_PRAGMA(GCC warning s_)
 
+#	undef XIEITE_DIAG_WARN_GCC
+#	define XIEITE_DIAG_WARN_GCC(s_) XIEITE_DIAG_WARN(s_)
+
 #	undef XIEITE_DIAG_ERR
 #	define XIEITE_DIAG_ERR(s_) XIEITE_PRAGMA(GCC error s_)
+
+#	undef XIEITE_DIAG_ERR_GCC
+#	define XIEITE_DIAG_ERR_GCC(s_) XIEITE_DIAG_ERR(s_)
 
 #	undef XIEITE_DIAG_PUSH
 #	define XIEITE_DIAG_PUSH() XIEITE_PRAGMA(GCC diagnostic push)
@@ -58,11 +76,20 @@
 #	undef XIEITE_DIAG_MSG
 #	define XIEITE_DIAG_MSG(s_) XIEITE_PRAGMA(message s_)
 
+#	undef XIEITE_DIAG_MSG_CLANG
+#	define XIEITE_DIAG_MSG_CLANG(s_) XIEITE_DIAG_MSG(s_)
+
 #	undef XIEITE_DIAG_WARN
 #	define XIEITE_DIAG_WARN(s_) XIEITE_PRAGMA(GCC warning s_)
 
+#	undef XIEITE_DIAG_WARN_CLANG
+#	define XIEITE_DIAG_WARN_CLANG(s_) XIEITE_DIAG_WARN(s_)
+
 #	undef XIEITE_DIAG_ERR
 #	define XIEITE_DIAG_ERR(s_) XIEITE_PRAGMA(GCC error s_)
+
+#	undef XIEITE_DIAG_ERR_CLANG
+#	define XIEITE_DIAG_ERR_CLANG(s_) XIEITE_DIAG_ERR(s_)
 
 #	undef XIEITE_DIAG_PUSH
 #	define XIEITE_DIAG_PUSH() XIEITE_PRAGMA(clang diagnostic push)
@@ -88,11 +115,20 @@
 #	undef XIEITE_DIAG_MSG
 #	define XIEITE_DIAG_MSG(s_) XIEITE_PRAGMA(message(s_))
 
+#	undef XIEITE_DIAG_MSG_MSVC
+#	define XIEITE_DIAG_MSG_MSVC(s_) XIEITE_DIAG_MSG(s_)
+
 #	undef XIEITE_DIAG_WARN
 #	define XIEITE_DIAG_WARN(s_) XIEITE_PRAGMA(message("\x1B[31m" s_ "\x1B[0m"))
 
+#	undef XIEITE_DIAG_WARN_MSVC
+#	define XIEITE_DIAG_WARN_MSVC(s_) XIEITE_DIAG_WARN(s_)
+
 #	undef XIEITE_DIAG_ERR
 #	define XIEITE_DIAG_ERR(s_) XIEITE_PRAGMA(message("\x1B[35m" s_ "\x1B[0m"))
+
+#	undef XIEITE_DIAG_ERR_MSVC
+#	define XIEITE_DIAG_ERR_MSVC(s_) XIEITE_DIAG_ERR(s_)
 
 #	undef XIEITE_DIAG_PUSH
 #	define XIEITE_DIAG_PUSH() XIEITE_PRAGMA(warning(push))
