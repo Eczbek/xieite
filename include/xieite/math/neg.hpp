7b53a6bdb3e7a03a373557326312b5e4882be030
Eczbek <eczbek.void@gmail.com>
2025-02-11 11:01:06 -0500

    Improve `xieite::neg()`

--- include/xieite/math/neg.hpp
+++ include/xieite/math/neg.hpp
@@ -1,12 +1,15 @@
 #pragma once
 
+#include <cmath>
 #include <concepts>
 #include "../trait/is_arith.hpp"
 
 namespace xieite {
 	template<xieite::is_arith T>
 	[[nodiscard]] constexpr bool neg(T n) noexcept {
-		if constexpr (!std::unsigned_integral<T>) {
+		if constexpr (std::floating_point<T>) {
+			return std::signbit(n);
+		} else if constexpr (!std::unsigned_integral<T>) {
 			return n < 0;
 		} else {
 			return false;
