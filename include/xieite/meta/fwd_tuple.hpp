9b2ce4678c1456cf7b0623dec372105ecad782f5
Eczbek <eczbek.void@gmail.com>
2025-02-12 23:36:34 -0500

    Improve `xieite::fwd_tuple()`

--- include/xieite/meta/fwd_tuple.hpp
+++ include/xieite/meta/fwd_tuple.hpp
@@ -3,11 +3,11 @@
 #include <tuple>
 #include "../pp/fwd.hpp"
 #include "../pp/lift.hpp"
-#include "../trait/is_spec.hpp"
+#include "../trait/is_tuple_like.hpp"
 
 namespace xieite {
-	template<xieite::is_spec<std::tuple> Tuple>
-	[[nodiscard]] constexpr auto fwd_tuple(Tuple&& tuple) noexcept {
-		return std::apply(XIEITE_LIFT(std::forward_as_tuple), XIEITE_FWD(tuple));
+	template<xieite::is_tuple_like Tuple>
+	[[nodiscard]] constexpr auto fwd_tuple(Tuple&& x) noexcept {
+		return std::apply(XIEITE_LIFT(std::forward_as_tuple), XIEITE_FWD(x));
 	}
 }
