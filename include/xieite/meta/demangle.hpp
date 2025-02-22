9e2ebac1b919d6c55b12f6efed8bf6b53ff95338
Eczbek <eczbek.void@gmail.com>
2025-02-21 21:56:09 -0500

    Improve `xieite::demangle()`

--- include/xieite/meta/demangle.hpp
+++ include/xieite/meta/demangle.hpp
@@ -10,14 +10,14 @@
 namespace xieite {
 	[[nodiscard]] inline std::string demangle(const std::string& mangled) noexcept {
 #if __has_include(<cxxabi.h>)
-		char* const buf = abi::__cxa_demangle(mangled.c_str(), nullptr, nullptr, nullptr);
-		if (buf) {
+		if (char* const buf = abi::__cxa_demangle(mangled.c_str(), nullptr, nullptr, nullptr)) {
 			const std::string demangled = std::string(buf);
 			std::free(buf);
 			return demangled;
 		}
 #endif
-		// No need to `free()` if demangling fails
 		return mangled;
 	}
 }
+
+// https://itanium-cxx-abi.github.io/cxx-abi/abi.html#demangler
