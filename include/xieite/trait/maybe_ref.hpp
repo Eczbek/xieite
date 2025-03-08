36b3e7e442cc813d39b059983f7e99a63574d6d7
Eczbek <eczbek.void@gmail.com>
2025-03-08 14:27:35 -0500

    Improve `xieite::maybe_ref<>`

--- include/xieite/trait/maybe_ref.hpp
+++ include/xieite/trait/maybe_ref.hpp
@@ -1,9 +1,8 @@
 #pragma once
 
 #include <type_traits>
-#include "../trait/has_trivial_cp.hpp"
 
 namespace xieite {
 	template<typename T>
-	using maybe_ref = std::conditional_t<(sizeof(T) <= (sizeof(int*) * 2)) && xieite::has_trivial_cp<T>, T, const T&>; 
+	using maybe_ref = std::conditional_t<((sizeof(T) <= (sizeof(int*) * 2)) && std::is_trivially_copyable_v<T>), T, const T&>; 
 }
