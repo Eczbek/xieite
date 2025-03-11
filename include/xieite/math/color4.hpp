a8aa535cef0bb635fe86732ae3d2d26b044e28be
Eczbek <eczbek.void@gmail.com>
2025-03-10 20:41:33 -0400

    Add `xieite::color4 {}`

---
+++ include/xieite/math/color4.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <cstdint>
+
+namespace xieite {
+	struct color4 {
+		std::uint8_t r;
+		std::uint8_t g;
+		std::uint8_t b;
+		std::uint8_t a;
+
+		[[nodiscard]] constexpr color4(std::uint8_t r, std::uint8_t g, std::uint8_t b, std::uint8_t a) noexcept
+		: r(r), g(g), b(b), a(a) {}
+
+		[[nodiscard]] explicit(false) constexpr color4(std::uint_least32_t n = 0) noexcept
+		: r((n >> 24) & 0xFF), g((n >> 16) & 0xFF), b((n >> 8) & 0xFF), a(n & 0xFF) {}
+
+		[[nodiscard]] friend bool operator==(const xieite::color4&, const xieite::color4&) = default;
+
+		[[nodiscard]] constexpr std::uint_least32_t value() const noexcept {
+			return (static_cast<std::uint_least32_t>(this->r) << 24)
+				| (static_cast<std::uint_least32_t>(this->g) << 16)
+				| (static_cast<std::uint_least32_t>(this->b) << 8)
+				| static_cast<std::uint_least32_t>(this->a);
+		}
+	};
+}
