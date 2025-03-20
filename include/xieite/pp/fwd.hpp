ca72cd3ed775694a8673a5b326bde3e155a030c3
Eczbek <eczbek.void@gmail.com>
2025-03-19 20:54:54 -0400

    Fix `XIEITE_FWD_PRVALUE()`

--- include/xieite/pp/fwd.hpp
+++ include/xieite/pp/fwd.hpp
@@ -8,9 +8,9 @@
 #define XIEITE_FWD_PRVALUE_LOCAL(...) DETAIL_XIEITE_FWD_PRVALUE(&, __VA_ARGS__)
 
 #define DETAIL_XIEITE_FWD_PRVALUE(capture_, ...) \
-	([capture_]<bool needs_cast = !::std::is_reference_v<decltype((__VA_ARGS__))> && ::std::same_as<decltype(__VA_ARGS__), decltype(__VA_ARGS__)>, typename Dest = decltype(__VA_ARGS__)> -> decltype(auto) { \
-		if constexpr (needs_cast) { \
-			return static_cast<Dest>(__VA_ARGS__); \
+	([capture_]<bool DETAIL_XIEITE_castable = !::std::is_reference_v<decltype((__VA_ARGS__))> && ::std::same_as<decltype(__VA_ARGS__), decltype(__VA_ARGS__)>, typename DETAIL_XIEITE_type = decltype(__VA_ARGS__)> -> decltype(auto) { \
+		if constexpr (DETAIL_XIEITE_castable) { \
+			return static_cast<DETAIL_XIEITE_type>(__VA_ARGS__); \
 		} else { \
 			return (__VA_ARGS__); \
 		} \
