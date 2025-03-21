00e5ef18378e4cfcb4f9c12ae9b3895a0b4d6526
Eczbek <eczbek.void@gmail.com>
2025-03-20 20:56:54 -0400

    Merge `xieite::distr_args_recurs()` into `xieite::distr_args()`

--- include/xieite/fn/distr_args.hpp
+++ include/xieite/fn/distr_args.hpp
@@ -1,27 +1,54 @@
 #pragma once
 
 #include <cstddef>
-#include <functional>
 #include <tuple>
 #include <utility>
 #include "../fn/unroll.hpp"
+#include "../meta/splice_tuple.hpp"
+#include "../meta/subtuple.hpp"
+#include "../meta/tuple_size.hpp"
+#include "../pp/arrow.hpp"
 #include "../pp/fwd.hpp"
 
 namespace xieite {
-	template<std::size_t arity, typename F, typename... Args>
-	constexpr void distr_args(F&& fn, Args&&... args) noexcept(false) {
-		static_assert(arity || !sizeof...(Args), "arguments must be distributable across functor calls");
-		static_assert(sizeof...(Args) >= arity, "number of arguments must not be less than functor arity");
-		if constexpr (sizeof...(Args) == arity) {
-			std::invoke(XIEITE_FWD(fn), XIEITE_FWD(args)...);
-		} else {
-			const auto args_tuple = std::forward_as_tuple(XIEITE_FWD(args)...);
-			xieite::unroll<arity>([&fn, &args_tuple]<std::size_t... i> -> void {
-				std::invoke(fn, std::get<i>(std::move(args_tuple))...);
-			});
-			xieite::unroll<sizeof...(Args) - arity>([&fn, &args_tuple]<std::size_t... i> -> void {
-				xieite::distr_args<arity>(XIEITE_FWD(fn), std::get<(i + arity)>(std::move(args_tuple))...);
-			});
-		}
+	template<std::size_t arity, std::size_t prev = 0>
+	constexpr decltype(auto) distr_args(auto&& fn, auto&&... args) noexcept(false) { // TODO: `noexcept` specifier
+		return ([]<typename Args>(this auto next, auto&& fn, const Args& args) -> decltype(auto) {
+			if constexpr (xieite::tuple_size<Args> == arity) {
+				return std::apply(XIEITE_FWD(fn), std::move(args));
+			} else if constexpr (prev) {
+				return std::apply(
+					fn,
+					xieite::splice_tuple<0, (xieite::tuple_size<Args> - (arity - prev))>(
+						std::move(args),
+						xieite::unroll<prev>(
+							[]<std::size_t... i>(auto next, const auto& fn, const auto& args) {
+								return std::make_tuple(next(
+									fn,
+									xieite::subtuple<
+										((xieite::tuple_size<Args> - (arity - prev)) / prev * i),
+										((xieite::tuple_size<Args> - (arity - prev)) / prev * (i + 1))
+									>(std::move(args))
+								)...);
+							},
+							next,
+							fn,
+							args
+						)
+					)
+				);
+			} else {
+				return xieite::unroll<(xieite::tuple_size<Args> / arity)>(
+					[]<std::size_t... i>(const auto& fn, const Args& args) static {
+						return (..., std::apply(
+							fn,
+							xieite::subtuple<(i * arity), ((i + 1) * arity)>(std::move(args))
+						));
+					},
+					fn,
+					args
+				);
+			}
+		})(XIEITE_FWD(fn), std::forward_as_tuple(XIEITE_FWD(args)...));
 	}
 }
