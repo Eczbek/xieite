de7f4a93e75ac3b089077bcacc5bf9fc2738112a
Eczbek <eczbek.void@gmail.com>
2025-03-26 22:30:29 -0400

    Improve `xieite::iters {}`

--- include/xieite/ctnr/iters.hpp
+++ include/xieite/ctnr/iters.hpp
@@ -11,7 +11,7 @@ namespace xieite {
 	struct iters {
 	private:
 		template<typename Iter>
-		struct proxy {
+		struct iter {
 			using value_type = Iter;
 
 			Iter value;
@@ -20,20 +20,20 @@ namespace xieite {
 				return self.value;
 			}
 
-			[[nodiscard]] friend constexpr auto operator==(xieite::iters<R>::proxy<Iter> l, xieite::iters<R>::proxy<Iter> r)
+			[[nodiscard]] friend constexpr auto operator==(xieite::iters<R>::iter<Iter> l, xieite::iters<R>::iter<Iter> r)
 				XIEITE_ARROW(l.value == r.value)
 
-			[[nodiscard]] friend constexpr auto operator==(xieite::iters<R>::proxy<Iter> l, Iter r)
+			[[nodiscard]] friend constexpr auto operator==(xieite::iters<R>::iter<Iter> l, Iter r)
 				XIEITE_ARROW(l.value == r)
 
-			[[nodiscard]] friend constexpr auto operator!=(xieite::iters<R>::proxy<Iter> l, xieite::iters<R>::proxy<Iter> r)
+			[[nodiscard]] friend constexpr auto operator!=(xieite::iters<R>::iter<Iter> l, xieite::iters<R>::iter<Iter> r)
 				XIEITE_ARROW(l.value != r.value)
 
-			[[nodiscard]] friend constexpr auto operator!=(xieite::iters<R>::proxy<Iter> l, Iter r)
+			[[nodiscard]] friend constexpr auto operator!=(xieite::iters<R>::iter<Iter> l, Iter r)
 				XIEITE_ARROW(l.value != r)
 
-			[[nodiscard]] friend constexpr auto operator+(xieite::iters<R>::proxy<Iter> l, std::iter_difference_t<Iter> r)
-				XIEITE_ARROW(xieite::iters<R>::proxy(l.value + r))
+			[[nodiscard]] friend constexpr auto operator+(xieite::iters<R>::iter<Iter> l, std::iter_difference_t<Iter> r)
+				XIEITE_ARROW(xieite::iters<R>::iter(l.value + r))
 
 			constexpr auto operator+=(std::iter_difference_t<Iter> r)
 				XIEITE_ARROW(void(this->value += r), *this)
@@ -44,8 +44,8 @@ namespace xieite {
 			constexpr auto operator++(int)
 				XIEITE_ARROW(([this](auto copy) XIEITE_ARROW(void(this->value++), auto(copy)))(*this))
 
-			[[nodiscard]] friend constexpr auto operator-(xieite::iters<R>::proxy<Iter> l, std::iter_difference_t<Iter> r)
-				XIEITE_ARROW(xieite::iters<R>::proxy(l.value - r))
+			[[nodiscard]] friend constexpr auto operator-(xieite::iters<R>::iter<Iter> l, std::iter_difference_t<Iter> r)
+				XIEITE_ARROW(xieite::iters<R>::iter(l.value - r))
 
 			constexpr auto operator-=(std::iter_difference_t<Iter> r)
 				XIEITE_ARROW(void(this->value -= r), *this)
@@ -58,34 +58,34 @@ namespace xieite {
 		};
 
 		template<typename Iter>
-		proxy(Iter) -> proxy<Iter>;
+		iter(Iter) -> iter<Iter>;
 
 	public:
 		R& value;
 
 		[[nodiscard]] constexpr auto begin(this auto&& self)
-			XIEITE_ARROW(xieite::iters<R>::proxy(std::ranges::begin(XIEITE_FWD(self).value)))
+			XIEITE_ARROW(xieite::iters<R>::iter(std::ranges::begin(XIEITE_FWD(self).value)))
 
 		[[nodiscard]] constexpr auto cbegin(this auto&& self)
-			XIEITE_ARROW(xieite::iters<R>::proxy(std::ranges::cbegin(XIEITE_FWD(self).value)))
+			XIEITE_ARROW(xieite::iters<R>::iter(std::ranges::cbegin(XIEITE_FWD(self).value)))
 
 		[[nodiscard]] constexpr auto rbegin(this auto&& self)
-			XIEITE_ARROW(xieite::iters<R>::proxy(std::ranges::rbegin(XIEITE_FWD(self).value)))
+			XIEITE_ARROW(xieite::iters<R>::iter(std::ranges::rbegin(XIEITE_FWD(self).value)))
 
 		[[nodiscard]] constexpr auto crbegin(this auto&& self)
-			XIEITE_ARROW(xieite::iters<R>::proxy(std::ranges::crbegin(XIEITE_FWD(self).value)))
+			XIEITE_ARROW(xieite::iters<R>::iter(std::ranges::crbegin(XIEITE_FWD(self).value)))
 
 		[[nodiscard]] constexpr auto end(this auto&& self)
-			XIEITE_ARROW(xieite::iters<R>::proxy(std::ranges::end(XIEITE_FWD(self).value)))
+			XIEITE_ARROW(xieite::iters<R>::iter(std::ranges::end(XIEITE_FWD(self).value)))
 
 		[[nodiscard]] constexpr auto cend(this auto&& self)
-			XIEITE_ARROW(xieite::iters<R>::proxy(std::ranges::cend(XIEITE_FWD(self).value)))
+			XIEITE_ARROW(xieite::iters<R>::iter(std::ranges::cend(XIEITE_FWD(self).value)))
 
 		[[nodiscard]] constexpr auto rend(this auto&& self)
-			XIEITE_ARROW(xieite::iters<R>::proxy(std::ranges::rend(XIEITE_FWD(self).value)))
+			XIEITE_ARROW(xieite::iters<R>::iter(std::ranges::rend(XIEITE_FWD(self).value)))
 
 		[[nodiscard]] constexpr auto crend(this auto&& self)
-			XIEITE_ARROW(xieite::iters<R>::proxy(std::ranges::crend(XIEITE_FWD(self).value)))
+			XIEITE_ARROW(xieite::iters<R>::iter(std::ranges::crend(XIEITE_FWD(self).value)))
 	};
 
 	template<typename R>
