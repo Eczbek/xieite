e486e92a971f261fb87049fb1e464156a003db94
Eczbek <eczbek.void@gmail.com>
2025-03-26 22:30:48 -0400

    Improve `xieite::is_bounded_array<>`

--- include/xieite/trait/is_bounded_array.hpp
+++ include/xieite/trait/is_bounded_array.hpp
@@ -1,11 +1,14 @@
 #ifndef DETAIL_XIEITE_HEADER_TRAIT_IS_BOUNDED_ARRAY
 #	define DETAIL_XIEITE_HEADER_TRAIT_IS_BOUNDED_ARRAY
 #
+#	include <cstddef>
 #	include <type_traits>
 
 namespace xieite {
-	template<typename T>
-	concept is_bounded_array = std::is_bounded_array_v<T>;
+	template<typename T, std::size_t n = -1uz>
+	concept is_bounded_array = (n == -1uz)
+		? requires { ([]<typename U, std::size_t m>(std::type_identity<U[m]>) {})(std::type_identity<T>()); }
+		: requires { ([]<typename U>(std::type_identity<U[n]>) {})(std::type_identity<T>()); };
 }
 
 #endif
