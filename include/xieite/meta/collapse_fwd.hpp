d8c97b8efd8deb36c66ab860aebe70d5f4ccc91f
Eczbek <eczbek.void@gmail.com>
2025-03-27 21:03:42 -0400

    Fix `xieite::collapse_fwd()`

--- include/xieite/meta/collapse_fwd.hpp
+++ include/xieite/meta/collapse_fwd.hpp
@@ -6,7 +6,7 @@
 namespace xieite {
 	template<typename T, typename U>
 	[[nodiscard]] constexpr auto&& collapse_fwd(U&& x) noexcept {
-		return static_cast<xieite::collapse_ref<T, U>>(x);
+		return static_cast<xieite::collapse_ref<T, U&&>>(x);
 	}
 }
 
