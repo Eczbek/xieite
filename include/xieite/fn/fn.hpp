9781f211b564c09de69be9a694385c4a735856fb
Eczbek <eczbek.void@gmail.com>
2025-04-03 17:12:41 -0400

    Allow specifying `noexcept` for `xieite::fn {}`

--- include/xieite/fn/fn.hpp
+++ include/xieite/fn/fn.hpp
@@ -14,8 +14,8 @@ namespace xieite {
 	template<typename>
 	struct fn;
 
-	template<typename Ret, typename... Args>
-	struct fn<Ret(Args...)> {
+	template<typename Ret, typename... Args, bool noex>
+	struct fn<Ret(Args...) noexcept(noex)> {
 	public:
 		[[nodiscard]] fn() = default;
 
@@ -25,13 +25,13 @@ namespace xieite {
 		template<xieite::is_invoc<Ret(Args...)> F>
 		requires(!std::same_as<std::remove_cvref_t<F>, xieite::fn<Ret(Args...)>>)
 		[[nodiscard]] constexpr fn(F&& fn) noexcept
-		: ptr(std::make_unique<xieite::fn<Ret(Args...)>::derived<std::remove_cvref_t<F>>>(XIEITE_FWD(fn))) {}
+		: ptr(std::make_unique<xieite::fn<Ret(Args...) noexcept(noex)>::derived<std::remove_cvref_t<F>>>(XIEITE_FWD(fn))) {}
 
 		[[nodiscard]] explicit constexpr operator bool() const noexcept {
 			return static_cast<bool>(this->ptr);
 		}
 
-		constexpr Ret operator()(Args... args) const noexcept(false) {
+		constexpr Ret operator()(Args... args) const noexcept(noex) {
 			if constexpr (XIEITE_DEBUG) {
 				if (!this->ptr) {
 					throw std::bad_function_call();
@@ -44,9 +44,9 @@ namespace xieite {
 		struct base {
 			virtual constexpr ~base() = default;
 
-			virtual constexpr Ret operator()(Args...) const noexcept(false) = 0;
+			virtual constexpr Ret operator()(Args...) const noexcept(noex) = 0;
 
-			virtual constexpr std::unique_ptr<xieite::fn<Ret(Args...)>::base> clone() const noexcept = 0;
+			virtual constexpr std::unique_ptr<xieite::fn<Ret(Args...) noexcept(noex)>::base> clone() const noexcept = 0;
 		};
 
 		template<typename F>
@@ -56,16 +56,16 @@ namespace xieite {
 			[[nodiscard]] constexpr derived(xieite::is_ref_to<F> auto&& fn) noexcept
 			: fn(XIEITE_FWD(fn)) {}
 
-			constexpr Ret operator()(Args... args) const noexcept(false) override {
+			constexpr Ret operator()(Args... args) const noexcept(noex) override {
 				return std::invoke(this->fn, XIEITE_FWD(args)...);
 			}
 
-			[[nodiscard]] constexpr std::unique_ptr<xieite::fn<Ret(Args...)>::base> clone() const noexcept override {
+			[[nodiscard]] constexpr std::unique_ptr<xieite::fn<Ret(Args...) noexcept(noex)>::base> clone() const noexcept override {
 				return std::make_unique<derived>(this->fn);
 			}
 		};
 
-		std::unique_ptr<xieite::fn<Ret(Args...)>::base> ptr;
+		std::unique_ptr<xieite::fn<Ret(Args...) noexcept(noex)>::base> ptr;
 	};
 }
 
