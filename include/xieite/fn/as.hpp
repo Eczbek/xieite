e1f67ff081e170a51dca65b207b8011dcf76aca2
Eczbek <eczbek.void@gmail.com>
2025-04-09 14:28:03 -0400

    Rewrite `xieite::as<>` without `XIEITE_ARROW()`

--- include/xieite/fn/as.hpp
+++ include/xieite/fn/as.hpp
@@ -2,7 +2,6 @@
 #	define DETAIL_XIEITE_HEADER_FN_AS
 #
 #	include <type_traits>
-#	include "../pp/arrow.hpp"
 #	include "../trait/add_c.hpp"
 #	include "../trait/add_cv.hpp"
 #	include "../trait/add_v.hpp"
@@ -15,33 +14,40 @@
 
 namespace xieite {
 	template<typename T>
-	constexpr auto as = [][[nodiscard]](std::type_identity_t<T> x) static
-		XIEITE_ARROW(x);
-
-	constexpr auto as_c = []<typename T>[[nodiscard]](T&& x) static
-		XIEITE_ARROW(const_cast<xieite::add_c<T&&>>(x));
-
-	constexpr auto as_cv = []<typename T>[[nodiscard]](T&& x) static
-		XIEITE_ARROW(const_cast<xieite::add_cv<T&&>>(x));
-
-	constexpr auto as_v = []<typename T>[[nodiscard]](T&& x) static
-		XIEITE_ARROW(const_cast<xieite::add_v<T&&>>(x));
-
-	constexpr auto as_not_c = []<typename T>[[nodiscard]](T&& x) static
-		XIEITE_ARROW(const_cast<xieite::rm_c<T&&>>(x));
-
-	constexpr auto as_not_cv = []<typename T>[[nodiscard]](T&& x) static
-		XIEITE_ARROW(const_cast<xieite::rm_cv<T&&>>(x));
-
-	constexpr auto as_not_v = []<typename T>[[nodiscard]](T&& x) static
-		XIEITE_ARROW(const_cast<xieite::rm_v<T&&>>(x));
-
-	constexpr auto as_sign = []<xieite::is_arith T>(T x) static noexcept {
-		return static_cast<xieite::try_sign<T>>(x);
+	constexpr auto as = [][[nodiscard]](std::type_identity_t<T> x) static noexcept {
+		return x;
 	};
 
-	constexpr auto as_unsign = []<xieite::is_arith T>(T x) static noexcept {
+	inline constexpr auto as_c = []<typename T>[[nodiscard]](T&& x) static noexcept {
+		return const_cast<xieite::add_c<T&&>>(x);
+	};
+
+	inline constexpr auto as_v = []<typename T>[[nodiscard]](T&& x) static noexcept {
+		return const_cast<xieite::add_v<T&&>>(x);
+	};
+
+	inline constexpr auto as_cv = []<typename T>[[nodiscard]](T&& x) static noexcept {
+		return const_cast<xieite::add_cv<T&&>>(x);
+	};
+
+	inline constexpr auto as_not_c = []<typename T>[[nodiscard]](T&& x) static noexcept {
+		return const_cast<xieite::rm_c<T&&>>(x);
+	};
+
+	inline constexpr auto as_not_v = []<typename T>[[nodiscard]](T&& x) static noexcept {
+		return const_cast<xieite::rm_v<T&&>>(x);
+	};
+
+	inline constexpr auto as_not_cv = []<typename T>[[nodiscard]](T&& x) static noexcept {
+		return const_cast<xieite::rm_cv<T&&>>(x);
+	};
+
+	inline constexpr auto as_unsign = []<xieite::is_arith T>(T x) static noexcept {
 		return static_cast<xieite::try_unsign<T>>(x);
+	};
+
+	inline constexpr auto as_sign = []<xieite::is_arith T>(T x) static noexcept {
+		return static_cast<xieite::try_sign<T>>(x);
 	};
 }
 
