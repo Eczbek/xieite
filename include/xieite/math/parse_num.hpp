6bf4c0cad14cdb8b73bae874e12454adbdd389c2
Eczbek <eczbek.void@gmail.com>
2025-04-10 17:18:22 -0400

    Make `xieite::parse_num()` accept a `std::string_view&` instead of a `std::size_t&`

--- include/xieite/math/parse_num.hpp
+++ include/xieite/math/parse_num.hpp
@@ -16,33 +16,33 @@
 
 namespace xieite {
 	template<xieite::is_arith T>
-	[[nodiscard]] constexpr T parse_num(std::size_t& out_size, std::string_view str, std::conditional_t<std::floating_point<T>, xieite::ssize_t, T> radix = 10, const xieite::num_str_config& config = {}) noexcept {
+	[[nodiscard]] constexpr T parse_num(std::string_view& strv, std::conditional_t<std::floating_point<T>, xieite::ssize_t, T> radix = 10, const xieite::num_str_config& config = {}) noexcept {
 		if (!radix) {
 			return 0;
 		}
-		const std::size_t size = str.size();
+		const std::size_t size = strv.size();
 		const std::string_view digits = config.digit.substr(0, xieite::abs(radix));
 		std::size_t i = 0;
-		while ((i < size) && !digits.contains(str[i]) && !((i < (size - 1)) && (config.minus.contains(str[i]) || config.plus.contains(str[i])) && digits.contains(str[i + 1]))) {
+		while ((i < size) && !digits.contains(strv[i]) && !((i < (size - 1)) && (config.minus.contains(strv[i]) || config.plus.contains(strv[i])) && digits.contains(strv[i + 1]))) {
 			++i;
 		}
-		const bool neg = (i < (size - 1)) && config.minus.contains(str[i]) && digits.contains(str[i + 1]);
-		i += neg || ((i < (size - 1)) && config.plus.contains(str[i]) && digits.contains(str[i + 1]));
+		const bool neg = (i < (size - 1)) && config.minus.contains(strv[i]) && digits.contains(strv[i + 1]);
+		i += neg || ((i < (size - 1)) && config.plus.contains(strv[i]) && digits.contains(strv[i + 1]));
 		if constexpr (std::floating_point<T>) {
 			T integral = 0;
 			T fractional = 0;
 			std::size_t point = 0;
 			xieite::ssize_t pow = 0;
-			for (; i < str.size(); ++i) {
-				const std::size_t digit = digits.find(str[i]);
+			for (; i < strv.size(); ++i) {
+				const std::size_t digit = digits.find(strv[i]);
 				if (digit == std::string::npos) {
-					if (config.pt.contains(str[i])) {
+					if (config.pt.contains(strv[i])) {
 						if (point) {
 							break;
 						}
 						point = 1;
-					} else if (config.e.contains(str[i])) {
-						pow = xieite::parse_num<xieite::ssize_t>(str.substr(i + 1), radix, config);
+					} else if (config.e.contains(strv[i])) {
+						pow = xieite::parse_num<xieite::ssize_t>(strv.substr(i + 1), radix, config);
 						break;
 					}
 					continue;
@@ -51,25 +51,25 @@ namespace xieite {
 				part = part * static_cast<T>(radix) + static_cast<T>(digit);
 				point += !!point;
 			}
-			out_size += i;
+			strv.remove_prefix(i);
 			return xieite::split_bool(!neg) * (integral + fractional / xieite::pow(radix, static_cast<xieite::ssize_t>(point - 1))) * xieite::pow(radix, pow);
 		} else {
 			T result = 0;
-			for (; i < str.size(); ++i) {
-				const std::size_t digit = digits.find(str[i]);
+			for (; i < strv.size(); ++i) {
+				const std::size_t digit = digits.find(strv[i]);
 				if (digit == std::string::npos) {
 					break;
 				}
 				result = static_cast<T>(result * static_cast<T>(radix) + static_cast<T>(digit));
 			}
-			out_size += i;
+			strv.remove_prefix(i);
 			return neg ? -result : result;
 		}
 	}
 
 	template<xieite::is_arith T>
-	[[nodiscard]] constexpr T parse_num(std::string_view str, std::conditional_t<std::floating_point<T>, xieite::ssize_t, T> radix = 10, const xieite::num_str_config& config = {}) noexcept {
-		return xieite::parse_num<T>(xieite::tmp(0uz), str, radix, config);
+	[[nodiscard]] constexpr T parse_num(const std::convertible_to<std::string_view> auto& strv, std::conditional_t<std::floating_point<T>, xieite::ssize_t, T> radix = 10, const xieite::num_str_config& config = {}) noexcept {
+		return xieite::parse_num<T>(xieite::tmp<std::string_view>(strv), radix, config);
 	}
 }
 
