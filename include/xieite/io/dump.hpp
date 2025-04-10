34a9191b000ff9407b22ce5cdcb34f7bac4db7cd
Eczbek <eczbek.void@gmail.com>
2025-04-10 15:52:43 -0400

    Allow custom delimiters in `xieite::dump()`

--- include/xieite/io/dump.hpp
+++ include/xieite/io/dump.hpp
@@ -1,20 +1,22 @@
 #ifndef DETAIL_XIEITE_HEADER_IO_DUMP
 #	define DETAIL_XIEITE_HEADER_IO_DUMP
 #
-#	include <array>
 #	include <cstddef>
 #	include <format>
 #	include <print>
 #	include <string_view>
+#	include "../ctnr/fixed_array.hpp"
+#	include "../ctnr/fixed_str.hpp"
 #	include "../fn/unroll.hpp"
 #	include "../pp/fwd.hpp"
 
 namespace xieite {
-	template<typename... Args>
+	template<xieite::fixed_str delim = " ", typename... Args>
 	void dump(Args&&... args) noexcept {
-		static constexpr std::size_t size = sizeof...(Args) * 3 - !!sizeof...(Args);
-		static constexpr std::array<char, size> data = xieite::unroll<size>([]<std::size_t... i> static {
-			return std::array<char, size> { "{} "[i % 3]... };
+		static constexpr auto fmt = xieite::fixed_str("{}") + delim;
+		static constexpr std::size_t size = sizeof...(Args) * fmt.size() - (!!sizeof...(Args) * delim.size());
+		static constexpr auto data = xieite::unroll<size>([]<std::size_t... i> static {
+			return xieite::fixed_array { fmt[i % fmt.size()]... };
 		});
 		std::println(std::format_string<Args...>(std::string_view(data.data(), size)), XIEITE_FWD(args)...);
 	}
