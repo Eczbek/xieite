e04f7cc161b021f82cd98f9bd572b86b089f923b
Eczbek <eczbek.void@gmail.com>
2025-04-25 13:38:08 -0400

    Make analog literals evaluate to a `std::size_t`

--- include/xieite/fn/analog.hpp
+++ include/xieite/fn/analog.hpp
@@ -1,11 +1,10 @@
 #ifndef DETAIL_XIEITE_HEADER_FN_ANALOG
 #	define DETAIL_XIEITE_HEADER_FN_ANALOG
 
-namespace DETAIL_XIEITE::analog {
-	template<int>
-	struct point;
+#	include <cstddef>
 
-	template<int w, int h, int d>
+namespace DETAIL_XIEITE::analog {
+	template<std::size_t w, std::size_t h, std::size_t d>
 	struct size {
 		[[nodiscard]] constexpr auto operator!() const noexcept {
 			return DETAIL_XIEITE::analog::size<w, (h + 1), d>();
@@ -15,16 +14,43 @@ namespace DETAIL_XIEITE::analog {
 			return DETAIL_XIEITE::analog::size<w, h, (d + 1)>();
 		}
 
-		[[nodiscard]] constexpr auto operator+() const noexcept {
-			return DETAIL_XIEITE::analog::size<w, h, d>();
-		}
-
 		[[nodiscard]] constexpr auto operator-() const noexcept {
 			return DETAIL_XIEITE::analog::size<w, h, d>();
 		}
 
-		template<int x>
-		[[nodiscard]] friend constexpr int operator-(DETAIL_XIEITE::analog::point<x>, DETAIL_XIEITE::analog::size<w, h, d>) noexcept {
+		[[nodiscard]] constexpr auto operator+() const noexcept {
+			return DETAIL_XIEITE::analog::size<w, h, d>();
+		}
+	};
+
+	template<std::size_t x>
+	struct point {
+		[[nodiscard]] constexpr auto operator-() const noexcept {
+			return DETAIL_XIEITE::analog::point<(x + 1)>();
+		}
+
+		[[nodiscard]] constexpr auto operator!() const noexcept {
+			return DETAIL_XIEITE::analog::point<(x + 1)>();
+		}
+
+		[[nodiscard]] constexpr auto operator*() const noexcept {
+			return DETAIL_XIEITE::analog::size<0, 1, 1>();
+		}
+
+		[[nodiscard]] constexpr auto operator+() const noexcept {
+			return DETAIL_XIEITE::analog::size<x, 0, 0>();
+		}
+
+		[[nodiscard]] friend constexpr std::size_t operator-(DETAIL_XIEITE::analog::point<0>, DETAIL_XIEITE::analog::point<x>) noexcept {
+			return x + 1;
+		}
+
+		[[nodiscard]] friend constexpr std::size_t operator|(DETAIL_XIEITE::analog::point<0>, DETAIL_XIEITE::analog::point<x>) noexcept {
+			return x + 1;
+		}
+
+		template<std::size_t w, std::size_t h, std::size_t d>
+		[[nodiscard]] friend constexpr std::size_t operator-(DETAIL_XIEITE::analog::point<x>, DETAIL_XIEITE::analog::size<w, h, d>) noexcept {
 			if constexpr (h) {
 				if constexpr (d) {
 					return w * (h / 3) * (d / 3);
@@ -36,42 +62,17 @@ namespace DETAIL_XIEITE::analog {
 			}
 		}
 
-		template<int x>
-		[[nodiscard]] friend constexpr int operator|(DETAIL_XIEITE::analog::point<x>, DETAIL_XIEITE::analog::size<w, h, d>) noexcept {
+		template<std::size_t w, std::size_t h, std::size_t d>
+		[[nodiscard]] friend constexpr std::size_t operator|(DETAIL_XIEITE::analog::point<x>, DETAIL_XIEITE::analog::size<w, h, d>) noexcept {
 			return (h / 2 + 1) * (d / 2);
-		}
-	};
-
-	template<int x = 0>
-	struct point {
-		[[nodiscard]] constexpr auto operator-() const noexcept {
-			return DETAIL_XIEITE::analog::point<(x + 1)>();
-		}
-
-		[[nodiscard]] constexpr auto operator!() const noexcept {
-			return DETAIL_XIEITE::analog::point<(x + 1)>();
-		}
-
-		[[nodiscard]] friend constexpr int operator-(DETAIL_XIEITE::analog::point<0>, DETAIL_XIEITE::analog::point<x>) noexcept {
-			return x + 1;
-		}
-
-		[[nodiscard]] friend constexpr int operator|(DETAIL_XIEITE::analog::point<0>, DETAIL_XIEITE::analog::point<x>) noexcept {
-			return x + 1;
-		}
-
-		[[nodiscard]] constexpr auto operator+() const noexcept {
-			return DETAIL_XIEITE::analog::size<x, 0, 0>();
-		}
-
-		[[nodiscard]] constexpr auto operator*() const noexcept {
-			return DETAIL_XIEITE::analog::size<0, 1, 1>();
 		}
 	};
 }
 
 namespace xieite::analog {
-	inline constexpr DETAIL_XIEITE::analog::point<> x;
+	inline constexpr DETAIL_XIEITE::analog::point<0> x;
 }
 
 #endif
+
+// http://www.eelis.net/C++/analogliterals.xhtml
