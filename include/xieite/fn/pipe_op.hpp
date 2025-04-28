aae6503a0eb90190410057331d65d14ed167bddf
Eczbek <eczbek.void@gmail.com>
2025-04-28 14:17:05 -0400

    Add `xieite::pipe_op::operator*()`

--- include/xieite/fn/pipe_op.hpp
+++ include/xieite/fn/pipe_op.hpp
@@ -8,6 +8,11 @@
 namespace xieite::pipe_op {
 	[[nodiscard]] constexpr auto operator|(auto&& arg, auto&& fn)
 		XIEITE_ARROW(std::invoke(XIEITE_FWD(fn), XIEITE_FWD(arg)))
+
+	[[nodiscard]] constexpr auto operator*(auto&& fn0, auto&& fn1) noexcept {
+		return [&fn0, &fn1][[nodiscard]](auto&&... args) mutable
+			XIEITE_ARROW(std::invoke(XIEITE_FWD(fn1), std::invoke(XIEITE_FWD(fn0), XIEITE_FWD(args)...)));
+	}
 }
 
 #endif
