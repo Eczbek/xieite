b5a7697ee5f19dfa9f96d6fd0d326b28a2b12a9d
Eczbek <eczbek.void@gmail.com>
2025-05-01 13:32:09 -0400

    Add `xieite::is_enum_value<>`

--- include/xieite/trait/is_complete.hpp
+++ include/xieite/trait/is_complete.hpp
@@ -1,11 +1,9 @@
 #ifndef DETAIL_XIEITE_HEADER_TRAIT_IS_COMPLETE
 #	define DETAIL_XIEITE_HEADER_TRAIT_IS_COMPLETE
-#
-#	include <type_traits>
 
 namespace xieite {
 	template<typename T, auto = [] {}>
-	concept is_complete = requires { ([]<typename U, auto = sizeof(U)>(std::type_identity<U>) {})(std::type_identity<T>()); };
+	concept is_complete = requires { ([]<typename U, auto = sizeof(U)> {}).template operator()<T>(); };
 }
 
 #endif
---
+++ include/xieite/trait/is_enum_value.hpp
@@ -0,0 +1,17 @@
+#ifndef DETAIL_XIEITE_HEADER_TRAIT_IS_ENUM_VALUE
+#	define DETAIL_XIEITE_HEADER_TRAIT_IS_ENUM_VALUE
+#
+#	include <concepts>
+#	include "../meta/name.hpp"
+#	include "../meta/value.hpp"
+#	include "../trait/is_enum.hpp"
+
+namespace xieite {
+	template<auto x, typename Enum>
+	concept is_enum_value =
+		xieite::is_enum<Enum>
+		&& requires { ([]<std::integral auto y, auto = static_cast<Enum>(y)> {}).template operator()<x>(); }
+		&& (xieite::name<static_cast<Enum>(x)>()[0] != '(');
+}
+
+#endif
