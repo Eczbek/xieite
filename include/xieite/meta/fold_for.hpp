927d1266db27651df7fa5c74949450859f420c40
Eczbek <eczbek.void@gmail.com>
2025-05-01 10:01:42 -0400

    Fix `xieite::fold_for<>`

--- include/xieite/meta/fold_for.hpp
+++ include/xieite/meta/fold_for.hpp
@@ -2,6 +2,7 @@
 #	define DETAIL_XIEITE_HEADER_META_FOLD_FOR
 #
 #	include <cstddef>
+#	include <type_traits>
 #	include "../fn/unroll.hpp"
 #	include "../meta/fold.hpp"
 #	include "../meta/value.hpp"
@@ -10,10 +11,10 @@ namespace DETAIL_XIEITE::fold_for {
 	template<auto fn, typename T>
 	constexpr auto impl = []<std::size_t... i> static {
 		return xieite::fold<
-			[]<typename U, typename Idx> static {
-				return std::type_identity<typename decltype(fn.template operator()<U, Idx::value>())::type>();
+			[]<typename Prev, typename Idx> static {
+				return std::type_identity<decltype(fn.template operator()<typename Prev::type, Idx::value>())>();
 			},
-			T,
+			std::type_identity<T>,
 			xieite::value<i>...
 		>();
 	};
