e9c18081b45772862b8e655d50a272cfbbb290bf
Eczbek <eczbek.void@gmail.com>
2025-05-02 16:46:34 -0400

    Make `XIEITE_OBJ()` wrap arguments in a type alias

--- include/xieite/pp/obj.hpp
+++ include/xieite/pp/obj.hpp
@@ -2,8 +2,14 @@
 #	define DETAIL_XIEITE_HEADER_PP_OBJ
 #
 #	ifdef __cplusplus
-#		define XIEITE_OBJ(type_) type_
+#		define XIEITE_OBJ(...) DETAIL_XIEITE::OBJ::paren<__VA_ARGS__>
+
+namespace DETAIL_XIEITE::OBJ {
+	template<typename T>
+	using paren = T;
+}
+
 #	else
-#		define XIEITE_OBJ(type_) (type_)
+#		define XIEITE_OBJ(...) (__VA_ARGS__)
 #	endif
 #endif
