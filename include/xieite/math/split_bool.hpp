c26de43885e3753b906654677ca6e002c56bee59
Eczbek <eczbek.void@gmail.com>
2025-05-10 18:23:40 -0400

    Allow easily casting `xieite::split_bool()` output

--- include/xieite/math/split_bool.hpp
+++ include/xieite/math/split_bool.hpp
@@ -2,10 +2,12 @@
 #	define DETAIL_XIEITE_HEADER_MATH_SPLIT_BOOL
 #
 #	include "../math/ssize_t.hpp"
+#	include "../trait/is_arith.hpp"
 
 namespace xieite {
-	[[nodiscard]] constexpr xieite::ssize_t split_bool(bool x) noexcept {
-		return x * 2 - 1;
+	template<xieite::is_arith Arith = xieite::ssize_t>
+	[[nodiscard]] constexpr Arith split_bool(bool x) noexcept {
+		return static_cast<Arith>(x * 2 - 1);
 	}
 }
 
