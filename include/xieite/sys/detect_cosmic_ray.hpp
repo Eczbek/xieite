3a40cd1ef340a4c2b27efc145bda9e7da11b1ebb
Eczbek <eczbek.void@gmail.com>
2025-05-13 11:04:44 -0400

    Fix array size in `xieite::detect_cosmic_ray()`

--- include/xieite/sys/detect_cosmic_ray.hpp
+++ include/xieite/sys/detect_cosmic_ray.hpp
@@ -7,8 +7,8 @@
 
 namespace xieite {
 	inline void detect_cosmic_ray(std::size_t bytes) noexcept {
-		const std::size_t size = bytes / sizeof(std::intmax_t);
-		const auto detector = std::make_unique<volatile const char[]>(size);
+		const std::size_t size = bytes / sizeof(std::uintmax_t);
+		const auto detector = std::make_unique<volatile const std::uintmax_t[]>(size);
 		for (std::size_t i = 0; !detector[++i %= size];);
 	}
 }
