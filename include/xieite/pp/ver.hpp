8991eda3aa163a87844829aadb6a035c038ccd68
Eczbek <eczbek.void@gmail.com>
2025-05-14 09:10:45 -0400

    Detect LLVM's libc

--- include/xieite/pp/ver.hpp
+++ include/xieite/pp/ver.hpp
@@ -5,7 +5,7 @@
 #
 #	define XIEITE_VER_MAJOR 0
 #	define XIEITE_VER_MINOR 83
-#	define XIEITE_VER_PATCH 1
+#	define XIEITE_VER_PATCH 2
 #
 #	define XIEITE_VER(cmp, major, ...) XIEITE_VER_CMP(cmp, XIEITE_VER_MAJOR, XIEITE_VER_MINOR, XIEITE_VER_PATCH, major, __VA_ARGS__)
 #endif
