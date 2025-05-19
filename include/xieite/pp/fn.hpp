6724f57233b50523193d17b19a5a59ad936bb045
Eczbek <eczbek.void@gmail.com>
2025-05-19 12:20:25 -0400

    Simplify `XIEITE_FN()`

--- include/xieite/pp/fn.hpp
+++ include/xieite/pp/fn.hpp
@@ -1,75 +1,29 @@
 #ifndef DETAIL_XIEITE_HEADER_PP_FN
 #	define DETAIL_XIEITE_HEADER_PP_FN
 #
-#	include <cstddef>
-#	include <type_traits>
-#	include "../pp/any.hpp"
 #	include "../pp/arrow.hpp"
 #	include "../pp/comma.hpp"
 #	include "../pp/diagnostic.hpp"
-#	include "../pp/each.hpp"
 #	include "../pp/fwd.hpp"
 #	include "../pp/if.hpp"
 #	include "../pp/opt.hpp"
-#	include "../pp/seq.hpp"
+#	include "../pp/repeat.hpp"
 #	include "../pp/unwrap.hpp"
 #
-#	define XIEITE_FN(...) ([]DETAIL_XIEITE_FN(0, 0, __VA_ARGS__))
-#	define XIEITE_FN_THIS(...) ([]DETAIL_XIEITE_FN(0, 1, __VA_ARGS__))
-#	define XIEITE_FN_LOCAL(captures_, ...) ([XIEITE_UNWRAP(captures_)]DETAIL_XIEITE_FN(XIEITE_ANY(captures_), 1, __VA_ARGS__))
-#	define XIEITE_FN_MUT(captures_, ...) (DETAIL_XIEITE::FN::indirect { [XIEITE_UNWRAP(captures_)] mutable { return [&]DETAIL_XIEITE_FN(1, 1, __VA_ARGS__); } })
+#	define XIEITE_FN(...) DETAIL_XIEITE_FN(, 0, __VA_ARGS__)
+#	define XIEITE_FN_THIS(...) DETAIL_XIEITE_FN(, 1, __VA_ARGS__)
+#	define XIEITE_FN_LOCAL(captures_, ...) DETAIL_XIEITE_FN(captures_, 1, __VA_ARGS__)
+#	define XIEITE_FN_MUT(captures_, ...) (DETAIL_XIEITE::FN::indirect { [XIEITE_UNWRAP(captures_)] mutable { return DETAIL_XIEITE_FN(&, 1, __VA_ARGS__); } })
 
 XIEITE_DIAGNOSTIC_OFF_CLANG("-Wdollar-in-identifier-extension")
 
-#	define DETAIL_XIEITE_FN(has_captures_, this_, ...) \
-	< \
-		typename... __VA_OPT__($$ XIEITE_IF(this_)(XIEITE_COMMA() \
-		typename $$this)(), \
-		XIEITE_SEQ(16, DETAIL_XIEITE_FN_TEMPLATE_PARAM)) \
-	>__VA_OPT__([[nodiscard]])(__VA_OPT__( \
-		XIEITE_IF(this_)([[maybe_unused]] this $$this&&F $this XIEITE_COMMA())() \
-		[[maybe_unused]]) auto&&... __VA_OPT__($) \
-	) XIEITE_IF(has_captures_)()(XIEITE_OPT(__VA_ARGS__)(XIEITE_IF(this_)()(static))(static)()) \
-	noexcept __VA_OPT__((requires(XIEITE_IF(this_)($$this&& $this XIEITE_COMMA())() XIEITE_SEQ(16, DETAIL_XIEITE_FN_PARAM)) { requires(noexcept(__VA_ARGS__)); }) \
-	-> decltype(auto) \
-	requires(requires(XIEITE_IF(this_)($$this&& $this XIEITE_COMMA())() XIEITE_SEQ(16, DETAIL_XIEITE_FN_PARAM)) { __VA_ARGS__; })) { \
-		__VA_OPT__(XIEITE_EACH_DELIM(DETAIL_XIEITE_FN_REF,, XIEITE_SEQ(16)) \
-		return __VA_ARGS__;) \
-	}
-#	define DETAIL_XIEITE_FN_TEMPLATE_PARAM(i_) typename $$##i_ = DETAIL_XIEITE::FN::t<i_, $$...>::type
-#	define DETAIL_XIEITE_FN_PARAM(i_) decltype(DETAIL_XIEITE::FN::v<i_>(XIEITE_FWD($)...)) $##i_
-#	define DETAIL_XIEITE_FN_REF(i_) [[maybe_unused]] auto&& $##i_ = DETAIL_XIEITE::FN::v<i_>(XIEITE_FWD($)...);
+#	define DETAIL_XIEITE_FN(captures_, this_, ...) ([XIEITE_UNWRAP(captures_)]<class... __VA_OPT__($$ XIEITE_IF(this_)(XIEITE_COMMA() class $$this)())>__VA_OPT__([[nodiscard]])(__VA_OPT__(XIEITE_IF(this_)([[maybe_unused]] this $$this&& $this XIEITE_COMMA())()[[maybe_unused]]) auto&&... __VA_OPT__($)) XIEITE_OPT(captures_)()(XIEITE_OPT(__VA_ARGS__)(XIEITE_IF(this_)()(static))(static)) noexcept __VA_OPT__(DETAIL_XIEITE_FN_CHECK(this_, noexcept(__VA_ARGS__)) -> decltype(auto) requires DETAIL_XIEITE_FN_CHECK(this_, requires { __VA_ARGS__; })) DETAIL_XIEITE_FN_BODY(__VA_ARGS__))
+#	define DETAIL_XIEITE_FN_CHECK(this_, ...) (requires(XIEITE_IF(this_)($$this&& $this XIEITE_COMMA())() decltype($)&&... $) { requires([](XIEITE_IF(this_)([[maybe_unused]] auto&& $this XIEITE_COMMA())() auto&&... $) static DETAIL_XIEITE_FN_BODY(__VA_ARGS__))(XIEITE_IF(this_)(XIEITE_FWD($this) XIEITE_COMMA())() XIEITE_FWD($)...); })
+#	define DETAIL_XIEITE_FN_BODY(...) { XIEITE_DIAGNOSTIC_PUSH_GCC() XIEITE_DIAGNOSTIC_OFF_GCC("-Wunused-parameter") return ([&]<class $$0, class $$1, class $$2, class $$3, class $$4, class $$5, class $$6, class $$7, class $$8, class $$9, class $$A, class $$B, class $$C, class $$D, class $$E, class $$F>(auto&& $0, auto&& $1, auto&& $2, auto&& $3, auto&& $4, auto&& $5, auto&& $6, auto&& $7, auto&& $8, auto&& $9, auto&& $A, auto&& $B, auto&& $C, auto&& $D, auto&& $E, auto&& $F, auto...) mutable -> decltype(auto) { return __VA_ARGS__; }).template operator()<XIEITE_REPEAT(16, int)>(XIEITE_FWD($)..., XIEITE_REPEAT(16, 0)); XIEITE_DIAGNOSTIC_POP_GCC() }
+#	define DETAIL_XIEITE_FN_TEMPLATE_PARAMS(i_) class $$##i_
+#	define DETAIL_XIEITE_FN_PARAMS(i_) auto&&$##i_
 
 namespace DETAIL_XIEITE::FN {
-	template<std::size_t idx> // Template parameter must be named here due to GCC bug (https://gcc.gnu.org/bugzilla/show_bug.cgi?id=117422)
-	constexpr auto&& v(auto&&...) noexcept;
-
-	struct unusable {
-	public:
-		template<std::size_t idx>
-		[[nodiscard]] friend constexpr auto&& v(auto&&... args) noexcept {
-			if constexpr (idx < sizeof...(args)) {
-				return XIEITE_FWD(args...[idx]);
-			} else {
-				static constexpr DETAIL_XIEITE::FN::unusable unusable;
-				return unusable;
-			}
-		}
-
-	private:
-		unusable() = default;
-	};
-
-	template<std::size_t idx, typename... Ts>
-	struct t : decltype(([]<typename... Us> static {
-		if constexpr (idx < sizeof...(Us)) {
-			return std::type_identity<Us...[idx]>();
-		} else {
-			return std::type_identity<DETAIL_XIEITE::FN::unusable>();
-		}
-	}).template operator()<Ts...>()) {};
-
-	// Workaround for GCC bug (https://gcc.gnu.org/bugzilla/show_bug.cgi?id=117061)
 	template<typename Fn>
 	struct indirect : Fn {
 		template<typename... Ts>
