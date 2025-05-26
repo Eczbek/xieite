523846cc0554d075a7ec663e746318d5bda7ba6f
Eczbek <eczbek.void@gmail.com>
2025-05-26 18:41:02 -0400

    Wrap `XIEITE_LIFT()` in parentheses

--- include/xieite/pp/lift.hpp
+++ include/xieite/pp/lift.hpp
@@ -4,11 +4,11 @@
 #	include "../pp/arrow.hpp"
 #	include "../pp/fwd.hpp"
 #
-#	define XIEITE_LIFT(...) []DETAIL_XIEITE_LIFT(static, __VA_ARGS__)
-#	define XIEITE_LIFT_LOCAL(...) [&]DETAIL_XIEITE_LIFT(, __VA_ARGS__)
+#	define XIEITE_LIFT(...) ([]DETAIL_XIEITE_LIFT(static, __VA_ARGS__))
+#	define XIEITE_LIFT_LOCAL(...) ([&]DETAIL_XIEITE_LIFT(, __VA_ARGS__))
 #	define XIEITE_LIFT_MEMBER(...) \
-		[][[nodiscard]](auto&& DETAIL_XIEITE_obj, auto&&... DETAIL_XIEITE_args) static \
-			XIEITE_ARROW((XIEITE_FWD(DETAIL_XIEITE_obj) __VA_ARGS__(XIEITE_FWD(DETAIL_XIEITE_args)...)))
+		([][[nodiscard]](auto&& DETAIL_XIEITE_obj, auto&&... DETAIL_XIEITE_args) static \
+			XIEITE_ARROW((XIEITE_FWD(DETAIL_XIEITE_obj) __VA_ARGS__(XIEITE_FWD(DETAIL_XIEITE_args)...))))
 #
 #	define DETAIL_XIEITE_LIFT(specs_, ...) \
 		[[nodiscard]](auto&&... DETAIL_XIEITE_args) specs_ \
