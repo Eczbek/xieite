181ae18518579f194e075249351b1e53936bd254
Eczbek <eczbek.void@gmail.com>
2025-05-27 02:34:09 -0400

    Add compiler-specific pragma macros

--- include/xieite/pp/pragma.hpp
+++ include/xieite/pp/pragma.hpp
@@ -7,9 +7,25 @@
 #
 #	if XIEITE_COMPILER_TYPE_GCC || XIEITE_COMPILER_TYPE_CLANG || XIEITE_LANG_VER(CPP, >=, 2011) || XIEITE_LANG_VER(C, >=, 1999)
 #		define XIEITE_PRAGMA(...) _Pragma(XIEITE_STR(__VA_ARGS__))
-#	elif XIEITE_COMPILER_TYPE_MSVC
-#		define XIEITE_PRAGMA(...) __pragma(__VA_ARGS__)
 #	else
-#		define XIEITE_PRAGMA(...)
+#		define XIEITE_PRAGMA(...) __pragma(__VA_ARGS__)
+#	endif
+#	define XIEITE_PRAGMA_GCC(...)
+#	define XIEITE_PRAGMA_CLANG(...)
+#	define XIEITE_PRAGMA_MSVC(...)
+#
+#	if XIEITE_COMPILER_TYPE_GCC
+#		undef XIEITE_PRAGMA_GCC
+#		define XIEITE_PRAGMA_GCC(...) XIEITE_PRAGMA(__VA_ARGS__)
+#	endif
+
+#	if XIEITE_COMPILER_TYPE_CLANG
+#		undef XIEITE_PRAGMA_CLANG
+#		define XIEITE_PRAGMA_CLANG(...) XIEITE_PRAGMA(__VA_ARGS__)
+#	endif
+#
+#	if XIEITE_COMPILER_TYPE_MSVC
+#		undef XIEITE_PRAGMA_MSVC
+#		define XIEITE_PRAGMA_MSVC(...) XIEITE_PRAGMA(__VA_ARGS__)
 #	endif
 #endif
--- include/xieite/pp/ver.hpp
+++ include/xieite/pp/ver.hpp
@@ -3,7 +3,7 @@
 #
 #	define XIEITE_VER_MAJOR 0
 #	define XIEITE_VER_MINOR 91
-#	define XIEITE_VER_PATCH 2
+#	define XIEITE_VER_PATCH 3
 #	define XIEITE_VER(type, cmp, major, ...) DETAIL_XIEITE_VER(cmp, major, __VA_ARGS__ __VA_OPT__(,) 0, 0)
 #
 #	define DETAIL_XIEITE_VER(cmp, major, minor, patch, ...) (((((0 cmp 0) && !(0 cmp 1) && !(1 cmp 0)) || (!(0 cmp 0) && (0 cmp 1) && (1 cmp 0))) && (((XIEITE_VER_MAJOR) cmp (major)) && ((XIEITE_VER_MINOR) cmp (minor)) && ((XIEITE_VER_PATCH) cmp (patch)))) || ((XIEITE_VER_MAJOR) cmp (major)) || (((XIEITE_VER_MAJOR) == (major)) && ((XIEITE_VER_MINOR) cmp (minor))) || (((XIEITE_VER_MINOR) == (minor)) && ((XIEITE_VER_PATCH) cmp (patch))))
