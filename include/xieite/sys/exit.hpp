4fecf8287253484a4af8c33ea61c1b25efc2ca69
Eczbek <eczbek.void@gmail.com>
2025-06-16 18:21:40 -0400

    Rename some exit codes

--- include/xieite/sys/exit.hpp
+++ include/xieite/sys/exit.hpp
@@ -4,35 +4,35 @@
 namespace xieite::exit {
 	inline constexpr int ok = 0;
 	
-	inline constexpr int err_usage = 64;
+	inline constexpr int usage = 64;
 	
-	inline constexpr int err_input = 65;
+	inline constexpr int input = 65;
 	
-	inline constexpr int err_no_input = 66;
+	inline constexpr int no_input = 66;
 	
-	inline constexpr int err_no_user = 67;
+	inline constexpr int no_user = 67;
 	
-	inline constexpr int err_no_host = 68;
+	inline constexpr int no_host = 68;
 	
-	inline constexpr int err_unavail = 69;
+	inline constexpr int unavail = 69;
 	
-	inline constexpr int err_software = 70;
+	inline constexpr int software = 70;
 	
-	inline constexpr int err_os = 71;
+	inline constexpr int os = 71;
 	
-	inline constexpr int err_sys_file = 72;
+	inline constexpr int sys_file = 72;
 	
-	inline constexpr int err_cant_create = 73;
+	inline constexpr int cant_create = 73;
 	
-	inline constexpr int err_io = 74;
+	inline constexpr int io = 74;
 	
-	inline constexpr int err_tmp = 75;
+	inline constexpr int tmp = 75;
 	
-	inline constexpr int err_protocol = 76;
+	inline constexpr int protocol = 76;
 	
-	inline constexpr int err_no_perm = 77;
+	inline constexpr int no_perm = 77;
 	
-	inline constexpr int err_config = 78;
+	inline constexpr int config = 78;
 }
 
 #endif
