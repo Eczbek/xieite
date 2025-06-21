80e5f623fa06028111b607f6e7bb8bf0d04e455f
Eczbek <eczbek.void@gmail.com>
2025-06-21 17:01:16 -0400

    Make prefix keyword macros suffixes instead

--- include/xieite/pp/keywords.hpp
+++ include/xieite/pp/keywords.hpp
@@ -9,7 +9,7 @@
 #	define xnor ^ DETAIL_XIEITE::KEYWORDS::xnor_impl ^
 #	define imply || DETAIL_XIEITE::KEYWORDS::imply_impl ||
 #	define nimply && DETAIL_XIEITE::KEYWORDS::nimply_impl &&
-#	define eq_not DETAIL_XIEITE::KEYWORDS::eq_not_impl ->*
+#	define eq_not ->* DETAIL_XIEITE::KEYWORDS::eq_not_impl
 #	define nand_eq += DETAIL_XIEITE::KEYWORDS::nand_eq_impl +=
 #	define nor_eq += DETAIL_XIEITE::KEYWORDS::nor_eq_impl +=
 #	define xnor_eq += DETAIL_XIEITE::KEYWORDS::xnor_eq_impl +=
@@ -20,7 +20,7 @@
 #	define bitxnor ^ DETAIL_XIEITE::KEYWORDS::bitxnor_impl ^
 #	define bitimply | DETAIL_XIEITE::KEYWORDS::bitimply_impl |
 #	define bitnimply & DETAIL_XIEITE::KEYWORDS::bitnimply_impl &
-#	define eq_compl DETAIL_XIEITE::KEYWORDS::eq_compl_impl ->*
+#	define eq_compl ->* DETAIL_XIEITE::KEYWORDS::eq_compl_impl
 #	define bitand_eq += DETAIL_XIEITE::KEYWORDS::bitand_eq_impl +=
 #	define bitnand_eq += DETAIL_XIEITE::KEYWORDS::bitnand_eq_impl +=
 #	define bitor_eq += DETAIL_XIEITE::KEYWORDS::bitor_eq_impl +=
@@ -95,8 +95,8 @@ namespace DETAIL_XIEITE::KEYWORDS {
 	};
 
 	template<auto fn>
-	struct pfx {
-		[[nodiscard]] constexpr auto operator->*(auto&& rhs)
+	struct sfx {
+		[[nodiscard]] friend constexpr auto operator->*(auto&& rhs, DETAIL_XIEITE::KEYWORDS::sfx<fn>)
 			XIEITE_ARROW(fn(XIEITE_FWD(rhs)))
 	};
 
@@ -105,7 +105,7 @@ namespace DETAIL_XIEITE::KEYWORDS {
 	inline constexpr auto xnor_impl = DETAIL_XIEITE::KEYWORDS::ifx<([](auto&& x, auto&& y) static { return XIEITE_FWD(x) == XIEITE_FWD(y); })>();
 	inline constexpr auto imply_impl = DETAIL_XIEITE::KEYWORDS::ifx<([](auto&& x, auto&& y) static { return !XIEITE_FWD(x) || XIEITE_FWD(y); })>();
 	inline constexpr auto nimply_impl = DETAIL_XIEITE::KEYWORDS::ifx<([](auto&& x, auto&& y) static { return XIEITE_FWD(x) && !XIEITE_FWD(y); })>();
-	inline constexpr auto eq_not_impl = DETAIL_XIEITE::KEYWORDS::pfx<([](auto&& x) static { return x = !x; })>();
+	inline constexpr auto eq_not_impl = DETAIL_XIEITE::KEYWORDS::sfx<([](auto&& x) static { return x = !x; })>();
 	inline constexpr auto nand_eq_impl = DETAIL_XIEITE::KEYWORDS::ifx<([](auto&& x, auto&& y) static { return x = !x || !XIEITE_FWD(y); })>();
 	inline constexpr auto nor_eq_impl = DETAIL_XIEITE::KEYWORDS::ifx<([](auto&& x, auto&& y) static { return x = !x && !XIEITE_FWD(y); })>();
 	inline constexpr auto xnor_eq_impl = DETAIL_XIEITE::KEYWORDS::ifx<([](auto&& x, auto&& y) static { return x = x == XIEITE_FWD(y); })>();
@@ -116,7 +116,7 @@ namespace DETAIL_XIEITE::KEYWORDS {
 	inline constexpr auto bitxnor_impl = DETAIL_XIEITE::KEYWORDS::ifx<([](auto&& x, auto&& y) static { return ~(~XIEITE_FWD(x) ^ ~XIEITE_FWD(y)); })>();
 	inline constexpr auto bitimply_impl = DETAIL_XIEITE::KEYWORDS::ifx<([](auto&& x, auto&& y) static { return ~XIEITE_FWD(x) | XIEITE_FWD(y); })>();
 	inline constexpr auto bitnimply_impl = DETAIL_XIEITE::KEYWORDS::ifx<([](auto&& x, auto&& y) static { return XIEITE_FWD(x) & ~XIEITE_FWD(y); })>();
-	inline constexpr auto eq_compl_impl = DETAIL_XIEITE::KEYWORDS::pfx<([](auto&& x) static { return x = ~x; })>();
+	inline constexpr auto eq_compl_impl = DETAIL_XIEITE::KEYWORDS::sfx<([](auto&& x) static { return x = ~x; })>();
 	inline constexpr auto bitand_eq_impl = DETAIL_XIEITE::KEYWORDS::ifx<([](auto&& x, auto&& y) static { return x = x & XIEITE_FWD(y); })>();
 	inline constexpr auto bitnand_eq_impl = DETAIL_XIEITE::KEYWORDS::ifx<([](auto&& x, auto&& y) static { return x = x bitnand XIEITE_FWD(y); })>();
 	inline constexpr auto bitor_eq_impl = DETAIL_XIEITE::KEYWORDS::ifx<([](auto&& x, auto&& y) static { return x = x | XIEITE_FWD(y); })>();
