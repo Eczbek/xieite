63c5d768a01c12c44a91e26ec5ca6e82310e246f
Eczbek <eczbek.void@gmail.com>
2025-06-23 14:01:46 -0400

    Fix `XIEITE_AS` for non-default-constructible types

--- include/xieite/pp/as.hpp
+++ include/xieite/pp/as.hpp
@@ -1,33 +1,17 @@
 #ifndef DETAIL_XIEITE_HEADER_PP_AS
 #	define DETAIL_XIEITE_HEADER_PP_AS
 #
-#	include <cstddef>
 #	include "../pp/arrow.hpp"
 #	include "../pp/fwd.hpp"
 #
-#	define XIEITE_AS ->*DETAIL_XIEITE::AS::impl()->*::new(DETAIL_XIEITE::AS::impl())
+#	define XIEITE_AS(...) ->*DETAIL_XIEITE::AS::impl<__VA_ARGS__>()
 
 namespace DETAIL_XIEITE::AS {
 	template<typename T>
-	struct proxy {
-		T&& x;
-
-		template<typename U>
-		[[nodiscard]] constexpr auto operator->*(U*)
-			XIEITE_ARROW(static_cast<U>(XIEITE_FWD(this->x)))
-	};
-
 	struct impl {
-		explicit impl() = default;
-
-		friend constexpr auto operator->*(auto&& x, DETAIL_XIEITE::AS::impl) noexcept {
-			return DETAIL_XIEITE::AS::proxy(XIEITE_FWD(x));
-		}
+		[[nodiscard]] friend constexpr auto operator->*(auto&& x, DETAIL_XIEITE::AS::impl<T>)
+			XIEITE_ARROW(static_cast<T>(XIEITE_FWD(x)))
 	};
-}
-
-constexpr void* operator new(std::size_t, DETAIL_XIEITE::AS::impl) noexcept {
-	return nullptr;
 }
 
 #endif
