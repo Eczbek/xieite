523a017fc174af1992e842346097b59eab8cbfff
Eczbek <eczbek.void@gmail.com>
2025-06-23 10:47:48 -0400

    Avoid allocating in `XIEITE_AS`

--- include/xieite/pp/as.hpp
+++ include/xieite/pp/as.hpp
@@ -1,28 +1,33 @@
 #ifndef DETAIL_XIEITE_HEADER_PP_AS
 #	define DETAIL_XIEITE_HEADER_PP_AS
 #
-#	include <new>
+#	include <cstddef>
+#	include "../pp/arrow.hpp"
 #	include "../pp/fwd.hpp"
 #
-#	define XIEITE_AS ->*DETAIL_XIEITE::AS::impl()->*::new(::std::nothrow)
+#	define XIEITE_AS ->*DETAIL_XIEITE::AS::impl()->*::new(DETAIL_XIEITE::AS::impl())
 
 namespace DETAIL_XIEITE::AS {
 	template<typename T>
 	struct proxy {
-		T&& lhs;
+		T&& x;
 
 		template<typename U>
-		[[nodiscard]] constexpr auto operator->*(U* rhs) noexcept {
-			delete rhs;
-			return static_cast<U>(XIEITE_FWD(this->lhs));
-		}
+		[[nodiscard]] constexpr auto operator->*(U*)
+			XIEITE_ARROW(static_cast<U>(XIEITE_FWD(this->x)))
 	};
 
 	struct impl {
-		friend constexpr auto operator->*(auto&& lhs, DETAIL_XIEITE::AS::impl) noexcept {
-			return DETAIL_XIEITE::AS::proxy { XIEITE_FWD(lhs) };
+		explicit impl() = default;
+
+		friend constexpr auto operator->*(auto&& x, DETAIL_XIEITE::AS::impl) noexcept {
+			return DETAIL_XIEITE::AS::proxy(XIEITE_FWD(x));
 		}
 	};
+}
+
+constexpr void* operator new(std::size_t, DETAIL_XIEITE::AS::impl) noexcept {
+	return nullptr;
 }
 
 #endif
