72bfc1826f46e80ce4a62590234d2863b836540e
Eczbek <eczbek.void@gmail.com>
2025-07-01 12:28:38 -0400

    Make `xieite::ext_bit_cast()` more reliable on major compilers

--- include/xieite/math/ext_bit_cast.hpp
+++ include/xieite/math/ext_bit_cast.hpp
@@ -2,16 +2,23 @@
 #	define DETAIL_XIEITE_HEADER_MATH_EXT_BIT_CAST
 #
 #	include <algorithm>
-#	include <array>
 #	include <bit>
 #	include "../fn/unroll.hpp"
+#	include "../pp/attr.hpp"
+
+namespace DETAIL_XIEITE::ext_bit_cast {
+	template<typename T>
+	struct XIEITE_ATTR_PACK(array {
+		char data[sizeof(T)];
+	});
+}
 
 namespace xieite {
 	template<typename T, typename U>
 	[[nodiscard]] constexpr T ext_bit_cast(const U& x) noexcept {
 		return xieite::unroll<std::min(sizeof(T), sizeof(U))>([&x]<std::size_t... i> -> T {
-			const auto bits = std::bit_cast<std::array<char, sizeof(U)>>(x);
-			return std::bit_cast<T>(std::array<char, sizeof(T)> { bits[i]... });
+			const auto bits = std::bit_cast<DETAIL_XIEITE::ext_bit_cast::array<U>>(x);
+			return std::bit_cast<T>(DETAIL_XIEITE::ext_bit_cast::array<T> { bits[i]... });
 		});
 	}
 }
