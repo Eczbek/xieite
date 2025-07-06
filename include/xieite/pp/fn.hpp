67317585e05bd6fc0666ab43d699afeedf3f4bc7
Eczbek <eczbek.void@gmail.com>
2025-07-05 21:00:12 -0400

    Fix more formatting

--- include/xieite/pp/fn.hpp
+++ include/xieite/pp/fn.hpp
@@ -21,7 +21,7 @@ XIEITE_DIAGNOSTIC_OFF_CLANG("-Wdollar-in-identifier-extension")
 #	define DETAIL_XIEITE_FN_CHECK(_this, ...) (requires(XIEITE_IF(_this)($$this&& $this XIEITE_COMMA())() decltype($)&&... $) { requires([](XIEITE_IF(_this)([[maybe_unused]] auto&& $this XIEITE_COMMA())() auto&&... $) static DETAIL_XIEITE_FN_BODY(__VA_ARGS__))(XIEITE_IF(_this)(XIEITE_FWD($this) XIEITE_COMMA())() XIEITE_FWD($)...); })
 #	define DETAIL_XIEITE_FN_BODY(...) { __VA_OPT__(XIEITE_DIAGNOSTIC_PUSH_GCC() XIEITE_DIAGNOSTIC_OFF_GCC("-Wunused-parameter") return ([&]<class $$0, class $$1, class $$2, class $$3, class $$4, class $$5, class $$6, class $$7, class $$8, class $$9, class $$A, class $$B, class $$C, class $$D, class $$E, class $$F>(auto&& $0, auto&& $1, auto&& $2, auto&& $3, auto&& $4, auto&& $5, auto&& $6, auto&& $7, auto&& $8, auto&& $9, auto&& $A, auto&& $B, auto&& $C, auto&& $D, auto&& $E, auto&& $F, auto...) mutable -> decltype(auto) { return __VA_ARGS__; }).template operator()<XIEITE_REPEAT(16, int)>(XIEITE_FWD($)..., XIEITE_REPEAT(16, 0)); XIEITE_DIAGNOSTIC_POP_GCC()) }
 #	define DETAIL_XIEITE_FN_TEMPLATE_PARAMS(_n) class $$##_n
-#	define DETAIL_XIEITE_FN_PARAMS(_n) auto&&$##_n
+#	define DETAIL_XIEITE_FN_PARAMS(_n) auto&& $##_n
 
 namespace DETAIL_XIEITE::FN {
 	template<typename Fn>
