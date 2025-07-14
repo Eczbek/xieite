de19da5c7d7d89515db45fd33f380978c1a2b5f8
Eczbek <eczbek.void@gmail.com>
2025-07-14 12:51:22 -0400

    Fix `xieite::cmp_ignore_case()`

--- include/xieite/data/cmp_ignore_case.hpp
+++ include/xieite/data/cmp_ignore_case.hpp
@@ -8,25 +8,21 @@
 #	include "../trait/is_fwd_input_range.hpp"
 
 namespace xieite {
-	template<typename Range0, typename Range1>
-	requires(xieite::is_fwd_input_range<Range0, []<xieite::is_char> {}> && xieite::is_fwd_input_range<Range1, []<xieite::is_char> {}>)
-	[[nodiscard]] constexpr std::strong_ordering cmp_ignore_case(Range0&& lhs, Range1&& rhs) noexcept {
+	[[nodiscard]] constexpr std::strong_ordering cmp_ignore_case(xieite::is_fwd_input_range<[]<xieite::is_char> {}> auto&& lhs, xieite::is_fwd_input_range<[]<xieite::is_char> {}> auto&& rhs) noexcept {
 		auto iter0 = std::ranges::begin(lhs);
 		auto iter1 = std::ranges::begin(rhs);
+		const auto end0 = std::ranges::end(lhs);
+		const auto end1 = std::ranges::end(rhs);
 		while (true) {
-			if (iter0 == lhs.end()) {
-				return std::strong_ordering::less;
+			if ((iter0 == end0) || (iter1 == end1)) {
+				return (iter0 == end0) <=> (iter1 == end1);
 			}
-			if (iter1 == rhs.end()) {
-				return std::strong_ordering::greater;
-			}
-			if (std::strong_ordering order = xieite::toupper(*iter0) <=> xieite::toupper(*iter1); !std::is_eq(order)) {
+			if (const std::strong_ordering order = xieite::toupper(*iter0) <=> xieite::toupper(*iter1); !std::is_eq(order)) {
 				return order;
 			}
 			++iter0;
 			++iter1;
 		}
-		return std::strong_ordering::equal;
 	}
 }
 
