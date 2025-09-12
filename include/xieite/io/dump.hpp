c802d4233c59781c34955588fe88fd689de8e8c9
Eczbek <eczbek.void@gmail.com>
2025-09-12 19:29:33 -0400

    Fix `xieite::dump()` for delimiters containing `{` or `}`

--- include/xieite/io/dump.hpp
+++ include/xieite/io/dump.hpp
@@ -11,18 +11,27 @@
 #	include "../fn/unroll.hpp"
 #	include "../pp/fwd.hpp"
 
+using namespace std::literals;
+
 namespace xieite {
 	template<xieite::fixed_str delim = " ", bool newline = true, typename... Args>
 	void dump(std::FILE* file, Args&&... args) noexcept {
-		static constexpr auto segment = xieite::fixed_str("{}") + delim;
-		static constexpr std::size_t size = sizeof...(Args) * segment.size() - (!!sizeof...(Args) * delim.size());
-		static constexpr auto fmt = xieite::unroll<size>([]<std::size_t... i> {
-			return xieite::fixed_array { segment[i % segment.size()]... };
+		static constexpr std::size_t fmt_size = ([] -> std::size_t {
+			std::size_t offset = 0;
+			for (char c : delim.view()) {
+				offset += "{}"sv.contains(c);
+			}
+			return sizeof...(Args) * 2 + (sizeof...(Args) - !!sizeof...(Args)) * (delim.size() + offset);
+		})();
+		static constexpr auto fmt = xieite::unroll<fmt_size>([]<std::size_t... i> {
+			std::size_t j = -1uz;
+			bool flag = false;
+			return xieite::fixed_array { (void(i), (((j += !flag) %= (2 + delim.size())) < 2) ? "{}"[j] : delim[j - 2 - (flag = !flag && (j > 2) && "{}"sv.contains(delim[j - 3]))])... };
 		});
 		if constexpr (newline) {
-			std::println(file, std::format_string<Args...>(std::string_view(fmt.data(), size)), XIEITE_FWD(args)...);
+			std::println(file, std::format_string<Args...>(std::string_view(fmt.data(), fmt_size)), XIEITE_FWD(args)...);
 		} else {
-			std::print(file, std::format_string<Args...>(std::string_view(fmt.data(), size)), XIEITE_FWD(args)...);
+			std::print(file, std::format_string<Args...>(std::string_view(fmt.data(), fmt_size)), XIEITE_FWD(args)...);
 		}
 	}
 
