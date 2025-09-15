2ba29ec56cdc6f28a140bf33a1bb3449b3422115
Eczbek <eczbek.void@gmail.com>
2025-09-14 20:55:06 -0400

    Fix `xieite::range_udl::_range()`

--- include/xieite/fn/range_udl.hpp
+++ include/xieite/fn/range_udl.hpp
@@ -11,23 +11,25 @@
 #	include "../math/ssize_t.hpp"
 
 namespace DETAIL_XIEITE::range_udl {
-	template<auto one_past, decltype(one_past) step>
+	template<auto last, auto step>
 	struct iter {
-		decltype(one_past) value;
+		decltype(last) value = last;
+		bool end = true;
 
 		[[nodiscard]] constexpr auto operator*() const noexcept {
 			return this->value;
 		}
 
-		[[nodiscard]] friend bool operator==(const DETAIL_XIEITE::range_udl::iter<one_past, step>&, const DETAIL_XIEITE::range_udl::iter<one_past, step>&) = default;
+		[[nodiscard]] friend bool operator==(const DETAIL_XIEITE::range_udl::iter<last, step>&, const DETAIL_XIEITE::range_udl::iter<last, step>&) = default;
 
 		constexpr auto& operator++() noexcept {
-			this->value = xieite::closest(this->value, one_past, step + this->value);
+			this->end = this->value == last;
+			this->value = xieite::closest(this->value, last, this->value + step);
 			return *this;
 		}
 	};
 
-	template<auto first, decltype(first) last, decltype(first) step>
+	template<auto first, auto last, auto step, auto actual_step = (first <= last) ? step : -step>
 	struct range {
 		[[nodiscard]] constexpr auto operator-() const noexcept {
 			using Type = std::conditional_t<(xieite::neg(last) || std::is_unsigned_v<decltype(first)>), xieite::ssize_t, std::size_t>;
@@ -35,12 +37,11 @@ namespace DETAIL_XIEITE::range_udl {
 		}
 
 		[[nodiscard]] constexpr auto begin() const noexcept {
-			return DETAIL_XIEITE::range_udl::iter<(last + ((first <= last) * 2 - 1)), (first <= last) ? step : -step>(first);
+			return DETAIL_XIEITE::range_udl::iter<last, actual_step>(first, false);
 		}
 
 		[[nodiscard]] constexpr auto end() const noexcept {
-			static constexpr auto one_past = (last + ((first <= last) * 2 - 1));
-			return DETAIL_XIEITE::range_udl::iter<one_past, (first <= last) ? step : -step>(one_past);
+			return DETAIL_XIEITE::range_udl::iter<last, actual_step>();
 		}
 	};
 }
@@ -52,7 +53,7 @@ namespace xieite::range_udl {
 		static constexpr auto str = std::string_view(array.begin(), array.end());
 		static constexpr std::size_t end_delim = str.find('.');
 		static constexpr std::size_t step_delim = str.find('e');
-		static constexpr bool end_sign = (end_delim != std::string_view::npos) && (end_delim < (sizeof...(digits) - 1)) && (digits...[end_delim + 1] == '0');
+		static constexpr bool end_sign = (end_delim != std::string_view::npos) && (end_delim < (((step_delim == std::string_view::npos) ? sizeof...(digits) : step_delim) - 2)) && (digits...[end_delim + 1] == '0');
 		using Type = std::conditional_t<end_sign, xieite::ssize_t, std::size_t>;
 		return DETAIL_XIEITE::range_udl::range<
 			xieite::parse_number<Type>(str),
