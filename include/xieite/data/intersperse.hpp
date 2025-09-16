29087853a270f78b3ab96bd2d58336a9c83ff567
Eczbek <eczbek.void@gmail.com>
2025-09-16 11:32:10 -0400

    Move `xieite::intersperse_to()` to a separate header

--- include/xieite/data/intersperse.hpp
+++ include/xieite/data/intersperse.hpp
@@ -2,7 +2,6 @@
 #	define DETAIL_XIEITE_HEADER_DATA_INTERSPERSE
 #
 #	include <format>
-#	include <iterator>
 #	include <string>
 #	include "../data/fixed_str.hpp"
 #	include "../data/fmt_join.hpp"
@@ -12,12 +11,6 @@ namespace xieite {
 	template<xieite::fixed_str delim = " ", typename... Args>
 	[[nodiscard]] constexpr std::string intersperse(Args&&... args) noexcept {
 		return std::format(xieite::fmt_join<delim, Args...>, XIEITE_FWD(args)...);
-	}
-
-	template<xieite::fixed_str delim = " ", std::output_iterator<const char&> Iter, typename... Args>
-	[[nodiscard]] constexpr Iter intersperse_to(Iter iter, Args&&... args) noexcept {
-		std::format_to(iter, xieite::fmt_join<delim, Args...>, XIEITE_FWD(args)...);
-		return iter;
 	}
 }
 
