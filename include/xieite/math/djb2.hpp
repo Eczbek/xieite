51833761ec9aeebd40a6d1a95f9e9df1ccca7973
Eczbek <eczbek.void@gmail.com>
2025-09-16 13:16:22 -0400

    Improve `xieite::djb2()`

--- include/xieite/math/djb2.hpp
+++ include/xieite/math/djb2.hpp
@@ -1,15 +1,13 @@
 #ifndef DETAIL_XIEITE_HEADER_MATH_DJB2
 #	define DETAIL_XIEITE_HEADER_MATH_DJB2
 #
-#	include <cstddef>
 #	include <string_view>
-#	include "../math/sign_cast.hpp"
 
 namespace xieite {
-	[[nodiscard]] constexpr std::size_t djb2(std::string_view strv) noexcept {
-		std::size_t result = 5381;
+	[[nodiscard]] constexpr unsigned long long int djb2(std::string_view strv) noexcept {
+		unsigned long long int result = 5381;
 		for (char c : strv) {
-			result = result * 33 + xieite::sign_cast<std::size_t>(c);
+			(result *= 33) += static_cast<unsigned char>(c);
 		}
 		return result;
 	}
