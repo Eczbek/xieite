f6e6a27fb1ed7f6456085670198194df5d94a905
Eczbek <eczbek.void@gmail.com>
2025-09-17 22:29:43 -0400

    Take range-specific size parameter in `xieite::find_occur()`

--- include/xieite/data/find_occur_if.hpp
+++ include/xieite/data/find_occur_if.hpp
@@ -1,7 +1,6 @@
 #ifndef DETAIL_XIEITE_HEADER_DATA_FIND_OCCUR_IF
 #	define DETAIL_XIEITE_HEADER_DATA_FIND_OCCUR_IF
 #
-#	include <cstddef>
 #	include <functional>
 #	include <ranges>
 #	include "../trait/is_lref_invoc.hpp"
@@ -10,7 +9,7 @@
 
 namespace xieite {
 	template<std::ranges::forward_range Range, xieite::is_lref_invoc<bool(std::ranges::subrange<std::ranges::iterator_t<Range>>)> Pred>
-	[[nodiscard]] constexpr std::ranges::subrange<std::ranges::iterator_t<Range>> find_occur_if(Range& range, std::size_t n, std::ranges::range_size_t<Range> subrange_size, Pred&& pred = {})
+	[[nodiscard]] constexpr std::ranges::subrange<std::ranges::iterator_t<Range>> find_occur_if(Range& range, std::ranges::range_size_t<Range> n, std::ranges::range_size_t<Range> subrange_size, Pred&& pred = {})
 	noexcept(xieite::is_noex_range<Range> && xieite::is_noex_lref_invoc<Pred, bool(std::ranges::subrange<std::ranges::iterator_t<Range>>)>) {
 		auto begin = std::ranges::begin(range);
 		auto end = begin;
@@ -28,7 +27,7 @@ namespace xieite {
 	}
 
 	template<std::ranges::forward_range Range, xieite::is_lref_invoc<bool(std::ranges::range_common_reference_t<Range>)> Pred>
-	[[nodiscard]] constexpr std::ranges::iterator_t<Range> find_occur_if(Range& range, std::size_t n, Pred&& pred = {})
+	[[nodiscard]] constexpr std::ranges::iterator_t<Range> find_occur_if(Range& range, std::ranges::range_size_t<Range> n, Pred&& pred = {})
 	noexcept(xieite::is_noex_range<Range> && xieite::is_noex_lref_invoc<Pred, bool(std::ranges::range_common_reference_t<Range>)>) {
 		return xieite::find_occur_if(range, n, 1, [&](auto&& subrange) { return std::invoke_r<bool>(pred, subrange.front()); }).begin();
 	}
