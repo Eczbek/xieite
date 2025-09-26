9b9a81e8c8bf39e142aaa9623187bee218f9b136
Hurubon <Huruboe@gmail.com>
2025-09-26 19:11:50 +0200

    Bugfix.

--- include/xieite/data/quote.hpp
+++ include/xieite/data/quote.hpp
@@ -11,21 +11,55 @@
 
 namespace xieite {
 	template<xieite::is_char Char, typename Traits, typename Alloc>
-	[[nodiscard]] constexpr std::basic_string<Char, Traits, Alloc> quote(const std::basic_string<Char, Traits, Alloc>& str, Char delim = '"', Char esc = '\\', const Alloc& alloc = {}) noexcept(false) {
-		using Str = std::basic_string<Char, Traits, Alloc>;
-		return (delim == esc)
-			? (delim + xieite::str_replace(str, delim, Str(1, esc, alloc) + delim, alloc) + delim, alloc)
-			: (delim + xieite::str_replace(xieite::str_replace(str, esc, Str(2, esc, alloc), alloc), delim, Str(1, esc, alloc) + delim, alloc) + delim);
+	[[nodiscard]] constexpr std::basic_string<Char, Traits, Alloc> quote(
+		const std::basic_string<Char, Traits, Alloc>& str,
+		Char delim = '"',
+		Char esc = '\\',
+		const Alloc& alloc = {}
+	) noexcept(false) {
+		using String = std::basic_string<Char, Traits, Alloc>;
+		const auto& esc_del = String(1, esc, alloc) + delim;
+		if (delim == esc) {
+			// TODO(Hurubon): Better naming.
+			const auto& out = xieite::str_replace(str, delim, esc_del, alloc);
+			return delim + out + delim;
+		}
+		else {
+			// TODO(Hurubon): Better naming.
+			const auto& esc_esc = String(2, esc, alloc);
+			const auto& inn = xieite::str_replace(str, esc  , esc_esc, alloc);
+			const auto& out = xieite::str_replace(inn, delim, esc_del, alloc);
+			return delim + out + delim;
+		}
 	}
 
-	template<xieite::is_char Char, typename Traits, typename Alloc = std::allocator<Char>>
-	[[nodiscard]] constexpr std::basic_string<Char, Traits, Alloc> quote(std::basic_string_view<Char, Traits> strv, Char delim = '"', Char esc = '\\', const Alloc& alloc = {}) noexcept(false) {
-		return xieite::quote(std::basic_string<Char, Traits, Alloc>(strv, alloc), delim, esc, alloc);
+	template<
+		xieite::is_char Char,
+		typename Traits,
+		typename Alloc = std::allocator<Char>
+	> [[nodiscard]] constexpr std::basic_string<Char, Traits, Alloc> quote(
+		std::basic_string_view<Char, Traits> strv,
+		Char delim = '"',
+		Char esc = '\\',
+		const Alloc& alloc = {}
+	) noexcept(false) {
+		using String = std::basic_string<Char, Traits, Alloc>;
+		return xieite::quote(String(strv, alloc), delim, esc, alloc);
 	}
 
-	template<xieite::is_char Char, typename Traits = std::char_traits<Char>, typename Alloc = std::allocator<Char>, std::size_t length>
-	[[nodiscard]] constexpr std::basic_string<Char, Traits, Alloc> pad(const xieite::type<Char[length]>& str, Char delim = '"', Char esc = '\\', const Alloc& alloc = {}) noexcept(false) {
-		return xieite::quote(std::basic_string<Char, Traits, Alloc>(str, length, alloc), delim, esc, alloc);
+	template<
+		xieite::is_char Char,
+		typename Traits = std::char_traits<Char>,
+		typename Alloc = std::allocator<Char>,
+		std::size_t length
+	> [[nodiscard]] constexpr std::basic_string<Char, Traits, Alloc> pad(
+		const xieite::type<Char[length]>& str,
+		Char delim = '"',
+		Char esc = '\\',
+		const Alloc& alloc = {}
+	) noexcept(false) {
+		using String = std::basic_string<Char, Traits, Alloc>;
+		return xieite::quote(String(str, length, alloc), delim, esc, alloc);
 	}
 }
 
