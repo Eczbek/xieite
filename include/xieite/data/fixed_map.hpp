c7681032cfcec57ad09ece866fd7ceaa794b5fc2
Hurubon <Huruboe@gmail.com>
2025-09-26 15:39:25 +0200

    Formatting.

--- include/xieite/data/fixed_map.hpp
+++ include/xieite/data/fixed_map.hpp
@@ -15,20 +15,35 @@
 #	include "../trait/is_ref_to.hpp"
 
 namespace xieite {
-	template<typename Key, typename Value, std::size_t length, typename Hash = std::hash<Key>, typename Pred = std::equal_to<>, typename Alloc = std::allocator<std::pair<const Key, Value*>>>
+	template<
+		typename Key,
+		typename Value,
+		std::size_t length,
+		typename Hash = std::hash<Key>,
+		typename Pred = std::equal_to<>,
+		typename Alloc = std::allocator<std::pair<const Key, Value*>>>
 	struct fixed_map {
 	public:
 		[[nodiscard]] fixed_map() = default;
 
 		template<std::ranges::input_range Range>
-		requires(std::convertible_to<std::ranges::range_value_t<Range>, std::pair<Key, Value>>)
-		[[nodiscard]] explicit constexpr fixed_map(Range&& entries) noexcept
-		: array(xieite::make_array<std::pair<Key, Value>, length>(XIEITE_FWD(entries))) {}
+		requires(std::convertible_to<
+			std::ranges::range_value_t<Range>,
+			std::pair<Key, Value>
+		>) [[nodiscard]] explicit constexpr fixed_map(Range&& entries) noexcept
+		: array(xieite::make_array<std::pair<Key, Value>, length>(
+			XIEITE_FWD(entries))
+		) {}
 
-		[[nodiscard]] explicit(false) constexpr fixed_map(std::initializer_list<std::pair<Key, Value>> entries) noexcept
+		[[nodiscard]] explicit(false) constexpr fixed_map(
+			std::initializer_list<std::pair<Key, Value>> entries
+		) noexcept
 		: array(xieite::make_array<std::pair<Key, Value>, length>(entries)) {}
 
-		[[nodiscard]] constexpr auto&& operator[](this auto&& self, xieite::is_ref_to<Key> auto&& key) noexcept(false) {
+		[[nodiscard]] constexpr auto&& operator[](
+			this auto&& self,
+			xieite::is_ref_to<Key> auto&& key
+		) noexcept(false) {
 			auto* value = XIEITE_FWD(self).get_val(XIEITE_FWD(key));
 			if (!value) {
 				throw std::out_of_range("must not access nonexistent key");
@@ -36,36 +51,49 @@ namespace xieite {
 			return std::forward_like<decltype(self)>(*value);
 		}
 
-		[[nodiscard]] constexpr auto&& at(this auto&& self, xieite::is_ref_to<Key> auto&& key) noexcept(false) {
+		[[nodiscard]] constexpr auto&& at(
+			this auto&& self,
+			xieite::is_ref_to<Key> auto&& key
+		) noexcept(false) {
 			return std::forward_like<decltype(self)>(self[XIEITE_FWD(key)]);
 		}
 
-		[[nodiscard]] constexpr bool has(xieite::is_ref_to<Key> auto&& key) const noexcept {
+		[[nodiscard]] constexpr bool has(
+			xieite::is_ref_to<Key> auto&& key
+		) const noexcept {
 			return this->get_val(XIEITE_FWD(key));
 		}
 
-		[[nodiscard]] constexpr const std::array<std::pair<Key, Value>, length>& data() const noexcept {
+		[[nodiscard]] constexpr const std::array<std::pair<Key, Value>, length>&
+		data() const noexcept {
 			return this->array;
 		}
 
 	private:
 		std::array<std::pair<Key, Value>, length> array;
 
-		template<typename Map = std::unordered_map<Key, Value*, Hash, Pred, Alloc>>
-		[[nodiscard]] Map& get_map() const noexcept {
-			static Map map = ([this] -> Map {
+		template<
+			typename Map = std::unordered_map<Key, Value*, Hash, Pred, Alloc>
+		> [[nodiscard]] Map& get_map() const noexcept {
+			static Map map = std::invoke([this] -> Map {
 				Map map;
 				map.reserve(this->array.size());
 				for (const std::pair<Key, Value>& entry : this->array) {
 					// Disregard constness here; it is properly handled in `operator[]()`
-					map.emplace(std::make_pair(entry.first, const_cast<Value*>(std::addressof(entry.second))));
+					map.emplace(std::make_pair(
+						entry.first,
+						const_cast<Value*>(std::addressof(entry.second))
+					));
 				}
 				return map;
-			})();
+			});
 			return map;
 		}
 
-		[[nodiscard]] constexpr auto* get_val(this auto&& self, xieite::is_ref_to<Key> auto&& key) noexcept {
+		[[nodiscard]] constexpr auto* get_val(
+			this auto&& self,
+			xieite::is_ref_to<Key> auto&& key
+		) noexcept {
 			if consteval {
 				Pred pred;
 				for (auto&& entry : self.array) {
