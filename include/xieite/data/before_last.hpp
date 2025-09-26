17a07f637f14f83cca81d2e2a3cbd40c4cb01117
Hurubon <Huruboe@gmail.com>
2025-09-26 02:51:40 +0200

    Formatting.

--- include/xieite/data/before_last.hpp
+++ include/xieite/data/before_last.hpp
@@ -10,16 +10,40 @@
 #	include "../trait/is_noex_range.hpp"
 
 namespace xieite {
-	template<std::ranges::forward_range Range, std::ranges::forward_range Subrange, xieite::is_invoc<bool(std::ranges::range_common_reference_t<Range>, std::ranges::range_common_reference_t<Subrange>)> Pred = std::ranges::equal_to>
-	[[nodiscard]] constexpr auto before_last(Range&& range, Subrange&& subrange, Pred&& pred = {})
-	noexcept(xieite::is_noex_range<Range> && xieite::is_noex_range<Subrange> && xieite::is_noex_invoc<Pred, bool(std::ranges::range_common_reference_t<Range>, std::ranges::range_common_reference_t<Subrange>)>) {
-		return std::ranges::subrange(std::ranges::begin(range), std::ranges::find_end(range, subrange, XIEITE_FWD(pred)).begin());
+	template<
+		std::ranges::forward_range Range,
+		std::ranges::forward_range Subrange,
+		xieite::is_invoc<bool(
+			std::ranges::range_common_reference_t<Range>,
+			std::ranges::range_common_reference_t<Subrange>
+		)> Pred = std::ranges::equal_to
+	> [[nodiscard]] constexpr auto before_last(
+		Range&& range,
+		Subrange&& subrange,
+		Pred&& pred = {}
+	) noexcept(
+		xieite::is_noex_range<Range>
+		&& xieite::is_noex_range<Subrange>
+		&& xieite::is_noex_invoc<Pred, bool(
+			std::ranges::range_common_reference_t<Range>,
+			std::ranges::range_common_reference_t<Subrange>
+		)>
+	) {
+		return std::ranges::subrange(
+			std::ranges::begin(range),
+			std::ranges::find_end(range, subrange, XIEITE_FWD(pred)).begin()
+		);
 	}
 
 	template<std::ranges::input_range Range>
-	[[nodiscard]] constexpr auto before_last(Range&& range, std::ranges::range_common_reference_t<Range> x)
-	noexcept(xieite::is_noex_range<Range>) {
-		return std::ranges::subrange(std::ranges::begin(range), std::ranges::find_last(range, x).begin());
+	[[nodiscard]] constexpr auto before_last(
+		Range&& range,
+		std::ranges::range_common_reference_t<Range> x
+	) noexcept(xieite::is_noex_range<Range>) {
+		return std::ranges::subrange(
+			std::ranges::begin(range),
+			std::ranges::find_last(range, x).begin()
+		);
 	}
 }
 
