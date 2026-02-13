280b16f537c0ad6ff7dd102dbfb73700ffb77c95
Eczbek <eczbek.void@gmail.com>
2026-02-13 15:08:35 -0500

    Fix constructor of `xte::sloc {}`

--- include/xte/meta/sloc.hpp
+++ include/xte/meta/sloc.hpp
@@ -10,17 +10,17 @@ namespace xte {
 	struct sloc {
 		xte::string_view file;
 		xte::string_view func;
-		xte::uz row;
+		xte::uz line;
 		xte::uz col;
 
 		[[nodiscard]] explicit(false) consteval sloc(std::source_location sloc = std::source_location::current()) noexcept
-		: file(std::define_static_string(sloc.file_name()))
-		, func(std::define_static_string(sloc.function_name()))
-		, row(sloc.line())
+		: file(std::define_static_string(xte::string_view(sloc.file_name())))
+		, func(std::define_static_string(xte::string_view(sloc.function_name())))
+		, line(sloc.line())
 		, col(sloc.column()) {}
 
-		[[nodiscard]] explicit constexpr sloc(xte::string_view file, xte::string_view func, xte::uz row, xte::uz col = 0) noexcept
-		: file(file), func(func), row(row), col(col) {}
+		[[nodiscard]] explicit constexpr sloc(xte::string_view file, xte::string_view func, xte::uz line, xte::uz col = 0) noexcept
+		: file(file), func(func), line(line), col(col) {}
 	};
 }
 
