0555be25523c509a92825e953487c8f94d2aa88d
Eczbek <eczbek.void@gmail.com>
2026-03-12 15:57:47 -0400

    Remove all qualifiers from both arguments to `xte::is_derived_from<>`

--- include/xte/trait/is_derived_from.hpp
+++ include/xte/trait/is_derived_from.hpp
@@ -2,11 +2,11 @@
 #	define DETAIL_XTE_HEADER_TRAIT_IS_DERIVED_FROM
 #
 #	include "../trait/is_castable.hpp"
-#	include "../trait/remove_cv.hpp"
+#	include "../trait/remove_cvref.hpp"
 
 namespace xte {
 	template<typename T, typename U>
-	concept is_derived_from = xte::is_castable<xte::remove_cv<T>*, U*>;
+	concept is_derived_from = xte::is_castable<xte::remove_cvref<T>*, xte::remove_cvref<U>*>;
 }
 
 #endif
