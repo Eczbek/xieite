d3003a785e43c53788824361c9444fccd0fbd399
Eczbek <eczbek.void@gmail.com>
2026-03-12 15:56:33 -0400

    Fix and improve `xte::array {}`

--- include/xte/data/array.hpp
+++ include/xte/data/array.hpp
@@ -9,14 +9,18 @@
 #	include "../preproc/diagnostic.hpp"
 #	include "../preproc/fwd.hpp"
 #	include "../preproc/lift.hpp"
+#	include "../trait/copy_cvref.hpp"
+#	include "../trait/is_assignable.hpp"
 #	include "../trait/is_castable.hpp"
 #	include "../trait/is_derived_from.hpp"
-#	include "../trait/is_noex_move_assignable.hpp"
-#	include "../trait/is_noex_move_ctor.hpp"
+#	include "../trait/is_noex_move_constructible.hpp"
 #	include "../util/address.hpp"
 #	include "../util/as_c.hpp"
+#	include "../util/construct.hpp"
+#	include "../util/destroy.hpp"
 #	include "../util/exchange.hpp"
 #	include "../util/like.hpp"
+#	include "../util/reconstruct.hpp"
 #	include "../util/types.hpp"
 #	include "../util/xvalue.hpp"
 #	include <compare>
@@ -33,10 +37,10 @@ namespace xte {
 		xte::uz _size = 0;
 		xte::uz _capacity = 0;
 
-		constexpr void reset(T* data = nullptr) & noexcept(std::is_nothrow_destructible_v<T>) {
+		constexpr void reset(T* data = nullptr) & noexcept {
 			if (this->_capacity) {
 				for (xte::uz i = 0; i < this->_size; ++i) {
-					this->_data[i].~T();
+					xte::destroy(this->_data[i]);
 				}
 				std::allocator<T>().deallocate(this->_data, this->_capacity);
 			}
@@ -44,9 +48,12 @@ namespace xte {
 		}
 
 		constexpr void reallocate(xte::uz capacity) & noexcept(false) {
-			T* data = std::allocator<T>().allocate(capacity);
-			for (xte::uz i : std::views::indices(this->_size)) {
-				::new(data + i) T(xte::xvalue(*this)._data[i]);
+			T* data = nullptr;
+			if (capacity) {
+				data = std::allocator<T>().allocate(capacity);
+				for (xte::uz i : std::views::indices(this->_size)) {
+					xte::construct(data[i], xte::xvalue(this->_data[i]));
+				}
 			}
 			this->reset(data);
 			this->_capacity = capacity;
@@ -55,43 +62,45 @@ namespace xte {
 	public:
 		[[nodiscard]] explicit(false) constexpr array() noexcept = default;
 
-		[[nodiscard]] explicit(false) constexpr array(const xte::array<T>& other) noexcept(false) {
-			this->reallocate(other._capacity);
-			while (this->_size < other._size) {
-				::new(this->_data + this->_size) T(other[this->_size++]);
+		[[nodiscard]] explicit(false) constexpr array(const xte::array<T>& other) noexcept(false)
+		requires(std::is_copy_constructible_v<T>) {
+			this->reserve_total(other._size);
+			for (xte::uz i : std::views::indices(this->_size = other._size)) {
+				xte::construct(this->_data[i], other._data[i]);
 			}
 		}
-
-		[[nodiscard]] explicit(false) constexpr array(xte::array<T>& other) noexcept(false)
-		: xte::array<T>(xte::as_c(other)) {}
 
 		[[nodiscard]] explicit(false) constexpr array(xte::array<T>&& other) noexcept
 		: _data(xte::exchange(other._data, nullptr))
 		, _size(xte::exchange(other._size, 0))
 		, _capacity(xte::exchange(other._capacity, 0)) {}
 
-		[[nodiscard]] explicit(false) constexpr array(const xte::array<T>&& other) noexcept(false)
-		: xte::array<T>(other) {}
-
 		[[nodiscard]] explicit(false) constexpr array(xte::init_list<T> init_list) noexcept(false) {
-			this->reserve(init_list.size());
+			this->reserve_total(init_list.size());
 			for (auto&& item : init_list) {
 				this->push(xte::xvalue(item));
 			}
 		}
 
 		template<std::ranges::input_range Range>
-		requires(xte::is_castable<std::ranges::range_value_t<Range>, T>)
+		requires(!xte::is_derived_from<Range, xte::array<T>>
+			&& xte::is_castable<std::ranges::range_value_t<Range>, T>)
 		[[nodiscard]] explicit constexpr array(Range&& range) noexcept(false) {
 			if constexpr (std::ranges::sized_range<Range>) {
-				this->reserve(std::ranges::size(range));
+				this->reserve_total(std::ranges::size(range));
 			}
 			for (auto&& item : range) {
 				this->push(xte::like<decltype(range)>(item));
 			}
 		}
 
-		[[nodiscard]] explicit constexpr array(xte::uz size, const T& fill = {}) noexcept(false) {
+		[[nodiscard]] explicit constexpr array(xte::uz size) noexcept(false)
+		requires(std::is_default_constructible_v<T>) {
+			this->resize(size);
+		}
+
+		[[nodiscard]] constexpr array(xte::uz size, const T& fill) noexcept(false)
+		requires(std::is_copy_constructible_v<T>) {
 			this->resize(size, fill);
 		}
 
@@ -99,22 +108,27 @@ namespace xte {
 			this->reset();
 		}
 
-		constexpr xte::array<T>& operator=(const xte::array<T>& other) & noexcept(false) {
+		constexpr xte::array<T>& operator=(const xte::array<T>& other) & noexcept(false)
+		requires(xte::is_assignable<T&, const T&>) {
 			if (this != xte::address(other)) {
-				this->reallocate(other._capacity);
-				while (this->_size < other._size) {
-					::new(this->_data + this->_size) T(other._data[this->_size++]);
+				if (other._size <= this->_capacity) {
+					for (xte::uz i = 0; (i < this->_size) && (i < other._size); ++i) {
+						this->_data[i] = other._data[i];
+					}
+					for (xte::uz i = this->_size; i < other._size; ++i) {
+						xte::construct(this->_data[i], other._data[i]);
+					}
+					this->erase(other._size, -1uz);
+				} else {
+					this->reset();
+					this->push(other);
 				}
 			}
 			return *this;
 		}
 
 		constexpr xte::array<T>& operator=(xte::array<T>&& other) & noexcept {
-			if (this == xte::address(other)) {
-				return *this;
-			}
-			this->~array();
-			return *::new(this) xte::array<T>(xte::xvalue(other));
+			return (this == xte::address(other)) ? *this : xte::reconstruct(*this, xte::xvalue(other));
 		}
 
 		[[nodiscard]] constexpr auto&& operator[](this auto&& self, xte::uz index) noexcept {
@@ -154,84 +168,113 @@ namespace xte {
 		)
 
 		[[nodiscard]] friend constexpr auto operator==(const xte::array<T>& lhs, const xte::array<T>& rhs) XTE_ARROW(
-			std::is_eq(lhs <=> rhs)
+			(lhs._size == rhs._size) && std::is_eq(lhs <=> rhs)
 		)
 
-		constexpr void resize(xte::uz size, const T& fill = {}) & noexcept(false) {
-			if (this->_size < size) {
-				this->reserve(size - this->_size);
-				while (this->_size < size) {
-					this->push(fill);
-				}
-				return;
+		constexpr void resize(xte::uz size) & noexcept(false)
+		requires(std::is_default_constructible_v<T>) {
+			this->reserve_total(size);
+			while (this->_size < size) {
+				this->push();
+			}
+			this->erase(size, -1uz);
+		}
+
+		constexpr void resize(xte::uz size, const T& fill) & noexcept(false)
+		requires(std::is_copy_constructible_v<T>) {
+			this->reserve_total(size);
+			while (this->_size < size) {
+				this->push(fill);
 			}
 			this->erase(size, -1uz);
 		}
 
 		constexpr void reserve(xte::uz additional = 1) & noexcept(false) {
-			if (additional) {
-				XTE_DIAGNOSTIC_PUSH_GCC(OFF, "-Winterference-size")
-				xte::uz new_capacity = xte::max(this->_capacity, std::hardware_destructive_interference_size / sizeof(T));
-				XTE_DIAGNOSTIC_POP_GCC()
-				while (new_capacity < (this->_capacity + additional)) {
-					new_capacity += new_capacity / 2;
-				}
-				this->reserve_total(new_capacity);
-			}
+			this->reserve_total(this->_capacity + additional);
 		}
 
 		constexpr void reserve_total(xte::uz total) & noexcept(false) {
 			if (total > this->_capacity) {
-				this->reallocate(total);
+				XTE_DIAGNOSTIC_PUSH_GCC(OFF, "-Winterference-size")
+				xte::uz capacity = xte::max(this->_capacity, std::hardware_destructive_interference_size / sizeof(T));
+				XTE_DIAGNOSTIC_POP_GCC()
+				while (capacity < total) {
+					capacity += xte::max(1, capacity / 2);
+				}
+				this->reallocate(capacity);
 			}
 		}
 
 		constexpr void shrink() & noexcept(false) {
-			if (this->_capacity != this->_size) {
+			if (this->_capacity > this->_size) {
 				this->reallocate(this->_size);
 			}
 		}
 
 		template<xte::is_castable<T> U = T>
-		constexpr void insert(xte::uz index, U&& x = {}) & noexcept(false) {
-			if (index <= this->_size) {
-				this->reserve(this->_size >= this->_capacity);
-				for (xte::uz i = this->_size++; i-- > index;) {
-					this->_data[i] = this->_data[i - 1];
-				}
-				this->_data[index] = static_cast<char>(XTE_FWD(x));
+		constexpr void insert(xte::uz index, U&& x = {}) & noexcept(false)
+		requires(xte::is_assignable<T&, T&&>) {
+			this->reserve(this->_size == this->_capacity);
+			if (index == this->_size) {
+				xte::construct(this->_data[this->_size++], XTE_FWD(x));
+				return;
 			}
+			T tmp = T(XTE_FWD(x));
+			xte::construct(this->_data[this->_size], xte::xvalue(this->_data[this->_size - 1]));
+			for (xte::uz i = this->_size++ - 1; i-- > index;) {
+				this->_data[i + 1] = xte::xvalue(this->_data[i]);
+			}
+			this->_data[index] = xte::xvalue(tmp);
 		}
 
 		template<std::ranges::input_range Range>
 		requires(xte::is_castable<std::ranges::range_value_t<Range>, T>)
-		constexpr void insert(xte::uz index, Range&& range) & noexcept(false) {
-			if (index <= this->_size) {
-				xte::array<T> old = xte::xvalue(*this);
-				this->reserve(old._capacity);
-				if constexpr (std::ranges::sized_range<Range>) {
-					this->reserve(std::ranges::size(range));
+		constexpr void insert(xte::uz index, Range&& range) & noexcept(false)
+		requires(xte::is_assignable<T&, T&&> && requires { T(xte::like<Range>(*std::ranges::begin(range))); }) {
+			xte::uz range_size = 0;
+			if constexpr (std::ranges::sized_range<Range>) {
+				range_size = std::ranges::size(range);
+				if ((this->_size + range_size) <= this->_capacity) {
+					for (xte::uz i = range_size; i-- && ((this->_size - (range_size - i)) >= index);) {
+						xte::construct(this->_data[this->_size + i], xte::xvalue(this->_data[this->_size - (range_size - i)]));
+					}
+					for (xte::uz i = this->_size; i-- && (i >= (index + range_size));) {
+						this->_data[i] = xte::xvalue(this->_data[i - range_size]);
+					}
+					auto iter = std::ranges::begin(range);
+					for (xte::uz i = index; (i < this->_size) && (i < (index + range_size)); ++iter) {
+						this->_data[i++] = T(xte::like<Range>(*iter));
+					}
+					auto end = std::ranges::end(range);
+					for (xte::uz i = this->_size; iter != end; ++iter) {
+						xte::construct(this->_data[i++], xte::like<Range>(*iter));
+					}
+					this->_size += range_size;
+					return;
 				}
-				for (auto&& item : old | std::views::take(index)) {
-					this->push(xte::xvalue(item));
-				}
-				for (auto&& item : range) {
-					this->push(xte::like<decltype(range)>(item));
-				}
-				for (auto&& item : old | std::views::drop(index)) {
-					this->push(xte::xvalue(item));
-				}
+			}
+			xte::array<T> old = xte::xvalue(*this);
+			this->reserve_total(old._size + range_size);
+			for (auto&& item : old | std::views::take(index)) {
+				this->push(xte::xvalue(item));
+			}
+			for (auto&& item : range) {
+				this->push(xte::like<Range>(item));
+			}
+			for (auto&& item : old | std::views::drop(index)) {
+				this->push(xte::xvalue(item));
 			}
 		}
 
-		constexpr void erase(xte::uz index, xte::uz count = 1) & noexcept(xte::is_noex_move_assignable<T>) {
+		constexpr void erase(xte::uz index, xte::uz count = 1) & noexcept {
 			if (index <= this->_size) {
 				count = xte::min(count, this->_size - index);
 				for (xte::uz i = index; i < (this->_size - count); ++i) {
-					this->_data[i] = this->_data[i + count];
-					this->_data[this->_size - count + i].~T();
+					this->_data[i] = xte::xvalue(this->_data[i + count]);
 				}
-				this->_size -= count;
+				while (count--) {
+					xte::destroy(this->_data[--this->_size + count]);
+				}
 			}
 		}
 
@@ -246,7 +289,7 @@ namespace xte {
 			this->insert(this->_size, XTE_FWD(range));
 		}
 
-		constexpr T pop() & noexcept(xte::is_noex_move_ctor<T>) {
+		constexpr T pop() & noexcept(xte::is_noex_move_constructible<T>) {
 			auto last = T(xte::xvalue(*this).back());
 			this->erase(this->_size - 1);
 			return last;
@@ -268,3 +311,5 @@ namespace xte {
 }
 
 #endif
+
+// TODO: Assume move constructors, move assignment, and destructors can throw
--- test/data/array.cpp
+++ test/data/array.cpp
@@ -1,12 +1,14 @@
 #include <xte/data/array.hpp>
 #include <xte/data/fixed_array.hpp>
 #include <xte/trait/is_same.hpp>
+#include <xte/util/as_c.hpp>
+#include <xte/util/lvalue.hpp>
 #include <xte/util/xvalue.hpp>
 
 static_assert(([] {
-	xte::array<int> a;
+	xte::array<int> a { 1, 2, 3 };
 	xte::array<int> b = a;
-	return true;
+	return a == b;
 })());
 static_assert(([] {
 	xte::array<int> a;
@@ -15,19 +17,16 @@ static_assert(([] {
 })());
 static_assert(xte::array<int>(xte::fixed_array<int, 1> { 5 })[0] == 5);
 
-static_assert(!xte::array<int>().size());
-static_assert(!xte::array<int>().capacity());
-static_assert(!xte::array<int>().data());
+static_assert(xte::array<int>().size() == 0);
+static_assert(xte::array<int>().capacity() == 0);
+
+static_assert((xte::array<int> { 0 }).size() == 1);
+static_assert((xte::array<int> { 0, 0 }).size() == 2);
+static_assert((xte::array<int> { 0, 0, 0 }).size() == 3);
 
 static_assert(xte::is_same<int&&, decltype(xte::array<int>()[0])>);
-static_assert(([] {
-	xte::array<int> a;
-	return xte::is_same<int&, decltype(a[0])>;
-})());
-static_assert(([] {
-	const xte::array<int> a;
-	return xte::is_same<const int&, decltype(a[0])>;
-})());
+static_assert(xte::is_same<int&, decltype(xte::lvalue(xte::array<int>())[0])>);
+static_assert(xte::is_same<const int&, decltype(xte::as_c(xte::lvalue(xte::array<int>())[0]))>);
 
 static_assert(([] {
 	xte::array<int> a;
@@ -40,4 +39,56 @@ static_assert(([] {
 	a.push(5);
 	a.push(3);
 	return (a.pop() == 3) && (a.size() == 1);
+})());
+
+
+static_assert(([] {
+	xte::array<int> a;
+	a.insert(0, 0);
+	return a == xte::array<int> { 0 };
+})());
+static_assert(([] {
+	xte::array<int> a { 0, 1, 2 };
+	a.insert(0, 3);
+	return a == xte::array<int> { 3, 0, 1, 2 };
+})());
+static_assert(([] {
+	xte::array<int> a { 0, 1, 2 };
+	a.insert(1, 3);
+	return a == xte::array<int> { 0, 3, 1, 2 };
+})());
+static_assert(([] {
+	xte::array<int> a { 0, 1, 2 };
+	a.insert(2, 3);
+	return a == xte::array<int> { 0, 1, 3, 2 };
+})());
+static_assert(([] {
+	xte::array<int> a { 0, 1, 2 };
+	a.insert(3, 3);
+	return a == xte::array<int> { 0, 1, 2, 3 };
+})());
+static_assert(([] {
+	xte::array<int> a { 0, 1, 2 };
+	a.insert(0, xte::array<int> { 3, 4 });
+	return a == xte::array<int> { 3, 4, 0, 1, 2 };
+})());
+static_assert(([] {
+	xte::array<int> a { 0, 1, 2 };
+	a.insert(1, xte::array<int> { 3, 4 });
+	return a == xte::array<int> { 0, 3, 4, 1, 2 };
+})());
+static_assert(([] {
+	xte::array<int> a { 0, 1, 2 };
+	a.insert(2, xte::array<int> { 3, 4 });
+	return a == xte::array<int> { 0, 1, 3, 4, 2 };
+})());
+static_assert(([] {
+	xte::array<int> a { 0, 1, 2 };
+	a.insert(3, xte::array<int> { 3, 4 });
+	return a == xte::array<int> { 0, 1, 2, 3, 4 };
+})());
+static_assert(([] {
+	xte::array<int> a { 0, 1, 2 };
+	a.insert(2, xte::array<int> { 3, 4, 5, 6 });
+	return a == xte::array<int> { 0, 1, 3, 4, 5, 6, 2 };
 })());
