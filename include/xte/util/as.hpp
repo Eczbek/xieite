7ace563bd526daf78f3f3a44ac4fba5ebb772e53
Eczbek <eczbek.void@gmail.com>
2026-03-19 11:54:44 -0400

    Allow multiple arguments to `xte::as()`

--- include/xte/util/as.hpp
+++ include/xte/util/as.hpp
@@ -3,11 +3,14 @@
 #
 #	include "../preproc/arrow.hpp"
 #	include "../preproc/fwd.hpp"
+#	include "../util/at.hpp"
 
 namespace xte {
 	template<typename T>
-	constexpr auto as = [][[nodiscard]](auto&& x) static XTE_ARROW(
-		static_cast<T>(XTE_FWD(x))
+	constexpr auto as = [][[nodiscard]](auto&&... args) static XTE_ARROW_CHOOSE(
+		(sizeof...(args) == 1),
+		static_cast<T>(xte::at<0>(XTE_FWD(args)...)),
+		T(XTE_FWD(args)...)
 	);
 }
 
