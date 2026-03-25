19e2465e4f03ddcadf82285d0aee307f5df03777
Eczbek <eczbek.void@gmail.com>
2026-03-25 15:34:56 -0400

    Fix `xte::string {}`

--- include/xte/data/string.hpp
+++ include/xte/data/string.hpp
@@ -1,310 +1,277 @@
 #ifndef DETAIL_XTE_HEADER_DATA_STRING
 #	define DETAIL_XTE_HEADER_DATA_STRING
 #
-#	include "../data/lowercase.hpp"
-#	include "../data/range_cmp.hpp"
-#	include "../data/string_view.hpp"
-#	include "../data/uppercase.hpp"
-#	include "../meta/type.hpp"
-#	include "../math/max.hpp"
+#	include "../data/array.hpp"
+#	include "../data/init_list.hpp"
 #	include "../math/min.hpp"
-#	include "../preproc/diagnostic.hpp"
+#	include "../meta/type.hpp"
+#	include "../preproc/arrow.hpp"
 #	include "../preproc/fwd.hpp"
-#	include "../trait/is_castable.hpp"
-#	include "../trait/is_implicit_castable.hpp"
-#	include "../util/address.hpp"
-#	include "../util/as_c.hpp"
-#	include "../util/destroy.hpp"
-#	include "../util/exchange.hpp"
+#	include "../trait/is_derived_from.hpp"
+#	include "../trait/is_noex_implicit_castable.hpp"
 #	include "../util/like.hpp"
 #	include "../util/lvalue.hpp"
 #	include "../util/types.hpp"
 #	include "../util/xvalue.hpp"
 #	include <algorithm>
 #	include <compare>
+#	include <cstdint>
 #	include <format>
+#	include <iterator>
 #	include <memory>
-#	include <new>
 #	include <ranges>
-#	include <type_traits>
 
 namespace xte {
 	struct string {
 	private:
-		char* _data = nullptr;
-		xte::uz _size = 0;
-		xte::uz _capacity = 0;
-
-		constexpr void reset(char* data = nullptr) & noexcept {
-			if (this->_capacity) {
-				for (xte::uz i = 0; i < this->_size; ++i) {
-					xte::destroy(this->_data[i]);
-				}
-				std::allocator<char>().deallocate(this->_data, this->_capacity);
-			}
-			this->_data = data;
-		}
-
-		constexpr void reallocate(xte::uz capacity) & noexcept(false) {
-			char* data = std::allocator<char>().allocate(capacity);
-			for (xte::uz i : std::views::indices(this->_size)) {
-				::new(data + i) char(this->_data[i]);
-			}
-			this->reset(data);
-			this->_capacity = capacity;
-		}
+		xte::array<char> _data;
 
 	public:
+		using value_type = char;
+		using allocator_type = std::allocator<char>;
+		using size_type = xte::uz;
+		using difference_type = std::ptrdiff_t;
+		using reference = char&;
+		using const_reference = const char&;
+		using pointer = char*;
+		using const_pointer = const char*;
+		using iterator = char*;
+		using const_iterator = const char*;
+		using reverse_iterator = std::reverse_iterator<char*>;
+		using const_reverse_iterator = std::reverse_iterator<const char*>;
+
 		[[nodiscard]] explicit(false) constexpr string() noexcept = default;
 
-		[[nodiscard]] explicit(false) constexpr string(const xte::string& other) noexcept(false) {
-			this->reallocate(other._capacity);
-			while (this->_size < other._size) {
-				::new(this->_data + this->_size) char(other._data[this->_size++]);
-			}
-		}
-
-		[[nodiscard]] explicit(false) constexpr string(xte::string& other) noexcept(false)
-		: xte::string(xte::as_c(other)) {}
+		[[nodiscard]] explicit(false) constexpr string(const xte::string& other) noexcept(false)
+		: _data(other._data) {}
 
 		[[nodiscard]] explicit(false) constexpr string(xte::string&& other) noexcept
-		: _data(xte::exchange(other._data, nullptr))
-		, _size(xte::exchange(other._size, 0))
-		, _capacity(xte::exchange(other._capacity, 0)) {}
+		: _data(xte::xvalue(other._data)) {}
 
-		[[nodiscard]] explicit(false) constexpr string(const xte::string&& other) noexcept(false)
-		: xte::string(other) {}
+		[[nodiscard]] explicit(false) constexpr string(xte::init_list<char> init_list) noexcept(false)
+		: _data(std::from_range, xte::xvalue(init_list)) {}
 
-		[[nodiscard]] constexpr string(const char* data, xte::uz size) noexcept(false) {
-			if (size) {
-				while (size && !data[size - 1]) {
-					--size;
-				}
-				this->reserve(size);
-				for (xte::uz i : std::views::indices(size)) {
-					::new(this->_data + i) char(data[i]);
-				}
-				::new(this->_data + size) char('\0');
-				this->_size = size + 1;
-			}
+		[[nodiscard]] constexpr string(std::from_range_t, auto&& range) XTE_ARROW_CTOR(,
+			_data,((std::from_range, XTE_FWD(range)))
+		)
+
+		[[nodiscard]] explicit constexpr string(xte::is_noex_implicit_castable<const char*> auto&& range) noexcept(false) {
+			if (const char* data = range) do {
+				this->_data.push(*data);
+			} while (*data++);
 		}
 
-		[[nodiscard]] explicit(false) constexpr string(char c, xte::uz size = 1) noexcept(false) {
-			this->resize(size, c);
+		[[nodiscard]] constexpr string(const char* data, xte::uz size) noexcept(false) {
+			this->_data.reserve_total(size);
+			while (size--) {
+				this->_data.push(*data++);
+			}
+			if (!this->_data.size() || this->_data.back()) {
+				this->_data.push();
+				return;
+			}
+			while ((this->_data.size() > 1) && !this->_data.back(1)) {
+				this->_data.pop();
+			}
 		}
 
 		template<xte::uz size>
 		[[nodiscard]] explicit(false) constexpr string(const xte::type<char[size]>& data) noexcept(false)
 		: xte::string(data, size) {}
 
-		[[nodiscard]] explicit constexpr string(const xte::is_implicit_castable<const char*> auto&& range) noexcept(false) {
-			if (const char* data = range) {
-				while (*data) {
-					this->push(*data++);
-				}
-			}
-		}
-
-		template<std::ranges::input_range Range>
-		requires(xte::is_castable<std::ranges::range_value_t<Range>, char>)
-		[[nodiscard]] explicit constexpr string(Range&& range) noexcept(false) {
-			if constexpr (std::ranges::sized_range<Range>) {
-				this->reserve(std::ranges::size(range));
-			}
-			xte::uz last = 0;
-			xte::uz i = 0;
-			for (char c : range) {
-				this->push(c);
-				if (this->_data[i++]) {
-					last = i;
-				}
-			}
-			this->resize(last);
-		}
-
-		constexpr ~string() {
-			this->reset();
-		}
+		[[nodiscard]] explicit(false) constexpr string(xte::uz size, char c = '\0') noexcept(false)
+		: _data(size, c) {}
 
 		constexpr xte::string& operator=(const xte::string& other) & noexcept(false) {
-			if (this != xte::address(other)) {
-				this->reset();
-				this->reserve(other._capacity);
-				for (char c : other) {
-					this->push(c);
-				}
-			}
+			this->_data = other._data;
 			return *this;
 		}
 
 		constexpr xte::string& operator=(xte::string&& other) & noexcept {
-			if (this == xte::address(other)) {
-				return *this;
+			this->_data = xte::xvalue(other._data);
+			return *this;
+		}
+
+		template<std::ranges::input_range Range>
+		constexpr auto operator=(Range&& range) & noexcept(false)
+		requires(!xte::is_derived_from<Range, xte::string> && requires (char x) { x = static_cast<char>(xte::like<Range>(*xte::lvalue(std::ranges::begin(range)))); }) {
+			this->_data = XTE_FWD(range);
+			if (!this->_data.size() || this->_data.back()) {
+				this->_data.push();
 			}
-			this->~string();
-			return *::new(this) xte::string(xte::xvalue(other));
+			return *this;
 		}
 
 		[[nodiscard]] constexpr auto&& operator[](this auto&& self, xte::uz index) noexcept {
-			return xte::like<decltype(self)>(self._data[index]);
+			return XTE_FWD(self)._data[index];
 		}
 
-		[[nodiscard]] constexpr char* data() const noexcept {
-			return this->_data;
+		[[nodiscard]] constexpr auto* data(this auto&& self) noexcept {
+			return self._data.data();
 		}
 
 		[[nodiscard]] constexpr xte::uz size() const noexcept {
-			return this->_size - !!this->_size;
+			return this->_data.size() - !!this->_data.size();
 		}
 
 		[[nodiscard]] constexpr xte::uz capacity() const noexcept {
-			return this->_capacity;
+			return this->_data.capacity() - !!this->_data.capacity();
 		}
 
-		[[nodiscard]] constexpr char* begin() const & noexcept {
-			return this->_data;
+		[[nodiscard]] constexpr auto* begin(this auto&& self) noexcept {
+			return self._data.begin();
 		}
 
-		[[nodiscard]] constexpr char* end() const & noexcept {
-			return this->_data + !!this->_data * ~-this->_size;
+		[[nodiscard]] constexpr auto* end(this auto&& self) noexcept {
+			return self._data.data() + self.size();
+		}
+
+		[[nodiscard]] constexpr const char* cbegin() const noexcept {
+			return this->begin();
+		}
+
+		[[nodiscard]] constexpr const char* cend() const noexcept {
+			return this->end();
+		}
+
+		[[nodiscard]] constexpr auto rbegin(this auto&& self) noexcept {
+			return std::reverse_iterator(self.end());
+		}
+
+		[[nodiscard]] constexpr auto rend(this auto&& self) noexcept {
+			return std::reverse_iterator(self.begin());
+		}
+
+		[[nodiscard]] constexpr auto crbegin() const noexcept {
+			return this->rbegin();
+		}
+
+		[[nodiscard]] constexpr auto crend() const noexcept {
+			return this->rend();
 		}
 
 		[[nodiscard]] constexpr auto&& front(this auto&& self, xte::uz index = 0) noexcept {
-			return XTE_FWD(self)._data[index];
+			return XTE_FWD(self)._data.front(index);
 		}
 		
 		[[nodiscard]] constexpr auto&& back(this auto&& self, xte::uz index = 0) noexcept {
-			return XTE_FWD(self)._data[self._size - index - 2];
+			return XTE_FWD(self)._data.back(index + 1);
 		}
 
 		[[nodiscard]] friend constexpr std::strong_ordering operator<=>(const xte::string& lhs, const xte::string& rhs) noexcept {
-			return xte::range_cmp(lhs, rhs);
+			return lhs._data <=> rhs._data;
 		}
 
 		[[nodiscard]] friend constexpr bool operator==(const xte::string& lhs, const xte::string& rhs) noexcept {
-			return std::is_eq(lhs <=> rhs);
+			return lhs._data == rhs._data;
 		}
 
 		constexpr void resize(xte::uz size, char fill = '\0') & noexcept(false) {
-			if (this->_size <= size) {
-				this->reserve(size - this->_size);
-				while (this->_size <= size) {
-					this->push(fill);
-				}
-				return;
+			this->reserve_total(size + 1);
+			while (this->size() < size) {
+				this->push(fill);
 			}
 			this->erase(size, -1uz);
 		}
 
 		constexpr void reserve(xte::uz additional = 1) & noexcept(false) {
-			if (additional) {
-				XTE_DIAGNOSTIC_PUSH_GCC(OFF, "-Winterference-size")
-				xte::uz new_capacity = xte::max(this->_capacity, std::hardware_destructive_interference_size);
-				XTE_DIAGNOSTIC_POP_GCC()
-				while (new_capacity < (this->_capacity + additional)) {
-					new_capacity += new_capacity / 2;
-				}
-				this->reserve_total(new_capacity);
-			}
+			this->_data.reserve(additional);
 		}
 
 		constexpr void reserve_total(xte::uz total) & noexcept(false) {
-			if (total > this->_capacity) {
-				this->reallocate(total);
-			}
+			this->_data.reserve_total(total + !!total);
 		}
 
 		constexpr void shrink() & noexcept(false) {
-			if (this->_capacity != this->_size) {
-				this->reallocate(this->_size);
+			this->_data.shrink();
+		}
+
+		constexpr void insert(xte::uz index, char c = '\0') & noexcept(false) {
+			this->_data.insert(xte::min(index, this->size()), c);
+			if (this->_data.back()) {
+				this->_data.push();
 			}
 		}
 
-		template<xte::is_castable<char> U = char>
-		constexpr void insert(xte::uz index, U&& x = {}) & noexcept(false) {
-			if (index <= (this->_size - !!this->_size)) {
-				this->reserve(this->_size >= this->_capacity);
-				for (xte::uz i = this->_size; i-- > index;) {
-					this->_data[i] = this->_data[i - 1];
-				}
-				this->_data[index] = static_cast<char>(XTE_FWD(x));
-				this->_size += 1 + !this->_size;
-				::new(this->_data + this->_size - 1) char('\0');
+		template<std::ranges::input_range Range = xte::string>
+		constexpr void insert_range(xte::uz index, Range&& range) noexcept(false)
+		requires(xte::is_constructible<char, decltype(xte::like<Range>(*xte::lvalue(std::ranges::begin(range))))>) {
+			this->_data.insert_range(xte::min(index, this->size()), XTE_FWD(range));
+			if (!this->_data.size() || this->_data.back()) {
+				this->_data.push();
 			}
 		}
 
-		template<std::ranges::input_range Range>
-		requires(xte::is_castable<std::ranges::range_value_t<Range>, char>)
-		constexpr void insert(xte::uz index, Range&& range) & noexcept(false) {
-			if (index <= (this->_size - !!this->_size)) {
-				xte::string old = xte::xvalue(*this);
-				this->reserve(old._capacity);
-				if constexpr (std::ranges::sized_range<Range>) {
-					this->reserve(std::ranges::size(range));
-				}
-				for (char c : old | std::views::take(index)) {
-					this->push(c);
-				}
-				for (auto&& item : range) {
-					this->push(xte::like<decltype(range)>(item));
-				}
-				for (char c : old | std::views::drop(index)) {
-					this->push(c);
-				}
+		template<std::ranges::input_range Range = xte::string>
+		constexpr void insert_string(xte::uz index, Range&& range) & noexcept(false)
+		requires(xte::is_constructible<char, decltype(xte::like<Range>(*xte::lvalue(std::ranges::begin(range))))>) {
+			index = xte::min(index, this->size());
+			xte::uz old_size = this->size();
+			this->insert_range(index, XTE_FWD(range));
+			xte::uz range_size = this->size() - old_size;
+			xte::uz i = range_size;
+			while (i && !this->_data[index + i - 1]) {
+				--i;
 			}
+			this->erase(index + i, range_size - i);
 		}
 
 		constexpr void erase(xte::uz index, xte::uz count = 1) & noexcept {
-			if (index <= (this->_size - !!this->_size)) {
-				count = xte::min(count, this->_size - !!this->_size - index);
-				for (xte::uz i = index; i < (this->_size - !!this->_size - count); ++i) {
-					this->_data[i] = this->_data[i + count];
-					xte::destroy(this->_data[this->_size - !!this->_size - count + i]);
-				}
-				if (this->_size -= count) {
-					this->_data[this->_size - 1] = '\0';
-				}
+			if (index <= this->size()) {
+				this->_data.erase(index, xte::min(count, this->size() - index));
 			}
 		}
 
-		template<xte::is_castable<char> U = char>
-		constexpr void push(U&& x = {}) & noexcept(false) {
-			this->insert(this->_size - !!this->_size, XTE_FWD(x));
+		constexpr void push(char c = '\0') & noexcept(false) {
+			this->insert(-1uz, c);
 		}
 
-		template<std::ranges::input_range Range>
-		requires(xte::is_castable<std::ranges::range_value_t<Range>, char>)
-		constexpr void push(Range&& range) & noexcept(false) {
-			this->insert(this->_size - !!this->_size, XTE_FWD(range));
-		}
+		template<typename Range = xte::string>
+		constexpr auto push_range(Range&& range) & XTE_ARROW(
+			this->insert_range(-1uz, XTE_FWD(range))
+		)
 
-		constexpr char pop() & noexcept {
+		template<typename Range = xte::string>
+		constexpr auto push_string(Range&& range) & XTE_ARROW(
+			this->insert_string(-1uz, XTE_FWD(range))
+		)
+
+		constexpr char pop() noexcept {
 			char last = this->back();
-			this->erase(this->_size - 1);
+			this->erase(this->size() - 1);
 			return last;
 		}
 
-		constexpr xte::string& operator+=(xte::string_view rhs) & noexcept(false) {
-			this->push(rhs);
-			return *this;
+		constexpr xte::string& operator+=(this auto& lhs, char rhs) noexcept(false) {
+			lhs.xte::string::push(rhs);
+			return lhs;
 		}
 
-		[[nodiscard]] friend constexpr xte::string operator+(xte::string_view lhs, xte::string_view rhs) noexcept(false) {
-			return xte::lvalue(xte::string(lhs)) += rhs;
-		}
+		constexpr auto operator+=(this auto& lhs, auto&& rhs) XTE_ARROW(
+			void(lhs.xte::string::push_string(XTE_FWD(rhs))),
+			lhs
+		)
 
-		[[nodiscard]] constexpr xte::string uppercase() const noexcept {
-			return xte::uppercase(*this);
-		}
+		[[nodiscard]] friend constexpr auto operator+(xte::is_derived_from<xte::string> auto&& lhs, auto&& rhs) XTE_ARROW(
+			auto(lhs.xte::string::operator+=(XTE_FWD(rhs)))
+		)
 
-		[[nodiscard]] constexpr xte::string lowercase() const noexcept {
-			return xte::lowercase(*this);
-		}
+		[[nodiscard]] friend constexpr auto operator+(char lhs, xte::is_derived_from<xte::string> auto rhs) XTE_ARROW(
+			rhs.xte::string::insert(0, lhs),
+			auto(rhs)
+		)
+
+		template<typename Lhs>
+		requires(!xte::is_derived_from<Lhs, xte::string>)
+		[[nodiscard]] friend constexpr auto operator+(Lhs&& lhs, xte::is_derived_from<xte::string> auto rhs) XTE_ARROW(
+			rhs.xte::string::insert_string(0, XTE_FWD(lhs)),
+			auto(rhs)
+		)
 	};
 }
 
 namespace xte::literal::string {
-	[[nodiscard]] constexpr xte::string operator""_str(const char* data, xte::uz size) noexcept(false) {
+	[[nodiscard]] constexpr xte::string operator""_string(const char* data, xte::uz size) noexcept(false) {
 		return xte::string(data, size);
 	}
 }
--- test/data/string.cpp
+++ test/data/string.cpp
@@ -1,37 +1,226 @@
 #include <xte/data/string.hpp>
+#include <xte/trait/is_copy_constructible.hpp>
+#include <xte/trait/is_noex_move_constructible.hpp>
+#include <xte/trait/is_same.hpp>
+#include <xte/util/as_c.hpp>
 #include <xte/util/lvalue.hpp>
+#include <xte/util/types.hpp>
+#include <iterator>
 #include <ranges>
 
-static_assert(xte::string().size() == 0);
-static_assert(xte::string().capacity() == 0);
-static_assert(xte::string("abc").size() == 3);
-static_assert(xte::string("abc").capacity() >= 3);
+static_assert(xte::is_same<xte::string::value_type, char>);
+static_assert(xte::is_same<xte::string::reference, char&>);
+static_assert(xte::is_same<xte::string::const_reference, const char&>);
+static_assert(xte::is_same<xte::string::pointer, char*>);
+static_assert(xte::is_same<xte::string::const_pointer, const char*>);
+static_assert(xte::is_same<xte::string::iterator, char*>);
+static_assert(xte::is_same<xte::string::const_iterator, const char*>);
+static_assert(xte::is_same<xte::string::reverse_iterator, std::reverse_iterator<char*>>);
+static_assert(xte::is_same<xte::string::const_reverse_iterator, std::reverse_iterator<const char*>>);
+
+static_assert(requires { { xte::string() } noexcept; });
+static_assert(xte::is_copy_constructible<xte::string>);
+static_assert(xte::is_noex_move_constructible<xte::string>);
+static_assert(requires { xte::string { 'a', 'b', 'c' }; });
+static_assert(requires { xte::string(std::from_range, typename[:^^char[]:] { 'a', 'b', 'c' }); });
+static_assert(requires { xte::string(3, 'a'); });
+static_assert(requires { xte::string("abc"); });
+static_assert(requires { xte::string("abc", 3); });
+static_assert(([] {
+	xte::string a;
+	a = xte::string("abc");
+	return a == "abc";
+})());
+static_assert(([] {
+	xte::string a;
+	a = xte::lvalue(xte::string("abc"));
+	return a == "abc";
+})());
+static_assert(([] {
+	xte::string a;
+	a = "abc";
+	return a == "abc";
+})());
+static_assert(([] {
+	xte::string a;
+	a = xte::array<char> { 'a', 'b', 'c' };
+	return a == "abc";
+})());
+
+
+static_assert(xte::is_same<char&&, decltype(xte::string()[0])>);
+static_assert(xte::is_same<char&, decltype(xte::lvalue(xte::string())[0])>);
+static_assert(xte::is_same<const char&, decltype(xte::as_c(xte::lvalue(xte::string())[0]))>);
 static_assert(xte::string("abc")[0] == 'a');
 static_assert(xte::string("abc")[1] == 'b');
 static_assert(xte::string("abc")[2] == 'c');
 static_assert(xte::string("abc")[3] == '\0');
+
+static_assert(xte::string("abc").data()[0] == 'a');
+static_assert(xte::string("abc").data()[1] == 'b');
+static_assert(xte::string("abc").data()[2] == 'c');
+static_assert(xte::string("abc").data()[3] == '\0');
+
+static_assert(xte::string().size() == 0);
+static_assert(xte::string("a").size() == 1);
+static_assert(xte::string("ab").size() == 2);
+static_assert(xte::string("abc").size() == 3);
+
+static_assert(xte::string().capacity() == 0);
+static_assert(([] {
+	auto a = xte::string(999);
+	return a.capacity() >= a.size();
+})());
+
+static_assert(([] {
+	xte::uz sum = 0;
+	for (char c : xte::string("abcdef")) {
+		sum += static_cast<xte::uz>(c);
+	}
+	return sum > 0;
+})());
+
+static_assert(xte::is_same<char&&, decltype(xte::string().front())>);
+static_assert(xte::is_same<char&, decltype(xte::lvalue(xte::string()).front())>);
+static_assert(xte::is_same<const char&, decltype(xte::as_c(xte::lvalue(xte::string()).front()))>);
+static_assert(xte::string("abc").front() == 'a');
+static_assert(xte::string("abc").front(0) == 'a');
+static_assert(xte::string("abc").front(1) == 'b');
+static_assert(xte::string("abc").front(2) == 'c');
+static_assert(xte::string("abc").front(3) == '\0');
+
+static_assert(xte::is_same<char&&, decltype(xte::string().back())>);
+static_assert(xte::is_same<char&, decltype(xte::lvalue(xte::string()).back())>);
+static_assert(xte::is_same<const char&, decltype(xte::as_c(xte::lvalue(xte::string()).back()))>);
+static_assert(xte::string("abc").back() == 'c');
+static_assert(xte::string("abc").back(0) == 'c');
+static_assert(xte::string("abc").back(1) == 'b');
+static_assert(xte::string("abc").back(2) == 'a');
+static_assert(xte::string("abc").back(-1uz) == '\0');
+
 static_assert(xte::string("abc") == "abc");
-static_assert(([] {
-	xte::string s = "abc";
-	return s == xte::string(s);
-})());
-static_assert(auto(xte::lvalue(xte::string("abc"))) == "abc");
-static_assert(auto(xte::string("abc")) == "abc");
-
-static_assert(std::ranges::input_range<xte::string>);
+static_assert(xte::string("abc") != "xyz");
+static_assert(xte::string("abc") < "xyz");
+static_assert(xte::string("xyz") > "abc");
 
 static_assert(([] {
-	xte::string s = "abc";
-	s = "xyz";
-	return s == "xyz";
+	xte::string a;
+	a.resize(1, 'a');
+	return a[0] == 'a';
+})());
+
+static_assert(([] {
+	xte::string a;
+	a.reserve(10);
+	a.reserve(10);
+	return a.capacity() >= 20;
+})());
+
+static_assert(([] {
+	xte::string a;
+	a.reserve(10);
+	xte::uz capacity = a.capacity();
+	a.reserve_total(10);
+	return a.capacity() == capacity;
+})());
+
+static_assert(([] {
+	xte::string a;
+	a.reserve(10);
+	a.shrink();
+	return a.capacity() == 0;
+})());
+
+static_assert(([] {
+	xte::string a;
+	a.insert(0, 'a');
+	return a == "a";
 })());
 static_assert(([] {
-	xte::string s = "abc";
-	s += "xyz";
-	return s == "abcxyz";
+	xte::string a = "abc";
+	a.insert(0, 'x');
+	return a == "xabc";
 })());
 static_assert(([] {
-	xte::string s = 'a';
-	s += 'b';
-	return s == "ab";
+	xte::string a = "abc";
+	a.insert(1, 'x');
+	return a == "axbc";
 })());
+static_assert(([] {
+	xte::string a = "abc";
+	a.insert(2, 'x');
+	return a == "abxc";
+})());
+static_assert(([] {
+	xte::string a = "abc";
+	a.insert(3, 'x');
+	return a == "abcx";
+})());
+
+static_assert(([] {
+	xte::string a = "abc";
+	a.insert_string(0, "xyz");
+	return a == "xyzabc";
+})());
+static_assert(([] {
+	xte::string a = "abc";
+	a.insert_string(1, "xyz");
+	return a == "axyzbc";
+})());
+static_assert(([] {
+	xte::string a = "abc";
+	a.insert_string(2, "xyz");
+	return a == "abxyzc";
+})());
+static_assert(([] {
+	xte::string a = "abc";
+	a.insert_string(3, "xyz");
+	return a == "abcxyz";
+})());
+
+static_assert(([] {
+	xte::string a = "abcdef";
+	a.erase(1, 3);
+	return a == "aef";
+})());
+static_assert(([] {
+	xte::string a = "abcdef";
+	a.erase(0, -1uz);
+	return a == "";
+})());
+
+static_assert(([] {
+	xte::string a = "abc";
+	a.push('x');
+	return a == "abcx";
+})());
+
+static_assert(([] {
+	xte::string a = "abc";
+	a.push_string("xyz");
+	return a == "abcxyz";
+})());
+
+static_assert(([] {
+	xte::string a = "a";
+	return (a.pop() == 'a') && (a.size() == 0);
+})());
+static_assert(([] {
+	xte::string a = "abc";
+	return (a.pop() == 'c') && (a.size() == 2);
+})());
+
+static_assert(([] {
+	xte::string a = "abc";
+	a += 'x';
+	return a == "abcx";
+})());
+static_assert(([] {
+	xte::string a = "abc";
+	a += "xyz";
+	return a == "abcxyz";
+})());
+
+static_assert((xte::string("abc") + 'x') == "abcx");
+static_assert((xte::string("abc") + "xyz") == "abcxyz");
+static_assert((xte::string("abc") + xte::string("xyz")) == "abcxyz");
