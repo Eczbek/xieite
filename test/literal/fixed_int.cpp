f96d39ba0093ce9aeab4897fc348ad0013a5337e
Eczbek <eczbek.void@gmail.com>
2026-04-02 21:46:26 -0400

    Improve tests for fixed integer literals

--- test/literal/fixed_int.cpp
+++ test/literal/fixed_int.cpp
@@ -1,24 +1,26 @@
 #include <xte/literal/fixed_int.hpp>
 #include <xte/preproc/feature.hpp>
+#include <xte/trait/is_same.hpp>
+#include <xte/util/types.hpp>
 
 using namespace xte::literal::fixed_int;
 
 #if XTE_FEATURE_INT_8
-[[maybe_unused]] constexpr auto u8 = 0_u8;
-[[maybe_unused]] constexpr auto i8 = 0_i8;
+static_assert(xte::is_same<decltype(0_u8), xte::u8>);
+static_assert(xte::is_same<decltype(0_i8), xte::i8>);
 #endif
 
 #if XTE_FEATURE_INT_16
-[[maybe_unused]] constexpr auto u16 = 0_u16;
-[[maybe_unused]] constexpr auto i16 = 0_i16;
+static_assert(xte::is_same<decltype(0_u16), xte::u16>);
+static_assert(xte::is_same<decltype(0_i16), xte::i16>);
 #endif
 
 #if XTE_FEATURE_INT_32
-[[maybe_unused]] constexpr auto u32 = 0_u32;
-[[maybe_unused]] constexpr auto i32 = 0_i32;
+static_assert(xte::is_same<decltype(0_u32), xte::u32>);
+static_assert(xte::is_same<decltype(0_i32), xte::i32>);
 #endif
 
 #if XTE_FEATURE_INT_64
-[[maybe_unused]] constexpr auto u64 = 0_u64;
-[[maybe_unused]] constexpr auto i64 = 0_i64;
+static_assert(xte::is_same<decltype(0_u64), xte::u64>);
+static_assert(xte::is_same<decltype(0_i64), xte::i64>);
 #endif
