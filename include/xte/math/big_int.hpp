038a43b6e148c9368acc9c0f49405a9fe8078f5b
Eczbek <eczbek.void@gmail.com>
2026-04-02 21:47:23 -0400

    Recreate `xte::big_int {}`

---
+++ include/xte/math/big_int.hpp
@@ -0,0 +1,582 @@
+#ifndef DETAIL_XTE_HEADER_MATH_BIG_INT
+#	define DETAIL_XTE_HEADER_MATH_BIG_INT
+#
+#	include "../data/array.hpp"
+#	include "../data/null.hpp"
+#	include "../data/opt.hpp"
+#	include "../data/range_cmp.hpp"
+#	include "../data/string.hpp"
+#	include "../data/string_view.hpp"
+#	include "../func/gen.hpp"
+#	include "../math/abs.hpp"
+#	include "../math/add_checked.hpp"
+#	include "../math/digits.hpp"
+#	include "../math/is_single_bit.hpp"
+#	include "../math/max.hpp"
+#	include "../math/number_format_config.hpp"
+#	include "../math/rshift.hpp"
+#	include "../math/sub_checked.hpp"
+#	include "../math/wide_uint.hpp"
+#	include "../math/width.hpp"
+#	include "../preproc/arrow.hpp"
+#	include "../preproc/fwd.hpp"
+#	include "../trait/is_same.hpp"
+#	include "../trait/is_unsigned.hpp"
+#	include "../util/error.hpp"
+#	include "../util/exchange.hpp"
+#	include "../util/types.hpp"
+#	include "../util/xvalue.hpp"
+#	include <compare>
+#	include <functional>
+#	include <ranges>
+
+namespace xte {
+	template<xte::is_unsigned T = xte::uz>
+	struct big_int {
+		template<xte::is_unsigned>
+		friend struct xte::big_int;
+
+	// private:
+		xte::array<T> _data;
+		bool _neg = false;
+
+		constexpr void _trim() noexcept(false) {
+			if (!this->_data.size()) {
+				this->_data.push(0);
+			} else while ((this->_data.size() > 1) && !this->_data.back()) {
+				this->_data.pop();
+			}
+			this->_neg &= !!*this;
+		}
+
+		[[nodiscard]] constexpr bool _is_single_bit() const noexcept {
+			if (!xte::is_single_bit(this->_data.back())) {
+				return false;
+			}
+			for (xte::uz i = 0; (i + 1) < this->_data.size(); ++i) {
+				if (this->_data[i]) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		[[nodiscard]] constexpr xte::big_int<T> _log2() const noexcept {
+			return xte::big_int<T>(xte::width<T>) * ~-this->_data.size() + ~-xte::digits(this->_data.back(), 2);
+		}
+
+		constexpr xte::big_int<T>& _bitwise(xte::big_int<T> rhs, auto func) noexcept(false) {
+			T lhs_neg = this->_neg;
+			T rhs_neg = rhs._neg;
+			*this += lhs_neg;
+			rhs += rhs_neg;
+			xte::uz size = xte::max(this->_data.size(), rhs._data.size());
+			this->_data.resize(size);
+			rhs._data.resize(size);
+			this->_neg = func(lhs_neg, rhs_neg);
+			for (xte::uz i : std::views::indices(size)) {
+				this->_data[i] = func(this->_data[i] ^ -lhs_neg, rhs._data[i] ^ -rhs_neg) ^ -this->_neg;
+			}
+			return *this -= +this->_neg;
+		}
+
+	public:
+		using type = T;
+
+		template<xte::is_int U = T>
+		[[nodiscard]] explicit(false) constexpr big_int(U x = 0) noexcept(false)
+		: _neg(x < 0) {
+			auto abs = xte::abs(x);
+			do {
+				this->_data.push(static_cast<T>(abs));
+			} while ((abs = xte::rshift(abs, xte::width<T>)));
+		}
+
+		[[nodiscard]] explicit(false) constexpr big_int(const xte::big_int<T>&) noexcept(false) = default;
+
+		[[nodiscard]] explicit(false) constexpr big_int(xte::big_int<T>&&) noexcept = default;
+
+		template<typename U>
+		requires(!xte::is_same<T, U>)
+		[[nodiscard]] explicit constexpr big_int(const xte::big_int<U>& other) noexcept(false)
+		: _neg(other._neg) {
+			if constexpr (xte::width<T> >= xte::width<U>) {
+				xte::uz shift = xte::width<T>;
+				for (U digit : other._data) {
+					if ((shift += xte::width<U>) >= xte::width<T>) {
+						this->_data.push((shift = 0));
+					}
+					this->_data.back() |= digit << shift;
+				}
+			} else {
+				for (U digit : other._data) {
+					xte::uz shift = 0;
+					do {
+						this->_data.push(static_cast<T>(digit >> shift));
+					} while ((shift += xte::width<T>) < xte::width<U>);
+				}
+				this->_trim();
+			}
+		}
+
+		template<typename Range = xte::array<T>>
+		[[nodiscard]] constexpr big_int(std::from_range_t, Range&& range, bool neg = false) XTE_ARROW_CTOR(
+			this->_trim(),
+			_data,((std::from_range, XTE_FWD(range))),
+			_neg,((neg))
+		)
+
+		template<xte::is_int Radix = xte::uz>
+		[[nodiscard]] explicit constexpr big_int(xte::string_view string, Radix radix = 10uz, const xte::number_format_config& config = {}) noexcept(false)
+		: xte::big_int<T>(0) {
+			bool neg = config.minus.contains(string[0]);
+			for (char c : string | std::views::drop(neg || config.plus.contains(string[0]))) {
+				xte::uz digit = config.digits.find(c);
+				if (!~digit) {
+					break;
+				}
+				(*this *= radix) += digit;
+			}
+			this->_neg = neg;
+		}
+
+		constexpr xte::big_int<T>& operator=(const xte::big_int<T>&) noexcept(false) = default;
+
+		constexpr xte::big_int<T>& operator=(xte::big_int<T>&&) noexcept = default;
+
+		template<xte::is_int U>
+		[[nodiscard]] explicit constexpr operator U() const noexcept {
+			U result = 0;
+			xte::uz shift = 0;
+			for (T digit : this->_data) {
+				result |= static_cast<U>(xte::lshift(digit, shift));
+				if ((shift += xte::width<T>) >= xte::width<U>) {
+					break;
+				}
+			}
+			return this->_neg ? -result : result;
+		}
+
+		[[nodiscard]] explicit constexpr operator bool() const noexcept {
+			return (this->_data.size() > 1) || this->_data[0];
+		}
+
+		[[nodiscard]] friend constexpr std::strong_ordering operator<=>(const xte::big_int<T>& lhs, const xte::big_int<T>& rhs) noexcept {
+			std::strong_ordering order = rhs._neg <=> lhs._neg;
+			if (std::is_eq(order)) {
+				if (lhs._neg) {
+					if (std::is_eq((order = rhs._data.size() <=> lhs._data.size()))) {
+						return xte::range_cmp(std::views::reverse(rhs._data), std::views::reverse(lhs._data));
+					}
+				} else if (std::is_eq((order = lhs._data.size() <=> rhs._data.size()))) {
+					return xte::range_cmp(std::views::reverse(lhs._data), std::views::reverse(rhs._data));
+				}
+			}
+			return order;
+		}
+
+		[[nodiscard]] friend constexpr bool operator==(const xte::big_int<T>&, const xte::big_int<T>&) noexcept = default;
+
+		[[nodiscard]] constexpr xte::big_int<T> operator+() const noexcept(false) {
+			return *this;
+		}
+
+		[[nodiscard]] friend constexpr xte::big_int<T> operator+(const xte::big_int<T>& lhs, const xte::big_int<T>& rhs) noexcept(false) {
+			return auto(lhs) += rhs;
+		}
+
+		constexpr xte::big_int<T>& operator+=(const xte::big_int<T>& rhs) noexcept(false) {
+			if (!*this) {
+				return *this = rhs;
+			}
+			if (!rhs) {
+				return *this;
+			}
+			if (this->_neg != rhs._neg) {
+				return *this -= -rhs;
+			}
+			xte::array<T> sum_data;
+			T carry = 0;
+			for (xte::uz i = 0; carry || (i < this->_data.size()) || (i < rhs._data.size()); ++i) {
+				T lhs_digit = (i < this->_data.size()) ? this->_data[i] : 0;
+				T rhs_digit = (i < rhs._data.size()) ? rhs._data[i] : 0;
+				sum_data.push(lhs_digit + rhs_digit + carry);
+				carry = !xte::add_checked(lhs_digit, rhs_digit, carry);
+			}
+			this->_data = xte::xvalue(sum_data);
+			return *this;
+		}
+
+		constexpr xte::big_int<T>& operator++() noexcept(false) {
+			return *this += 1;
+		}
+
+		constexpr xte::big_int<T> operator++(int) noexcept(false) {
+			return xte::exchange(*this, *this + 1);
+		}
+
+		[[nodiscard]] constexpr xte::big_int<T> operator-() const noexcept(false) {
+			return xte::big_int<T>(std::from_range, this->_data, !this->_neg);
+		}
+
+		[[nodiscard]] friend constexpr xte::big_int<T> operator-(const xte::big_int<T>& lhs, const xte::big_int<T>& rhs) noexcept(false) {
+			return auto(lhs) -= rhs;
+		}
+
+		constexpr xte::big_int<T>& operator-=(const xte::big_int<T>& rhs) noexcept(false) {
+			if (!*this) {
+				return *this = -rhs;
+			}
+			if (!rhs) {
+				return *this;
+			}
+			if (this->_neg != rhs._neg) {
+				return *this += -rhs;
+			}
+			if (this->_neg ? (*this > rhs) : (*this < rhs)) {
+				return *this = -(rhs - *this);
+			}
+			if (*this == rhs) {
+				return *this = 0;
+			}
+			xte::array<T> diff_data;
+			T borrow = 0;
+			for (xte::uz i = 0; borrow || (i < rhs._data.size()); ++i) {
+				T rhs_digit = (i < rhs._data.size()) ? rhs._data[i] : 0;
+				diff_data.push(this->_data[i] - rhs_digit - borrow);
+				borrow = (i < ~-this->_data.size()) && !xte::sub_checked(this->_data[i], rhs_digit, borrow);
+			}
+			this->_data = xte::xvalue(diff_data);
+			return *this;
+		}
+
+		constexpr xte::big_int<T>& operator--() noexcept(false) {
+			return *this -= 1;
+		}
+
+		constexpr xte::big_int<T> operator--(int) noexcept(false) {
+			return xte::exchange(*this, *this - 1);
+		}
+
+		[[nodiscard]] friend constexpr xte::big_int<T> operator*(const xte::big_int<T>& lhs, const xte::big_int<T> rhs) noexcept(false) {
+			return auto(lhs) *= rhs;
+		}
+
+		constexpr xte::big_int<T>& operator*=(const xte::big_int<T>& rhs) noexcept(false) {
+			if (!*this || !rhs) {
+				return *this = 0;
+			}
+			bool same_sign = this->_neg == rhs._neg;
+			if ((*this == 1) || (*this == -1)) {
+				return *this = same_sign ? rhs : -rhs;
+			}
+			if ((rhs == 1) || (rhs == -1)) {
+				return *this = same_sign ? *this : -*this;
+			}
+			if (this->_is_single_bit()) {
+				return *this = same_sign ? (rhs.abs() << this->_log2()) : -(rhs.abs() << this->_log2());
+			}
+			if (rhs._is_single_bit()) {
+				return *this = same_sign ? (this->abs() << rhs._log2()) : -(this->abs() << rhs._log2());
+			}
+			xte::big_int<T> prod;
+			for (xte::uz i = this->_data.size(); i--;) {
+				if (this->_data[i]) {
+					for (xte::uz j = rhs._data.size(); j--;) {
+						if (rhs._data[j]) {
+							auto [lo, hi] = xte::wide_uint<T>(this->_data[i]) * rhs._data[j];
+							prod += ((xte::big_int<T>(hi) << xte::width<T>) | lo) << (xte::big_int<T>(i) << xte::digits(xte::width<T> - 1, 2)) << (xte::big_int<T>(j) << xte::digits(xte::width<T> - 1, 2));
+						}
+					}
+				}
+			}
+			prod._neg = !same_sign;
+			return *this = xte::xvalue(prod);
+		}
+
+		[[nodiscard]] friend constexpr xte::big_int<T> operator/(const xte::big_int<T>& lhs, const xte::big_int<T>& rhs) noexcept(false) {
+			return auto(lhs) /= rhs;
+		}
+
+		constexpr xte::big_int<T>& operator/=(const xte::big_int<T>& rhs) noexcept(false) {
+			if (!rhs) {
+				throw xte::error("must not divide by zero");
+			}
+			bool same_sign = this->_neg == rhs._neg;
+			if ((this->_data.size() < 2) && (rhs._data.size() < 2)) {
+				this->_data[0] /= rhs._data[0];
+				this->_neg = !same_sign;
+				return *this;
+			}
+			xte::big_int<T> rhs_abs = rhs.abs();
+			this->_neg = false;
+			if (!*this || (*this < rhs_abs)) {
+				return *this = 0;
+			}
+			if (*this == rhs_abs) {
+				return *this = same_sign ? 1 : -1;
+			}
+			if (rhs_abs._is_single_bit()) {
+				return same_sign ? (*this >>= rhs_abs._log2()) : (*this = -(*this >> rhs_abs._log2()));
+			}
+			xte::big_int<T> rem;
+			xte::big_int<T> quot;
+			quot._data.resize(this->_data.size());
+			for (xte::uz i : std::views::indices(this->_data.size())) {
+				for (xte::uz j = xte::width<T>; j--;) {
+					rem <<= 1;
+					rem._data[0] |= (this->_data[i] >> j) & 1;
+					if (rem >= rhs_abs) {
+						rem -= rhs_abs;
+						quot._data[i] |= static_cast<T>(1) << j;
+					}
+				}
+			}
+			quot._neg = !same_sign;
+			quot._trim();
+			return *this = xte::xvalue(quot);
+		}
+
+		[[nodiscard]] friend constexpr xte::big_int<T> operator%(const xte::big_int<T>& lhs, const xte::big_int<T>& rhs) noexcept(false) {
+			return auto(lhs) %= rhs;
+		}
+
+		constexpr xte::big_int<T>& operator%=(const xte::big_int<T>& rhs) noexcept(false) {
+			if (!rhs) {
+				throw xte::error("must not take remainder of division by zero");
+			}
+			xte::big_int<T> rhs_abs = rhs.abs();
+			if (rhs_abs._is_single_bit()) {
+				return *this &= (rhs_abs - 1);
+			}
+			bool neg = xte::exchange(this->_neg, false);
+			if (!*this || (rhs_abs == 1) || (*this == rhs_abs)) {
+				return *this = 0;
+			}
+			if (*this < rhs_abs) {
+				this->_neg = neg;
+				return *this;
+			}
+			xte::big_int<T> rem;
+			for (T digit : std::views::reverse(this->_data)) {
+				for (xte::uz j = xte::width<T>; j--;) {
+					(rem <<= 1)._data[0] |= (digit >> j) & 1;
+					if (rem >= rhs_abs) {
+						rem -= rhs_abs;
+					}
+				}
+			}
+			rem._neg = neg;
+			return *this = xte::xvalue(rem);
+		}
+
+		[[nodiscard]] constexpr xte::big_int<T> operator~() const noexcept(false) {
+			return -*this - 1;
+		}
+
+		[[nodiscard]] friend constexpr xte::big_int<T> operator&(const xte::big_int<T>& lhs, const xte::big_int<T>& rhs) noexcept(false) {
+			return auto(lhs) &= rhs;
+		}
+
+		constexpr xte::big_int<T>& operator&=(const xte::big_int<T>& rhs) noexcept(false) {
+			return this->_bitwise(rhs, std::bit_and<T>());
+		}
+
+		[[nodiscard]] friend constexpr xte::big_int<T> operator|(const xte::big_int<T>& lhs, const xte::big_int<T>& rhs) noexcept(false) {
+			return auto(lhs) |= rhs;
+		}
+
+		constexpr xte::big_int<T>& operator|=(const xte::big_int<T>& rhs) noexcept(false) {
+			return this->_bitwise(rhs, std::bit_or<T>());
+		}
+
+		[[nodiscard]] friend constexpr xte::big_int<T> operator^(const xte::big_int<T>& lhs, const xte::big_int<T>& rhs) noexcept(false) {
+			return auto(lhs) ^= rhs;
+		}
+
+		constexpr xte::big_int<T>& operator^=(const xte::big_int<T>& rhs) noexcept(false) {
+			return this->_bitwise(rhs, std::bit_xor<T>());
+		}
+
+		[[nodiscard]] friend constexpr xte::big_int<T> operator<<(const xte::big_int<T>& lhs, const xte::big_int<T>& rhs) noexcept(false) {
+			return auto(lhs) <<= rhs;
+		}
+
+		constexpr xte::big_int<T>& operator<<=(const xte::big_int<T>& rhs) noexcept(false) {
+			if (!*this || !rhs) {
+				return *this;
+			}
+			if (rhs._neg) {
+				return *this >>= -rhs;
+			}
+			if (xte::uz bit_shift = static_cast<xte::uz>(rhs) & ~-xte::width<T>) {
+				T carry = 0;
+				for (T& digit : this->_data) {
+					carry = xte::exchange(digit, (digit << bit_shift) | carry) >> (xte::width<T> - bit_shift);
+				}
+				if (carry) {
+					this->_data.push(carry);
+				}
+			}
+			this->_data.insert_range(0, xte::gen([digit_shift = static_cast<xte::uz>(rhs) / xte::width<T>] mutable {
+				return digit_shift-- ? xte::opt<T>(0) : xte::null;
+			}));
+			return *this;
+		}
+
+		[[nodiscard]] friend constexpr xte::big_int<T> operator>>(const xte::big_int<T>& lhs, const xte::big_int<T>& rhs) noexcept(false) {
+			return auto(lhs) >>= rhs;
+		}
+
+		constexpr xte::big_int<T>& operator>>=(const xte::big_int<T>& rhs) noexcept(false) {
+			if (!*this || !rhs) {
+				return *this;
+			}
+			if (rhs._neg) {
+				return *this <<= -rhs;
+			}
+			xte::uz digit_shift = static_cast<xte::uz>(rhs) / xte::width<T>;
+			if (digit_shift >= this->_data.size()) {
+				return *this = 0;
+			}
+			this->_data.erase(0, digit_shift);
+			if (xte::uz bit_shift = static_cast<xte::uz>(rhs) & ~-xte::width<T>) {
+				T carry = 0;
+				for (T& digit : std::views::reverse(this->_data)) {
+					carry = xte::exchange(digit, (digit >> bit_shift) | carry) << (xte::width<T> - bit_shift);
+				}
+			}
+			return *this -= +this->_neg;
+		}
+
+		[[nodiscard]] constexpr xte::big_int<T> abs() const noexcept(false) {
+			return xte::big_int<T>(std::from_range, this->_data);
+		}
+
+		[[nodiscard]] constexpr xte::big_int<T> pow(const xte::big_int<T>& exp) const noexcept(false) {
+			if ((*this == 1) || (exp == 1)) {
+				return *this;
+			}
+			if (*this == -1) {
+				return *this * ((exp & 1) ? 1 : -1);
+			}
+			if (!*this || !exp) {
+				if (exp._neg) {
+					throw xte::error("must not take power of zero to negative exponent");
+				}
+				return +!exp;
+			}
+			return ([&, exp = exp] mutable {
+				xte::big_int<T> base = *this;
+				xte::big_int<T> power = 1;
+				while (exp > 1) {
+					if (exp._data[0] & 1) {
+						power *= base;
+						--exp;
+					}
+					base *= base;
+					exp >>= 1;
+				}
+				return power * base;
+			})();
+		}
+
+		[[nodiscard]] constexpr xte::big_int<T> root(const xte::big_int<T>& degree) const noexcept(false) {
+			if (this->_neg) {
+				throw xte::error("must not take root of negative radicand");
+			}
+			if (!degree) {
+				throw xte::error("must not take root of zeroth degree");
+			}
+			if (*this == 1) {
+				return *this;
+			}
+			if (degree._neg) {
+				return 0;
+			}
+			xte::big_int<T> tmp = *this;
+			xte::big_int<T> exp = degree - 1;
+			xte::big_int<T> root = tmp + 1;
+			while (tmp < root) {
+				root = tmp;
+				((tmp *= exp) += *this / tmp.pow(exp)) /= degree;
+			}
+			return root;
+		}
+
+		[[nodiscard]] constexpr xte::big_int<T> log(const xte::big_int<T>& base) const noexcept(false) {
+			if (!base) {
+				return 0;
+			}
+			if (this->_neg) {
+				throw xte::error("must not take logarithm of negative anti-logarithm");
+			}
+			if (base == 1) {
+				throw xte::error("must not take logarithm to unary base");
+			}
+			if (base._neg) {
+				throw xte::error("must not take logarithm to negative base");
+			}
+			return this->_log2() / base._log2();
+		}
+
+		template<xte::is_int Radix = xte::uz>
+		[[nodiscard]] constexpr xte::string str(Radix radix = 10uz, const xte::number_format_config& config = {}) const noexcept(false) {
+			if (!*this || !radix) {
+				return { config.digits[0] };
+			}
+			xte::string result;
+			xte::big_int<T> abs = this->abs();
+			do {
+				if (radix == 1) {
+					result += xte::string(static_cast<xte::uz>(abs), config.digits[1]);
+					break;
+				}
+				if constexpr (xte::is_signed<decltype(radix)>) {
+					if (radix == -1) {
+						result += config.digits[1];
+						for (xte::uz i : std::views::indices(static_cast<xte::uz>(abs - 1))) {
+							result += config.digits[0];
+							result += config.digits[1];
+						}
+						break;
+					}
+				}
+				do {
+					auto index = abs % radix;
+					abs /= radix;
+					if (index < 0) {
+						index += radix;
+						++abs;
+					}
+					result += config.digits[static_cast<xte::uz>(index)];
+				} while (abs);
+			} while (false);
+			if (*this < 0) {
+				result += config.minus[0];
+			}
+			std::ranges::reverse(result);
+			return result;
+		}
+	};
+}
+
+namespace xte::literal::big_int {
+	template<char... digits>
+	[[nodiscard]] consteval xte::big_int<> operator""_big() noexcept(false) {
+		return xte::big_int(xte::string { digits... });
+	}
+}
+
+template<typename T>
+struct std::formatter<xte::big_int<T>> {
+	constexpr auto parse(std::format_parse_context& ctx) noexcept {
+		return ctx.begin();
+	}
+
+	auto format(const xte::big_int<T>& x, std::format_context& ctx) const noexcept(false) {
+		return std::format_to(ctx.out(), "{}", x.str());
+	}
+};
+
+#endif
---
+++ test/math/big_int.cpp
@@ -0,0 +1,88 @@
+#include <xte/data/array.hpp>
+#include <xte/math/big_int.hpp>
+#include <xte/math/width.hpp>
+#include <xte/preproc/feature.hpp>
+#include <xte/util/types.hpp>
+#include <ranges>
+
+static_assert(xte::big_int() == 0);
+static_assert(xte::big_int(0) == 0);
+static_assert(xte::big_int(123) == 123);
+#if XTE_FEATURE_INT_8
+static_assert(xte::big_int<xte::u8>(257) == 257);
+#endif
+static_assert(xte::big_int<unsigned char>(xte::big_int<unsigned long long>(123456789)) == 123456789);
+static_assert(xte::big_int<unsigned long long>(xte::big_int<unsigned char>(123456789)) == 123456789);
+static_assert((sizeof(int) == sizeof(long long)) || (xte::big_int<unsigned>(std::from_range, { 0u, 1u }) == (-1u + 1ull)));
+static_assert(xte::big_int("") == 0);
+static_assert(xte::big_int("0") == 0);
+static_assert(xte::big_int("123") == 123);
+static_assert(xte::big_int("FF", 16) == 255);
+static_assert(static_cast<unsigned>(xte::big_int(123)) == 123);
+static_assert(static_cast<unsigned>(xte::big_int("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", 16)) == -1u);
+static_assert(!xte::big_int(0));
+static_assert(!!xte::big_int(123));
+static_assert(!(xte::big_int(0) < 0));
+static_assert(xte::big_int(123) == 123);
+static_assert(xte::big_int("FFFFFFFFFFFFFFFFFFF", 16) > xte::big_int("FFFF", 16));
+static_assert(xte::big_int(123) > -123);
+static_assert(xte::big_int(-123) < 0);
+static_assert(xte::big_int(1) != 0);
+static_assert(+xte::big_int(123) == 123);
+static_assert((xte::big_int(0) + 0) == 0);
+static_assert((xte::big_int(0) + 123) == 123);
+static_assert((sizeof(char) == sizeof(long long)) || (xte::big_int<unsigned char>(static_cast<unsigned char>(-1)) + 1) == (static_cast<unsigned char>(-1) + 1ull));
+static_assert((xte::big_int(-123) + -1) == -124);
+static_assert(-xte::big_int(0) == 0);
+static_assert(-xte::big_int(123) == -123);
+static_assert((xte::big_int(123) - 123) == 0);
+static_assert((xte::big_int(1) - 123) == -122);
+static_assert((xte::big_int(-5) + 4) == -1);
+static_assert((xte::big_int(0) * 0) == 0);
+static_assert((xte::big_int(123) * 0) == 0);
+static_assert((xte::big_int(0) * 123) == 0);
+static_assert((xte::big_int(2) * 2) == 4);
+static_assert((xte::big_int<unsigned char>(999999999ull) * 999999999ull) == 999999998000000001ull);
+static_assert((xte::big_int(360) / 2) == 180);
+static_assert((xte::big_int(360) / 3) == 120);
+static_assert((xte::big_int(360) / 5) == 72);
+static_assert((xte::big_int(360) / 15) == 24);
+static_assert((xte::big_int(12345) / -1) == -12345);
+static_assert((xte::big_int(12345) % 2) == 1);
+static_assert((xte::big_int(12345) % 3) == 0);
+static_assert((xte::big_int(12345) % 17) == 3);
+static_assert((xte::big_int(12345) % -62) == 7);
+static_assert((xte::big_int(-12345) % -904) == -593);
+static_assert(~xte::big_int(345) == -346);
+static_assert(~xte::big_int(1) == -2);
+static_assert(~xte::big_int(-1) == 0);
+static_assert((xte::big_int(1957) & 9274) == 1056);
+static_assert((xte::big_int(9602) | 7247) == 15823);
+static_assert((xte::big_int(19634) ^ 27592) == 10106);
+static_assert((xte::big_int(1) << 0) == 1);
+static_assert((xte::big_int(1) << 1) == 2);
+static_assert((xte::big_int(1) << 2) == 4);
+static_assert((xte::big_int<unsigned>(1) << xte::width<unsigned>) == xte::big_int<unsigned>(std::from_range, { 0u, 1u }));
+static_assert((xte::big_int(1) >> -1) == 2);
+static_assert((xte::big_int(256) >> 0) == 256);
+static_assert((xte::big_int(256) >> 1) == 128);
+static_assert((xte::big_int(256) >> 2) == 64);
+static_assert((xte::big_int<unsigned>(std::from_range, { 0u, 1u }) >> xte::width<unsigned>) == 1);
+static_assert((xte::big_int(256) << -1) == 128);
+static_assert(xte::big_int(0).abs() == 0);
+static_assert(xte::big_int(123).abs() == 123);
+static_assert(xte::big_int(-123).abs() == 123);
+static_assert(xte::big_int(2).pow(0) == 1);
+static_assert(xte::big_int(2).pow(1) == 2);
+static_assert(xte::big_int(2).pow(2) == 4);
+static_assert(xte::big_int(2).pow(64) == (xte::big_int(1) << 64));
+static_assert(xte::big_int(2).pow(128) == (xte::big_int(1) << 128));
+static_assert(xte::big_int(123).pow(20) == xte::big_int("628206215175202159781085149496179361969201"));
+static_assert(xte::big_int(99).pow(99) == xte::big_int(std::from_range, { 11052210921748023483ull, 5342752518335406880ull, 18273117834117442446ull, 16806157478442079400ull, 5211608751722454046ull, 8940752602480459803ull, 4625783107637031297ull, 14385845698021359473ull, 2377983349987779313ull, 12684730458741789166ull, 81037ull }));
+static_assert(xte::big_int(256).root(2) == 16);
+static_assert(xte::big_int(256).root(4) == 4);
+static_assert(xte::big_int(256).log(2) == 8);
+static_assert(xte::big_int(256).log(2) == 8);
+static_assert(xte::big_int(256).log(4) == 4);
+static_assert(xte::big_int(12345).str() == "12345");
+static_assert(xte::big_int(-12345).str() == "-12345");
