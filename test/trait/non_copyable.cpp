02a091ff813111c3d4ab266f9dc6826aabeef563
Eczbek <eczbek.void@gmail.com>
2026-04-10 15:07:43 -0400

    Fix some missing parentheses

--- test/trait/non_copyable.cpp
+++ test/trait/non_copyable.cpp
@@ -4,7 +4,7 @@
 struct S : xte::non_copyable {};
 
 static_assert(requires { S(); });
-static_assert(!requires (S s) { requires requires { S(s); }; });
+static_assert(!requires (S s) { requires(requires { S(s); }); });
 static_assert(requires (S s) { S(xte::xvalue(s)); });
-static_assert(!requires (S lhs, S rhs) { requires requires { lhs = rhs; }; });
+static_assert(!requires (S lhs, S rhs) { requires(requires { lhs = rhs; }); });
 static_assert(requires (S lhs, S rhs) { lhs = xte::xvalue(rhs); });
