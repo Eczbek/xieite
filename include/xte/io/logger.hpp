3f90740ed121c8a2fa37df23a21d02140bb1773e
Eczbek <eczbek.void@gmail.com>
2026-04-13 15:12:22 -0400

    Properly specify `noexcept` of class constructors in `xte::logger {}`

--- include/xte/io/logger.hpp
+++ include/xte/io/logger.hpp
@@ -16,14 +16,14 @@ namespace DETAIL_XTE {
 	template<xte::uz color, xte::fixed_string tag, typename... Args>
 	struct logger {
 	public:
-		explicit logger(std::format_string<Args...> fmt, Args&&... args, xte::sloc sloc = {}) noexcept
+		explicit logger(std::format_string<Args...> fmt, Args&&... args, xte::sloc sloc = {}) noexcept(false)
 		: logger(sloc, stdout, fmt, XTE_FWD(args)...) {}
 
-		explicit logger(std::FILE* file, std::format_string<Args...> fmt, Args&&... args, xte::sloc sloc = {}) noexcept
+		explicit logger(std::FILE* file, std::format_string<Args...> fmt, Args&&... args, xte::sloc sloc = {}) noexcept(false)
 		: logger(sloc, file, fmt, XTE_FWD(args)...) {}
 
 	private:
-		explicit logger(const xte::sloc& sloc, std::FILE* file, std::format_string<Args...> fmt, Args&&... args) noexcept {
+		explicit logger(const xte::sloc& sloc, std::FILE* file, std::format_string<Args...> fmt, Args&&... args) noexcept(false) {
 			bool tty = xte::isatty(file);
 			std::println(file, "{}{:<5} [{:%F %T}] {}:{}:{}: {}{}", (tty ? std::format("\x1B[{}m", color).c_str() : ""), std::define_static_string(tag), std::chrono::floor<std::chrono::seconds>(std::chrono::system_clock::now()), sloc.file, sloc.func, sloc.line, std::format(fmt, XTE_FWD(args)...), (tty ? "\x1B[0m" : ""));
 		}
