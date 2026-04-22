b59034e6a65f19ba2c12cb1420fea7510420eec8
Eczbek <eczbek.void@gmail.com>
2026-04-21 20:14:35 -0400

    Fix `xte::wrap()` again

--- include/xte/math/wrap.hpp
+++ include/xte/math/wrap.hpp
@@ -4,26 +4,27 @@
 #	include "../math/minmax.hpp"
 #	include "../math/mod.hpp"
 #	include "../trait/is_number.hpp"
-#	include <limits>
 #	include <type_traits>
 
 namespace xte {
 	[[nodiscard]] constexpr auto wrap(xte::is_number auto x, xte::is_number auto limit0, xte::is_number auto limit1) noexcept {
 		using Common = std::common_type_t<decltype(x), decltype(limit0), decltype(limit1)>;
-		if (limit0 == limit1) {
-			return static_cast<Common>(limit0);
-		}
 		auto [min, max] = xte::minmax(static_cast<Common>(limit0), static_cast<Common>(limit1));
-		Common y = static_cast<Common>(x);
-		return ((y >= min) && (y <= max))
-			? y
-			: (min == std::numeric_limits<Common>::min())
-				? (y - max + min)
-				: (max == std::numeric_limits<Common>::max())
-					? (y != std::numeric_limits<Common>::min())
-						? max
-						: (min - y + max)
-					: (xte::mod(y - min, max - min + 1) + min);
+		if constexpr (xte::is_float<Common>) {
+			return xte::mod(static_cast<Common>(x) - min, max - min + 1) + min;
+		} else {
+			using Unsigned = std::make_unsigned_t<Common>;
+			auto range = static_cast<Unsigned>(max) - static_cast<Unsigned>(min) + 1;
+			if (!range) {
+				return static_cast<Common>(x);
+			}
+			auto rem = (static_cast<Unsigned>(x) - static_cast<Unsigned>(min)) % range;
+			if (x < min) {
+				auto mod = (static_cast<Unsigned>(-1) % range + 1) % range;
+				rem += (rem < mod) ? (range - mod) : -mod;
+			}
+			return static_cast<Common>(static_cast<Unsigned>(min) + rem);
+		}
 	}
 }
 
