e632519bd2212454d1aae34a49e076e942f009ca
Eczbek <eczbek.void@gmail.com>
2026-05-19 23:30:11 -0400

    Simplify implementation of `xte::is_order<>`

--- include/xte/trait/is_order.hpp
+++ include/xte/trait/is_order.hpp
@@ -1,13 +1,12 @@
 #ifndef DETAIL_XTE_HEADER_TRAIT_IS_ORDER
 #	define DETAIL_XTE_HEADER_TRAIT_IS_ORDER
 #
-#	include "../trait/is_same_any.hpp"
-#	include "../trait/remove_cv.hpp"
+#	include "../trait/is_same_any_ignore_cv.hpp"
 #	include <compare>
 
 namespace xte {
 	template<typename T>
-	concept is_order = xte::is_same_any<xte::remove_cv<T>, std::strong_ordering, std::weak_ordering, std::partial_ordering>;
+	concept is_order = xte::is_same_any_ignore_cv<T, std::strong_ordering, std::weak_ordering, std::partial_ordering>;
 }
 
 #endif
