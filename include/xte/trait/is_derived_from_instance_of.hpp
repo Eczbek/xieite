476ba77e313b367aaa51236faf81fe591193094c
Eczbek <eczbek.void@gmail.com>
2026-05-20 01:18:22 -0400

    Make behavior of `xte::is_derived_from_instance_of<>` more intuitive

--- include/xte/trait/is_derived_from_instance_of.hpp
+++ include/xte/trait/is_derived_from_instance_of.hpp
@@ -8,10 +8,11 @@
 namespace xte {
 	template<typename T, std::meta::info tmpl>
 	concept is_derived_from_instance_of =
-		std::meta::is_class_type(std::meta::dealias(^^T))
+		std::meta::is_class_type(^^T)
 		&& std::meta::is_class_template(tmpl)
 		&& std::ranges::any_of(
-			xte::refl::bases_of(^^T) | std::views::transform(std::meta::type_of),
+			std::views::concat(std::views::single(^^T), xte::refl::bases_of(^^T))
+				| std::views::transform(xte::refl::type_of),
 			[](std::meta::info base) {
 				return std::meta::has_template_arguments(base)
 					&& (std::meta::template_of(base) == tmpl);
