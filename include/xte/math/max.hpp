d6fdb80d11e6c5e1a19c1b82ac6bd8ae36be3f62
Eczbek <eczbek.void@gmail.com>
2026-05-27 19:58:01 -0400

    Allow NRVO in `xte::max()` and `xte::min()`

--- include/xte/math/max.hpp
+++ include/xte/math/max.hpp
@@ -2,19 +2,30 @@
 #	define DETAIL_XTE_HEADER_MATH_MAX
 #
 #	include "../math/less.hpp"
-#	include "../preproc/arrow.hpp"
 #	include "../preproc/fwd.hpp"
 #	include "../util/assign.hpp"
 #	include "../util/cast.hpp"
-#	include "../util/xvalue.hpp"
 #	include <type_traits>
 
 namespace xte {
-	[[nodiscard]] constexpr auto max(auto&& first, auto&&... rest) XTE_ARROW(
-		([](auto max, auto&&... rest) XTE_ARROW(
-			(..., void(xte::less(max, rest) ? xte::assign(max, XTE_FWD(rest)) : max)), auto(xte::xvalue(max))
-		))(xte::cast<std::common_type_t<decltype(first), decltype(rest)...>>(XTE_FWD(first)), XTE_FWD(rest)...)
-	)
+	template<typename T, typename... Ts,
+		typename common_type = std::common_type_t<T, Ts...>>
+	[[nodiscard]] constexpr auto max(T&& first, Ts&&... rest)
+	noexcept(noexcept(xte::cast<common_type>(XTE_FWD(first)))
+		&& (... && requires (common_type max) {
+			{ xte::less(max, rest) } noexcept;
+			{ xte::assign(max, XTE_FWD(rest)) } noexcept;
+		}))
+	requires(requires { xte::cast<common_type>(XTE_FWD(first)); }
+		&& (... && requires (common_type max) {
+			xte::less(max, rest);
+			xte::assign(max, XTE_FWD(rest));
+		}))
+	{
+		auto max = xte::cast<common_type>(XTE_FWD(first));
+		(..., (xte::less(max, rest) ? void(xte::assign(max, XTE_FWD(rest))) : void()));
+		return max;
+	}
 }
 
 #endif
--- include/xte/math/min.hpp
+++ include/xte/math/min.hpp
@@ -2,19 +2,30 @@
 #	define DETAIL_XTE_HEADER_MATH_MIN
 #
 #	include "../math/less.hpp"
-#	include "../preproc/arrow.hpp"
 #	include "../preproc/fwd.hpp"
 #	include "../util/assign.hpp"
 #	include "../util/cast.hpp"
-#	include "../util/xvalue.hpp"
 #	include <type_traits>
 
 namespace xte {
-	[[nodiscard]] constexpr auto min(auto&& first, auto&&... rest) XTE_ARROW(
-		([](auto min, auto&&... rest) XTE_ARROW(
-			(..., void(xte::less(rest, min) ? xte::assign(min, XTE_FWD(rest)) : min)), auto(xte::xvalue(min))
-		))(xte::cast<std::common_type_t<decltype(first), decltype(rest)...>>(XTE_FWD(first)), XTE_FWD(rest)...)
-	)
+	template<typename T, typename... Ts,
+		typename common_type = std::common_type_t<T, Ts...>>
+	[[nodiscard]] constexpr auto min(T&& first, Ts&&... rest)
+	noexcept(noexcept(xte::cast<common_type>(XTE_FWD(first)))
+		&& (... && requires (common_type min) {
+			{ xte::less(rest, min) } noexcept;
+			{ xte::assign(min, XTE_FWD(rest)) } noexcept;
+		}))
+	requires(requires { xte::cast<common_type>(XTE_FWD(first)); }
+		&& (... && requires (common_type min) {
+			xte::less(rest, min);
+			xte::assign(min, XTE_FWD(rest));
+		}))
+	{
+		auto min = xte::cast<common_type>(XTE_FWD(first));
+		(..., (xte::less(rest, min) ? void(xte::assign(min, XTE_FWD(rest))) : void()));
+		return min;
+	}
 }
 
 #endif
