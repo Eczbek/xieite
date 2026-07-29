fdac73ded895b53d37cee3cbc1e214317ab863ec
Eczbek <eczbek.void@gmail.com>
2026-07-29 13:18:44 +0200

    Add `xte::file_pipe {}`

---
+++ include/xte/io/file_pipe.hpp
@@ -0,0 +1,64 @@
+#ifndef DETAIL_XTE_HEADER_IO_FILE_PIPE
+#	define DETAIL_XTE_HEADER_IO_FILE_PIPE
+#
+#	include "../data/string.hpp"
+#	include "../data/string_view.hpp"
+#	include "../io/eof.hpp"
+#	include "../io/file.hpp"
+#	include "../io/file_mode.hpp"
+#	include "../preproc/platform.hpp"
+#	include <cstdio>
+#	include <stdio.h>
+
+#	if XTE_PLATFORM_UNIX
+#		include <sys/wait.h>
+#	endif
+
+namespace xte {
+	struct file_pipe : xte::file {
+	public:
+		[[nodiscard]] explicit(false) file_pipe() noexcept = default;
+
+		[[nodiscard]] file_pipe(std::FILE* stream, xte::file_mode mode) noexcept
+		: xte::file(stream, mode) {}
+
+		[[nodiscard]] file_pipe(xte::string_view command, xte::file_mode mode) noexcept(false) {
+			this->open(command, mode);
+		}
+
+		bool open(xte::string_view command, xte::file_mode mode) noexcept(false) {
+#	if XTE_PLATFORM_WINDOWS
+			return (*this = xte::file_pipe(::_popen(xte::string(command).data(), mode), mode));
+#	else
+			return (*this = xte::file_pipe(::popen(xte::string(command).data(), mode), mode));
+#	endif
+		}
+
+		int close() noexcept {
+			if (!*this) {
+				return xte::eof;
+			}
+#	if XTE_PLATFORM_WINDOWS
+			int status = ::_pclose(this->release());
+#	else
+			int status = ::pclose(this->release());
+			if (WIFEXITED(status)) {
+				return WEXITSTATUS(status);
+			}
+			if (WIFSIGNALED(status)) {
+				return WTERMSIG(status);
+			}
+			if (WIFSTOPPED(status)) {
+				return WSTOPSIG(status);
+			}
+#	endif
+			return status;
+		}
+
+		bool reopen(xte::string_view command, xte::file_mode mode) noexcept(false) {
+			return !this->close() && this->open(command, mode);
+		}
+	};
+}
+
+#endif
