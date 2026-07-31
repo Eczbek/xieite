37e1effa9f52dfbd2dba2c364b7a0462ee032eb0
Eczbek <eczbek.void@gmail.com>
2026-07-31 15:31:44 +0200

    Make `xte::rotate_swap()` properly propagate constraints

--- include/xte/func/rotate_swap.hpp
+++ include/xte/func/rotate_swap.hpp
@@ -2,33 +2,33 @@
 #	define DETAIL_XTE_HEADER_FUNC_ROTATE_SWAP
 #
 #	include "../func/unroll.hpp"
-#	include "../trait/is_noex_move_assignable.hpp"
-#	include "../trait/is_noex_move_constructible.hpp"
+#	include "../preproc/returns.hpp"
 #	include "../util/as_xvalue.hpp"
+#	include "../util/assign.hpp"
 #	include "../util/number_types.hpp"
 
 namespace xte {
 	template<xte::uz distance = 1>
-	constexpr void rotate_swap(auto&... args)
-	noexcept((... && (xte::is_noex_move_constructible<decltype(args)> && xte::is_noex_move_assignable<decltype(args)>))) {
-		if constexpr (static constexpr xte::uz size = sizeof...(args); size && (distance % size)) {
-			if constexpr (static constexpr xte::uz mod = distance % size; (mod == 1) || (size % mod)) {
-				auto tmp = xte::as_xvalue(args...[0]);
-				xte::unroll<(size - 1)>([&]<xte::uz i> {
-					args...[i * mod % size] = xte::as_xvalue(args...[(i * mod % size + mod) % size]);
-				});
-				args...[size - mod] = xte::as_xvalue(tmp);
-			} else {
-				xte::unroll<mod>([&]<xte::uz i> {
-					auto tmp = xte::as_xvalue(args...[i]);
-					xte::unroll<(size / mod)>([&]<xte::uz j> {
-						args...[i + j * mod] = xte::as_xvalue(args...[(i + j * mod + mod) % size]);
-					});
-					args...[size - mod + i] = xte::as_xvalue(tmp);
-				});
-			}
-		}
-	}
+	constexpr auto rotate_swap(auto&... args) XTE_RETURNS_IF(
+		(sizeof...(args) && !!(distance % sizeof...(args))),
+		(void)([]<xte::uz size = sizeof...(args), xte::uz mod = (distance % size)>(auto&... args) XTE_RETURNS_CHOOSE(
+			((mod == 1) || (size % mod)),
+			([](auto tmp, auto&... args) XTE_RETURNS(
+				xte::unroll<(size - 1)>([]<xte::uz i>(auto&... args) XTE_RETURNS(
+					xte::assign(args...[i * mod % size], xte::as_xvalue(args...[(i * mod % size + mod) % size]))
+				), args...),
+				xte::assign(args...[size - mod], xte::as_xvalue(tmp))
+			))(xte::as_xvalue(args...[0]), args...),
+			xte::unroll<mod>([]<xte::uz i>(auto&... args) XTE_RETURNS(
+				([](auto tmp, auto&... args) XTE_RETURNS(
+					xte::unroll<(size / mod)>([]<xte::uz j>(auto&... args) XTE_RETURNS(
+						xte::assign(args...[i + j * mod], xte::as_xvalue(args...[(i + j * mod + mod) % size]))
+					), args...),
+					xte::assign(args...[size - mod + i], xte::as_xvalue(tmp))
+				))(xte::as_xvalue(args...[i]), args...)
+			), args...)
+		))(args...)
+	)
 }
 
 #endif
