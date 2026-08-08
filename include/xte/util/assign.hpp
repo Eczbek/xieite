09dd313a34b0a22b6cff187d21397c7bc42a122f
Eczbek <eczbek.void@gmail.com>
2026-08-08 02:36:49 +0200

    Constrain `xte::assign` to return reference

--- include/xte/util/assign.hpp
+++ include/xte/util/assign.hpp
@@ -14,17 +14,17 @@
 
 namespace DETAIL_XTE::assign {
 	template<xte::is_arithmetic lhs_type>
-	constexpr lhs_type& impl(lhs_type& lhs, xte::is_arithmetic auto rhs) {
+	constexpr lhs_type& impl(lhs_type& lhs, xte::is_arithmetic auto rhs) noexcept {
 		return (lhs = xte::make<lhs_type>(rhs));
 	}
 
 	template<typename lhs_type, typename rhs_type>
 	requires(!xte::is_arithmetic<lhs_type> || !xte::is_arithmetic<xte::drop_ref<rhs_type>>)
 	constexpr auto impl(lhs_type& lhs, rhs_type&& rhs) XTE_RETURNS_FIRST(
-		XTE_FWD(lhs) = XTE_FWD(rhs),
-		XTE_FWD(lhs) = xte::make<xte::drop_cvref<lhs_type>>(XTE_FWD(rhs)),
+		static_cast<lhs_type&>(lhs = XTE_FWD(rhs)),
+		static_cast<lhs_type&>(lhs = xte::make<lhs_type>(XTE_FWD(rhs))),
 		xte::reconstruct(lhs, XTE_FWD(rhs)),
-		xte::reconstruct(lhs, xte::make<xte::drop_cvref<lhs_type>>(XTE_FWD(rhs)))
+		xte::reconstruct(lhs, xte::make<lhs_type>(XTE_FWD(rhs)))
 	)
 }
 
@@ -38,7 +38,7 @@ namespace xte {
 	)
 
 	constexpr auto assign(auto& lhs) XTE_RETURNS_FIRST(
-		XTE_FWD(lhs) = xte::drop_cvref<decltype(lhs)>(),
+		static_cast<decltype(lhs)&>(lhs = xte::drop_cvref<decltype(lhs)>()),
 		xte::reconstruct(lhs)
 	)
 }
