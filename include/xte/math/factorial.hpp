a536a385bb89a5eaec7c193a6bcfa08f6d47d03e
Eczbek <eczbek.void@gmail.com>
2026-08-08 02:35:11 +0200

    Simplify `xte::factorial` and `xte::fibonacci`

--- include/xte/math/factorial.hpp
+++ include/xte/math/factorial.hpp
@@ -2,7 +2,6 @@
 #	define DETAIL_XTE_HEADER_MATH_FACTORIAL
 #
 #	include "../data/array.hpp"
-#	include "../func/gen.hpp"
 #	include "../math/mul_checked.hpp"
 #	include "../trait/is_arithmetic.hpp"
 #	include <meta>
@@ -11,7 +10,10 @@ namespace xte {
 	template<xte::is_arithmetic T>
 	constexpr auto factorial = std::define_static_array(([] {
 		xte::array<T> result = { 1 };
-		return result += xte::gen([&] { return xte::mul_checked(result.size(), result.back()); });
+		while (auto next = xte::mul_checked(result.size(), result.back())) {
+			result.append(*next);
+		}
+		return result;
 	})());
 }
 
--- include/xte/math/fibonacci.hpp
+++ include/xte/math/fibonacci.hpp
@@ -2,7 +2,6 @@
 #	define DETAIL_XTE_HEADER_MATH_FIBONACCI
 #
 #	include "../data/array.hpp"
-#	include "../func/gen.hpp"
 #	include "../math/add_checked.hpp"
 #	include "../trait/is_arithmetic.hpp"
 #	include <meta>
@@ -11,7 +10,10 @@ namespace xte {
 	template<xte::is_arithmetic T>
 	constexpr auto fibonacci = std::define_static_array(([] {
 		xte::array<T> result = { 0, 1 };
-		return result += xte::gen([&] { return xte::add_checked(result.back(), result.back(1)); });
+		while (auto next = xte::add_checked(result.back(), result.back(1))) {
+			result.append(*next);
+		}
+		return result;
 	})());
 }
 
