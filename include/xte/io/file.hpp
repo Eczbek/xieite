494b1ed35c255e8b2cd0d8f1068ec18c77924b9c
Eczbek <eczbek.void@gmail.com>
2026-09-22 17:16:56 +0200

    Improve `xte::file`

--- include/xte/io/file.hpp
+++ include/xte/io/file.hpp
@@ -3,18 +3,78 @@
 #
 #	include "../aliases.hpp"
 #	include "../detect/platform.hpp"
+#	include "../exchange.hpp"
 #	include "../io/eof.hpp"
-#	include "../io/file_mode.hpp"
 #	include "../preproc/lift.hpp"
-#	include "../ptr.hpp"
+#	include "../qual_cast.hpp"
 #	include "../string.hpp"
 #	include "../string_view.hpp"
 #	include "../sys/is_tty.hpp"
 #	include <cstdio>
 #	include <stdio.h>
 #	include <utility>
+#	if XTE_PLATFORM_UNIX
+#		include <sys/wait.h>
+#	endif
 
 namespace xte {
+	struct file_mode {
+	private:
+		xte::uz _mask;
+	
+		[[nodiscard]] constexpr file_mode(xte::uz mask) noexcept
+		: _mask(mask) {}
+
+	public:
+		[[nodiscard]] constexpr file_mode() noexcept
+		: _mask(xte::file_mode::none) {}
+
+		static const xte::file_mode none;
+		static const xte::file_mode read;
+		static const xte::file_mode write;
+		static const xte::file_mode overwrite;
+		static const xte::file_mode append;
+		static const xte::file_mode read_write;
+		static const xte::file_mode read_overwrite;
+		static const xte::file_mode read_append;
+		
+		[[nodiscard]] explicit(false) constexpr operator xte::uz() const noexcept {
+			return this->_mask;
+		}
+
+		[[nodiscard]] explicit(false) constexpr operator const char*() const noexcept;
+	};
+
+	constexpr xte::file_mode xte::file_mode::none = xte::file_mode(0b0000);
+	constexpr xte::file_mode xte::file_mode::read = xte::file_mode(0b0001);
+	constexpr xte::file_mode xte::file_mode::write = xte::file_mode(0b0010);
+	constexpr xte::file_mode xte::file_mode::overwrite = xte::file_mode(0b0110);
+	constexpr xte::file_mode xte::file_mode::append = xte::file_mode(0b1000);
+	constexpr xte::file_mode xte::file_mode::read_write = xte::file_mode(0b0011);
+	constexpr xte::file_mode xte::file_mode::read_overwrite = xte::file_mode(0b0111);
+	constexpr xte::file_mode xte::file_mode::read_append = xte::file_mode(0b1001);
+
+	constexpr xte::file_mode::operator const char*() const noexcept {
+		switch (this->_mask) {
+			case xte::file_mode::read:
+				return "r";
+			case xte::file_mode::write:
+				return "wx";
+			case xte::file_mode::overwrite:
+				return "w";
+			case xte::file_mode::append:
+				return "a";
+			case xte::file_mode::read_write:
+				return "r+";
+			case xte::file_mode::read_overwrite:
+				return "wx+";
+			case xte::file_mode::read_append:
+				return "a+";
+			default:
+				return nullptr;
+		}
+	}
+
 	struct file {
 	public:
 		[[nodiscard]] explicit(false) file() noexcept = default;
@@ -30,25 +90,27 @@ namespace xte {
 			this->close();
 		}
 
-		xte::file& operator=(xte::file&& other) noexcept {
-			this->_stream = other._stream.release();
+		xte::file& operator=(xte::file&& other) & noexcept {
+			this->close();
+			this->_stream = other.release();
 			this->_mode = other._mode;
 			return *this;
 		}
 
 		[[nodiscard]] explicit(false) operator std::FILE*() const noexcept {
-			return this->data();
+			return this->_stream;
 		}
 
-		[[nodiscard]] std::FILE* data() const noexcept {
-			return this->_stream.data();
+		[[nodiscard]] xte::file_mode mode() const noexcept {
+			return this->_mode;
 		}
 
 		[[nodiscard]] std::FILE* release() noexcept {
-			return this->_stream.release();
+			this->_mode = xte::file_mode::none;
+			return xte::exchange(this->_stream, nullptr);
 		}
 
-		bool open(xte::string_view path, xte::file_mode mode) noexcept(false) {
+		bool open(xte::string_view path, xte::file_mode mode) & noexcept(false) {
 			if (*this) {
 				return false;
 			}
@@ -56,38 +118,18 @@ namespace xte {
 			return !!(this->_stream = path.make_c_str_for(XTE_LIFT(std::fopen), mode));
 		}
 
-		bool close() noexcept {
+		bool close() & noexcept {
 			return (this->_stream && !xte::is_tty(*this))
 				? !std::fclose(this->release())
 				: !!this->release();
 		}
 
-		bool reopen(xte::string_view path, xte::file_mode mode) noexcept(false) {
-			return this->close() && this->open(path, mode);
-		}
-
-		[[nodiscard]] bool is_readable() const noexcept {
-			return *this && (this->_mode & xte::file_mode::read);
-		}
-
-		[[nodiscard]] bool is_writable() const noexcept {
-			return *this && (this->_mode & xte::file_mode::write);
-		}
-
-		[[nodiscard]] bool is_overwritable() const noexcept {
-			return *this && (this->_mode & xte::file_mode::overwrite);
-		}
-
-		[[nodiscard]] bool is_appendable() const noexcept {
-			return *this && (this->_mode & xte::file_mode::append);
-		}
-
 		bool write(xte::string_view content) const noexcept {
-			return std::fwrite(content.data(), 1, content.size(), *this) == content.size();
+			return std::fwrite(content.data(), 1, content.size(), this->_stream) == content.size();
 		}
 
 		bool flush() noexcept {
-			return !std::fflush(*this);
+			return !std::fflush(this->_stream);
 		}
 
 		[[nodiscard]] xte::string read() const noexcept(false) {
@@ -96,7 +138,7 @@ namespace xte {
 			while (true) {
 				xte::uz prev_size = content.size();
 				content.resize(prev_size + chunk_size);
-				if (xte::uz bytes_read = std::fread(content.data() + prev_size, 1, chunk_size, *this); bytes_read != chunk_size) {
+				if (xte::uz bytes_read = std::fread(content.data() + prev_size, 1, chunk_size, this->_stream); bytes_read != chunk_size) {
 					content.resize(prev_size + bytes_read);
 					break;
 				}
@@ -107,7 +149,7 @@ namespace xte {
 		[[nodiscard]] xte::string read(char delim) const noexcept(false) {
 			xte::string content;
 			while (true) {
-				int input = std::fgetc(*this);
+				int input = std::fgetc(this->_stream);
 				if ((input == xte::eof) || (input == delim)) {
 					break;
 				}
@@ -118,17 +160,91 @@ namespace xte {
 
 		[[nodiscard]] int descriptor() const noexcept {
 #	if XTE_PLATFORM_WINDOWS
-			return ::_fileno(this->_stream.data());
+			return ::_fileno(this->_stream);
 #	elif XTE_PLATFORM_UNIX
-			return ::fileno(this->_stream.data());
+			return ::fileno(this->_stream);
 #	else
 			return -1;
 #	endif
 		}
 
 	private:
-		xte::ptr<std::FILE> _stream;
+		std::FILE* _stream = nullptr;
 		xte::file_mode _mode = xte::file_mode::none;
+	};
+
+	struct file_pipe {
+	public:
+		[[nodiscard]] explicit(false) file_pipe() noexcept = default;
+
+		[[nodiscard]] explicit file_pipe(std::FILE* stream) noexcept
+		: _file(stream, xte::file_mode::read) {}
+
+		[[nodiscard]] explicit file_pipe(xte::string_view path) noexcept(false) {
+			this->open(path);
+		}
+
+		~file_pipe() {
+			this->close();
+		}
+
+		xte::file_pipe& operator=(xte::file_pipe&& other) & noexcept {
+			this->_file = xte::as_xvalue(other)._file;
+			return *this;
+		}
+
+		[[nodiscard]] explicit(false) operator std::FILE*() const noexcept {
+			return this->_file;
+		}
+
+		[[nodiscard]] std::FILE* release() noexcept {
+			return this->_file.release();
+		}
+
+		bool open(xte::string_view command) & noexcept(false) {
+#	if XTE_PLATFORM_WINDOWS
+			std::FILE* pipe = command.make_c_str_for(XTE_LIFT(::_popen), xte::file_mode::read);
+#	else
+			std::FILE* pipe = command.make_c_str_for(XTE_LIFT(::popen), xte::file_mode::read);
+#	endif
+			return (*this = xte::file_pipe(pipe));
+		}
+
+		bool close() & noexcept {
+			if (!*this) {
+				return xte::eof;
+			}
+#	if XTE_PLATFORM_WINDOWS
+			int status = ::_pclose(this->release());
+#	else
+			int status = ::pclose(this->release());
+			if (WIFEXITED(status)) {
+				return WEXITSTATUS(status);
+			}
+			if (WIFSIGNALED(status)) {
+				return WTERMSIG(status);
+			}
+			if (WIFSTOPPED(status)) {
+				return WSTOPSIG(status);
+			}
+#	endif
+			return status;
+		}
+
+		[[nodiscard]] xte::string read() const noexcept(false) {
+			return this->_file.read();
+		}
+
+		[[nodiscard]] xte::string read(char delim) const noexcept(false) {
+			return this->_file.read(delim);
+		}
+
+		[[nodiscard]] int descriptor() const noexcept {
+			return this->_file.descriptor();
+		}
+
+	private:
+		xte::file _file;
 	};
 }
 
--- include/xte/sys/exec.hpp
+++ include/xte/sys/exec.hpp
@@ -2,14 +2,13 @@
 #	define DETAIL_XTE_HEADER_SYS_EXEC
 #
 #	include "../aggregate.hpp"
-#	include "../io/file_mode.hpp"
-#	include "../io/file_pipe.hpp"
+#	include "../io/file.hpp"
 #	include "../string.hpp"
 #	include "../string_view.hpp"
 
 namespace xte {
 	inline xte::aggregate<^^xte::string, "output", ^^int, "status"> exec(xte::string_view command) noexcept(false) {
-		if (auto pipe = xte::file_pipe(command, xte::file_mode::read)) {
+		if (auto pipe = xte::file_pipe(command)) {
 			return { pipe.read(), pipe.close() };
 		} else {
 			return { "", pipe.close() };
